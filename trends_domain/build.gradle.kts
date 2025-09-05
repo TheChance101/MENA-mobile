1412b0ee476db132aaf26f1428e7c040f3ded955
AhmadKharfan <plamoplamo12@example.com>
2025-09-05 15:17:28 +0000

    add trends domain, data, presentation and api modules

---
+++ trends_domain/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+kotlin {
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+
+        }
+    }
+}
