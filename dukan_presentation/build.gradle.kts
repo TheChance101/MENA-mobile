f21fec6bcea671ee28e297ad32f2929fc52dc38a
a7med naser <ad0112727@gmail.com>
2025-09-10 21:39:21 +0300

    add build configure Compose resources for dukan_presentation module add `add dukan icon`

--- dukan_presentation/build.gradle.kts
+++ dukan_presentation/build.gradle.kts
@@ -53,3 +53,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
+compose.resources {
+    publicResClass = false
+    packageOfResClass = "dukan_presentation.src.commonMain.composeResources"
+    generateResClass = auto
+}
