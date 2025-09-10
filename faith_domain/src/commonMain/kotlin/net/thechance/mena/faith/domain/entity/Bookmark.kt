970bf515441a373d4d4c0f298dc868eb4facf331
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-09-11 01:04:01 +0300

    feat: update Bookmark entity to use LocalDateTime and add kotlinx.datetime dependency

--- faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/entity/Bookmark.kt
+++ faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/entity/Bookmark.kt
@@ -1,8 +1,10 @@
 package net.thechance.mena.faith.domain.entity
 
+import kotlinx.datetime.LocalDateTime
+
 data class Bookmark(
     val id: Int,
     val surah: Surah,
     val ayah: Ayah,
-    val createdAt: Long // LocalDateTime
+    val createdAt: LocalDateTime
 )
