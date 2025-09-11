323dd03556e4d1d4519f51967576e8637b4f74fd
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-09-11 21:37:49 +0300

    refactor: remove Bookmark methods from QuranRepository and its implementation

--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/repository/QuranRepositoryImpl.kt
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/repository/QuranRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package net.thechance.mena.faith.data.repository
 
 import net.thechance.mena.faith.domain.entity.Ayah
-import net.thechance.mena.faith.domain.entity.Bookmark
 import net.thechance.mena.faith.domain.entity.Surah
 import net.thechance.mena.faith.domain.repository.QuranRepository
 
@@ -21,21 +20,6 @@ class QuranRepositoryImpl(
     }
 
     override fun getAyatContent(ayahNo: Int, surahId: Int): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getBookmark(): List<Bookmark> {
-        TODO("Not yet implemented")
-    }
-
-    override fun saveAyahBookmark(
-        ayahNo: Int,
-        surahId: Int
-    ): Bookmark {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteBookmark(id: Int) {
         TODO("Not yet implemented")
     }
 }
--- faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/repository/QuranRepository.kt
+++ faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/repository/QuranRepository.kt
@@ -1,7 +1,6 @@
 package net.thechance.mena.faith.domain.repository
 
 import net.thechance.mena.faith.domain.entity.Ayah
-import net.thechance.mena.faith.domain.entity.Bookmark
 import net.thechance.mena.faith.domain.entity.Surah
 
 interface QuranRepository {
@@ -9,7 +8,4 @@ interface QuranRepository {
     fun getSurahById(id: Int): Surah
     fun getAyatOfSurah(surahId: Int): List<Ayah>
     fun getAyatContent(ayahNo: Int, surahId: Int): String
-    fun getBookmark(): List<Bookmark>
-    fun saveAyahBookmark(ayahNo: Int, surahId: Int): Bookmark
-    fun deleteBookmark(id: Int)
 }
