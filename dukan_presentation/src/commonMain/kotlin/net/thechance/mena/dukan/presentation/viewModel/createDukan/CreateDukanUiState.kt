38a78545d0ff74dd0d4811a06e59f5bf28b6849f
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-12 14:22:51 +0300

    feat: add initial create dukan ui state

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanUiState.kt
@@ -0,0 +1,6 @@
+package net.thechance.mena.dukan.presentation.viewModel.createDukan
+
+data class CreateDukanUiState(
+    val selectedImageUri: Int = 0,
+    val isNextButtonEnabled: Boolean = false
+)
