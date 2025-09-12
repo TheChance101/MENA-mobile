6b93ad8db38ec8e7f60ae3ee19e24b4839c2e54c
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-12 14:38:33 +0300

    feat: add initial create dukan screen

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/CreateDukanScreen.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/CreateDukanScreen.kt
@@ -1,8 +1,5 @@
 package net.thechance.mena.dukan.presentation.screen.CreateDukan
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -10,10 +7,7 @@ fun CreateDukanScreen() {
 
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateDukanContent() {
-    Column{
 
-    }
 }
