ed14519a8ae815615d3aab551fb94e4ff803c546
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-12 22:24:31 +0300

    feat: implement UploadImageBox component

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/components/UploadImageBox.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/components/UploadImageBox.kt
@@ -1,6 +1,7 @@
 package net.thechance.mena.dukan.presentation.screen.CreateDukan.components
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,45 +17,51 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-fun UploadImageContainer() {
-    val dashEffect = PathEffect.dashPathEffect(floatArrayOf(6f, 6f), 0f)
+fun UploadImageContainer(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    val dashEffect = PathEffect.dashPathEffect(floatArrayOf(16f, 12f), 0f)
     val borderColor = Theme.colorScheme.brand.brand
+    val radius = Theme.radius.xl
+
     Box(
-        modifier =
-            Modifier.fillMaxWidth()
-                .height(156.dp)
-                .clip(shape = RoundedCornerShape(Theme.radius.xl))
-                .border(
-                    width = 1.dp,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(156.dp)
+            .clip(RoundedCornerShape(radius))
+            .drawBehind {
+                drawRoundRect(
                     color = borderColor,
-                    shape = RoundedCornerShape(Theme.radius.xl),
+                    style = Stroke(width = 2.dp.toPx(), pathEffect = dashEffect),
+                    cornerRadius = CornerRadius(radius.toPx())
                 )
-                .drawBehind {
-                    drawRoundRect(
-                        color = borderColor,
-                        style = Stroke(width = 2.dp.toPx(), pathEffect = dashEffect),
-                        cornerRadius = CornerRadius(12.dp.toPx())
-                    )
-                },
+            }
+            .clickable { onClick() },
         contentAlignment = Alignment.Center
-    )
-    {
-        Column {
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image()//add the image here
             Text(
                 text = "Click to upload",
                 color = Theme.colorScheme.primary.primary,
-                style = Theme.typography.label.medium,
+                style = Theme.typography.label.medium
             )
         }
     }
 }
 
+
 @Preview
 @Composable
 private fun UploadImageContainerPreview() {
-    UploadImageContainer()
+    MenaTheme {
+        UploadImageContainer {
+        }
+    }
 }
