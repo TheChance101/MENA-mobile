cb01507327f4a974c14b797d3115f65745c24953
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-12 14:22:28 +0300

    feat: add create dukan interaction listener

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanInteractionListener.kt
@@ -0,0 +1,11 @@
+package net.thechance.mena.dukan.presentation.viewModel.createDukan
+
+interface CreateDukanInteractionListener {
+    fun onClickUploadImage()
+    fun onClickEditImage()
+    fun onCLickNext()
+    fun onZoomInClicked()
+    fun onZoomOutClicked()
+    fun onResetClicked()
+    fun onUploadAnotherImageClicked()
+}
