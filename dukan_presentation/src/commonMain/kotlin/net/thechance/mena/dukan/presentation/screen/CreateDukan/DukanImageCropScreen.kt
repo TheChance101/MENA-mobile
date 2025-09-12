efce4b035741125b0bf2c82aa54fb83c2b1d4439
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-12 22:24:11 +0300

    feat: add dukan image crop screen

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/DukanImageCropScreen.kt
@@ -0,0 +1,85 @@
+package net.thechance.mena.dukan.presentation.screen.CreateDukan
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import net.thechance.mena.dukan.presentation.screen.CreateDukan.components.ImageCroppingBox
+import net.thechance.mena.dukan.presentation.screen.CreateDukan.components.SaveButton
+import net.thechance.mena.dukan.presentation.screen.CreateDukan.components.UploadAnotherImageButton
+import net.thechance.mena.dukan.presentation.screen.CreateDukan.components.ZoomControls
+import net.thechance.mena.dukan.presentation.viewModel.createDukan.CreateDukanInteractionListener
+import net.thechance.mena.dukan.presentation.viewModel.createDukan.CreateDukanUiState
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DukanImageCropScreen() {
+}
+
+@Composable
+fun DukanImageCropContent(
+    state: CreateDukanUiState,
+    interactionListener: CreateDukanInteractionListener
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Theme.colorScheme.background.surfaceHigh)
+            .statusBarsPadding()
+            .padding(horizontal = 16.dp),
+    ) {
+        AppBar(title = "Dukan image")
+        ImageCroppingBox(
+            content = {},
+            modifier = Modifier.padding(top = 24.dp)
+        )//The cropper will be here
+        ZoomControls(
+            onZoomInClicked = interactionListener::onZoomInClicked,
+            onZoomOutClicked = interactionListener::onZoomOutClicked,
+            onResetClicked = interactionListener::onResetClicked,
+            modifier = Modifier.padding(top = 12.dp)
+        )
+        SaveButton(
+            onClick = interactionListener::onSaveClicked,
+            modifier = Modifier.padding(top = 12.dp)
+        )
+        UploadAnotherImageButton(
+            onClick = interactionListener::onUploadAnotherImageClicked,
+            modifier = Modifier.padding(top = 12.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DukanImageCropContentPreview() {
+    MenaTheme {
+        DukanImageCropContent(
+            state = CreateDukanUiState(),
+            interactionListener = object : CreateDukanInteractionListener {
+                override fun onClickUploadImage() {}
+
+                override fun onClickEditImage() {}
+
+                override fun onCLickNext() {}
+
+                override fun onSaveClicked() {}
+
+                override fun onZoomInClicked() {}
+
+                override fun onZoomOutClicked() {}
+
+                override fun onResetClicked() {}
+
+                override fun onUploadAnotherImageClicked() {}
+            }
+        )
+    }
+}
