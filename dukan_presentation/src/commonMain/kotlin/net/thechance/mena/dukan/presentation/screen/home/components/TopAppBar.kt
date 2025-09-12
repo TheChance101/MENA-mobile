c5cdd3bb28255567c84abbb41f2abb3d0b564a80
a7med naser <ad0112727@gmail.com>
2025-09-13 00:24:21 +0300

    refactor: use design system AppBar in TopAppBar

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/home/components/TopAppBar.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/home/components/TopAppBar.kt
@@ -1,59 +1,94 @@
 package net.thechance.mena.dukan.presentation.screen.home.components
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
-import dukan_presentation.src.commonMain.composeResources.Res
-import dukan_presentation.src.commonMain.composeResources.ic_add_dukan_icon
+import mena.dukan_presentation.generated.resources.Dukan
+import mena.dukan_presentation.generated.resources.Res
+import mena.dukan_presentation.generated.resources.add_dukan_icon
+import mena.dukan_presentation.generated.resources.dukan_button
+import mena.dukan_presentation.generated.resources.dukan_icon
+import mena.dukan_presentation.generated.resources.ic_add_dukan
+import mena.dukan_presentation.generated.resources.ic_dukan
+import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun TopAppBar(
     modifier: Modifier = Modifier,
     onAddDukanButtonClicked: () -> Unit = {},
+    isDukanPending: Boolean = false
 ) {
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            modifier = Modifier.weight(1f),
-            text = "Dukan",
-        )
-
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(color = White, shape = RoundedCornerShape(12.dp))
-                .clip(shape = RoundedCornerShape(12.dp))
-                .clickable(onClick = onAddDukanButtonClicked),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painter = painterResource(resource = Res.drawable.ic_add_dukan_icon),
-                contentDescription = "add dukan icon"
-            )
+    AppBar(
+        title = stringResource(resource = Res.string.Dukan),
+        titleColor = Theme.colorScheme.shadePrimary,
+        onLeadingClick = onAddDukanButtonClicked,
+        modifier = modifier,
+        trailingContent = {
+            Row {
+                AnimatedContent(
+                    targetState = isDukanPending,
+                    transitionSpec = {
+                        fadeIn()togetherWith(fadeOut())
+                    },
+                    label = stringResource(resource = Res.string.dukan_button)
+                )
+                { isPending ->
+                    Box(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .background(color = Theme.colorScheme.background.surfaceLow, shape = RoundedCornerShape(Theme.spacing._12))
+                            .clip(shape = RoundedCornerShape(Theme.spacing._12))
+                            .clickable(onClick = onAddDukanButtonClicked),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (isPending) {
+                            Icon(
+                                painter = painterResource(resource = Res.drawable.ic_dukan),
+                                contentDescription = stringResource(resource = Res.string.dukan_icon)
+                            )
+                        } else {
+                            Icon(
+                                painter = painterResource(resource = Res.drawable.ic_add_dukan),
+                                contentDescription = stringResource(resource = Res.string.add_dukan_icon)
+                            )
+                        }
+                    }
+                }
+            }
         }
-    }
+    )
 }
 
 @Preview
 @Composable
 fun TopAppBarPreview() {
-    TopAppBar()
+    MenaTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Theme.colorScheme.background.surface),
+            contentAlignment = Alignment.Center
+        ) {
+            TopAppBar()
+        }
+    }
 }
