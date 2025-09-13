4fb3c35c27e7be4f609ae10914cd502acfab72be
nada <nadamansoursamir@gmail.com>
2025-09-13 15:13:35 +0300

    refactor to add HorizontalStyle icons

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/HorizontalStyle.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/HorizontalStyle.kt
@@ -5,19 +5,27 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import mena.dukan_presentation.generated.resources.Res
+import mena.dukan_presentation.generated.resources.ic_add_shopping_basket
+import mena.dukan_presentation.generated.resources.ic_image
+import net.thechance.mena.designsystem.presentation.component.icon.MenaIcon
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HorizontalStyle(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    addToCartColor: Color = Theme.colorScheme.primary.onPrimary
 ) {
     Row(
         modifier = modifier
@@ -32,7 +40,12 @@ fun HorizontalStyle(
                 .padding(4.dp),
             contentAlignment = Alignment.Center
         ) {
-//            MenaIcon()
+            MenaIcon(
+                painter = painterResource(Res.drawable.ic_image),
+                tint = Theme.colorScheme.stroke,
+                contentDescription = "style has image",
+                modifier = Modifier.size(24.dp)
+            )
         }
         Spacer(Modifier.weight(1f))
         Box(
@@ -42,15 +55,20 @@ fun HorizontalStyle(
                 .background(Theme.colorScheme.background.surfaceHigh)
                 .padding(3.dp),
             contentAlignment = Alignment.Center
-        ){
-//            MenaIcon()
+        ) {
+            MenaIcon(
+                painter = painterResource(Res.drawable.ic_add_shopping_basket),
+                tint = addToCartColor,
+                contentDescription = "add shopping basket",
+                modifier = Modifier.size(10.dp)
+            )
         }
     }
 }
 
 @Preview
 @Composable
-private fun HorizontalStylePreview(){
+private fun HorizontalStylePreview() {
     MenaTheme {
         HorizontalStyle()
     }
