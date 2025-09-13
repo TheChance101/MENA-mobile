c1ed1a449289899646bb2fd570439bc4d4932c44
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-13 17:06:09 +0300

    refactor: update CreateDukanUiState

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanUiState.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanUiState.kt
@@ -3,8 +3,8 @@ package net.thechance.mena.dukan.presentation.viewModel.createDukan
 data class CreateDukanUiState(
     val name: String = "",
     val currentStep: Int = 0,
-    val isButtonEnabled: Boolean = true, // TODO: Change this to be default be false
-    val isButtonLoading: Boolean = false
-    val selectedImageUri: Int = 0,
+    val isButtonEnabled: Boolean = true,
+    val isButtonLoading: Boolean = false,
+    val savedImageUri: String? = null,
     val isNextButtonEnabled: Boolean = false
 )
