bab2f3416f351176b38ae749277147d9825508a4
nada <nadamansoursamir@gmail.com>
2025-09-13 15:14:33 +0300

    refactor VerticalStyle ui and add icons

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/VerticalStyle.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/VerticalStyle.kt
@@ -2,24 +2,30 @@ package net.thechance.mena.dukan.presentation.screen.createDukan
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import mena.dukan_presentation.generated.resources.Res
+import mena.dukan_presentation.generated.resources.ic_add_shopping_basket
+import mena.dukan_presentation.generated.resources.ic_image
+import net.thechance.mena.designsystem.presentation.component.icon.MenaIcon
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun VerticalStyle(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    addToCartColor: Color = Theme.colorScheme.primary.onPrimary
 ) {
-    Column(
+    Box(
         modifier = modifier
             .clip(RoundedCornerShape(Theme.radius.xs))
             .background(Theme.colorScheme.background.surface)
@@ -30,21 +36,31 @@ fun VerticalStyle(
                 .clip(RoundedCornerShape(Theme.radius.xxs))
                 .background(Theme.colorScheme.background.surfaceLow)
                 .padding(9.dp),
-//            contentAlignment = Alignment.Center
+            contentAlignment = Alignment.Center
         ) {
-//            MenaIcon()
-            Box(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(Theme.radius.full))
-                    .background(Theme.colorScheme.background.surfaceHigh)
-                    .padding(3.dp)
-                    .align(Alignment.BottomCenter),
-                contentAlignment = Alignment.Center
-            ) {
-//            MenaIcon()
-            }
+            MenaIcon(
+                painter = painterResource(Res.drawable.ic_image),
+                tint = Theme.colorScheme.stroke,
+                contentDescription = "style has image",
+                modifier = Modifier.size(24.dp)
+            )
         }
-
+        Box(
+            modifier = Modifier
+                .padding(top = 34.dp)
+                .align(Alignment.Center)
+                .clip(RoundedCornerShape(Theme.radius.full))
+                .background(Theme.colorScheme.background.surfaceHigh)
+                .padding(3.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            MenaIcon(
+                painter = painterResource(Res.drawable.ic_add_shopping_basket),
+                tint = addToCartColor,
+                contentDescription = "add shopping basket",
+                modifier = Modifier.size(10.dp)
+            )
+        }
     }
 }
 
