f0bcd2eb5280033961b07464c233575495346e63
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-09-13 17:05:41 +0300

    feat: add NavigateNext to CreateDukanEffect

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanEffect.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanEffect.kt
@@ -2,4 +2,5 @@ package net.thechance.mena.dukan.presentation.viewModel.createDukan
 
 sealed interface CreateDukanEffect {
     data object NavigateToImageCropScreen : CreateDukanEffect
+    data object NavigateNext : CreateDukanEffect
 }
