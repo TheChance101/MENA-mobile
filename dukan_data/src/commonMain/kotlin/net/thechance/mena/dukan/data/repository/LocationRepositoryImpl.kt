f9f79c939f71a3db8d1216aa7381906f967b366c
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-09-14 15:04:34 +0300

    feat: created location repository to get the device location.

--- dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/LocationRepositoryImpl.kt
+++ dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/LocationRepositoryImpl.kt
@@ -1,8 +1,37 @@
 package net.thechance.mena.dukan.data.repository
 
+import dev.jordond.compass.Coordinates
+import dev.jordond.compass.Location
+import dev.jordond.compass.geocoder.MobileGeocoder
+import dev.jordond.compass.geocoder.placeOrNull
+import dev.jordond.compass.geolocation.Geolocator
+import dev.jordond.compass.geolocation.currentLocationOrNull
+import dev.jordond.compass.geolocation.mobile
 import net.thechance.mena.dukan.domain.entity.Dukan
 import net.thechance.mena.dukan.domain.repository.LocationRepository
 
-expect class LocationRepositoryImpl: LocationRepository {
-    override suspend fun getCurrentLocation(): Dukan.Location
+class LocationRepositoryImpl : LocationRepository {
+    override suspend fun getCurrentLocation(): Dukan.Coordinates {
+        val geolocator: Geolocator = Geolocator.mobile()
+        val currentLocation: Location? = geolocator.currentLocationOrNull()
+        return currentLocation?.coordinates?.let { current ->
+            Dukan.Coordinates(latitude = current.latitude, longitude = current.longitude)
+        } ?: Dukan.Coordinates(latitude = DEFAULT_LATITUDE, longitude = DEFAULT_LONGITUDE)
+    }
+
+    override suspend fun getCurrentLocationName(
+        coordinates: Dukan.Coordinates,
+    ): String {
+        return MobileGeocoder().placeOrNull(
+            Coordinates(
+                coordinates.latitude,
+                coordinates.longitude
+            )
+        )?.locality.toString()
+    }
+
+    companion object {
+        const val DEFAULT_LATITUDE = 28.0
+        const val DEFAULT_LONGITUDE = 29.5
+    }
 }
--- dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/repository/LocationRepository.kt
+++ dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/repository/LocationRepository.kt
@@ -4,5 +4,6 @@ import net.thechance.mena.dukan.domain.entity.Dukan
 
 interface LocationRepository {
 
-    suspend fun getCurrentLocation(): Dukan.Location
+    suspend fun getCurrentLocation(): Dukan.Coordinates
+    suspend fun getCurrentLocationName(coordinates: Dukan.Coordinates): String
 }
