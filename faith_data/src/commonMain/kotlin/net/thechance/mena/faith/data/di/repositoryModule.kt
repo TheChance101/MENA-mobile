13bc1b74f3a3b21cb6a6285a990704e04a8d32c1
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-09-15 13:53:59 +0200

    create annotaion text component

--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/di/repositoryModule.kt
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/di/repositoryModule.kt
@@ -1,9 +1,5 @@
 package net.thechance.mena.faith.data.di
 
-import net.thechance.mena.faith.data.repository.DummyQuranRepositorySampleImpl
-import net.thechance.mena.faith.domain.repository.QuranRepository
-import org.koin.core.module.dsl.singleOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal val repositoryModule = module {
