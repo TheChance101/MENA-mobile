6222950aa9b2f8232a5862d5cb83fd9836b42f4a
nada <nadamansoursamir@gmail.com>
2025-09-16 17:58:29 +0300

    increase dukan style last row padding

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/content/component/dukanstyle/DukanStyle.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/CreateDukan/content/component/dukanstyle/DukanStyle.kt
@@ -116,7 +116,7 @@ private fun VerticalContent(selectedColor: Color) {
         if (selectedColor == color) 0.dp else Theme.spacing._4
     )
     val lastRowPaddingBottom by animateDpAsState(
-        if (selectedColor == color) 5.dp else 0.dp
+        if (selectedColor == color) 10.dp else 0.dp
     )
     Column {
         SmallImageIcon(
