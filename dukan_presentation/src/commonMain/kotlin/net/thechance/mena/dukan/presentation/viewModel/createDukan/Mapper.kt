c7a87e0220aef9b9cb5c716510e5a17507c93c74
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-09-17 23:56:38 +0300

    refactor: removed unused function in the mapper.

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/Mapper.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/Mapper.kt
@@ -12,16 +12,6 @@ fun List<Category>.toUiState(): List<DukanCategoryUiState> {
         )
     }
 }
-fun Dukan.Coordinates?.toUiState() : CreateDukanUiState.CoordinatesUiState {
-    return this?.let {
-        CreateDukanUiState.CoordinatesUiState(
-            latitude = latitude,
-            longitude = longitude,
-        )
-    } ?: CreateDukanUiState.CoordinatesUiState(
-        28.0, 29.0
-    )
-}
 
 fun CreateDukanUiState.CoordinatesUiState.toEntity() = Dukan.Coordinates(
     latitude = latitude,
