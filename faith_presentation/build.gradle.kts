52e84596c365a3d8310b03ce6cf3879ad7109740
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-09-18 01:29:58 +0300

    feat: Add navigation.compose dependency

--- faith_presentation/build.gradle.kts
+++ faith_presentation/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
+            implementation(libs.androidx.navigation.compose)
         }
         iosMain.dependencies {
 
