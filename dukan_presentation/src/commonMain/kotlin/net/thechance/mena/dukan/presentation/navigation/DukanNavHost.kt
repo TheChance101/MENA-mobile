7fc0c26b471e972843a2acd612012281090b7202
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-09-17 23:57:48 +0300

    refactor: set MainScreenRoute for DukanNavHost as Start Destination.

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/navigation/DukanNavHost.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/navigation/DukanNavHost.kt
@@ -15,7 +15,7 @@ fun DukanNavHost() {
     ) {
         NavHost(
             navController = navController,
-            startDestination = DukanRoute.CreateDukanScreenRoute,
+            startDestination = DukanRoute.MainScreenRoute,
         ) {
             composable<DukanRoute.MainScreenRoute> {
                 //  MainScreen()
