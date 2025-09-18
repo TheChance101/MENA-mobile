5b67b0ccc18b5991ced84dae5c7be2eca1e5f542
fatmahgazy <143911324+fatmahgazy@users.noreply.github.com>
2025-09-18 09:22:19 +0200

    FIH-10: add copy functionality with use clip board, use them quran font , avoid use any thing related with ui in view model and rename component

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/di/faithPresentationModule.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/di/faithPresentationModule.kt
@@ -4,5 +4,4 @@ import org.koin.dsl.module
 
 val faithPresentationModule = module {
     includes(viewModelModule)
-
 }
--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/di/viewModelModule.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/di/viewModelModule.kt
@@ -1,19 +1,7 @@
 package net.thechance.mena.faith.presentation.di
 
-import net.thechance.mena.faith.presentation.feature.quran.sur.SurViewModel
-import net.thechance.mena.faith.presentation.feature.quran.surah.SurahViewModel
-import org.koin.core.module.dsl.viewModel
-import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 internal val viewModelModule = module {
-    viewModelOf(::SurViewModel)
-    viewModel { (surahId: Int, surahName: String) ->
-        SurahViewModel(
-            quranRepository = get(),
-            surahId = surahId,
-            surahName = surahName
-        )
-    }
 
 }
