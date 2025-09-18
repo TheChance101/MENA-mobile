dacf07dfa5c73e2bb200e4e3ddbc608304f004f3
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-09-18 17:22:27 +0300

    include main  viewModel in di

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/di/DukanViewModelModule.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/di/DukanViewModelModule.kt
@@ -1,7 +1,9 @@
 package net.thechance.mena.dukan.presentation.di
 
+import net.thechance.mena.dukan.presentation.viewModel.mainScreen.MainViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 internal val dukanViewModelModule = module {
-
+    viewModelOf(::MainViewModel)
 }
