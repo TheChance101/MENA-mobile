54194311864f9199c5e6d30e9c8481360491ed46
Fares <dev.faresmohamed@gmail.com>
2025-09-19 15:42:52 +0300

    FTH-27: add ObserveAsEffect composable for flow collection

---
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/base/ObserveAsEffect.kt
@@ -0,0 +1,17 @@
+package net.thechance.mena.faith.presentation.base
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> ObserveAsEffect(
+    effect: Flow<T>,
+    onEvent: (T) -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    LaunchedEffect(key1 = lifecycleOwner.lifecycle) {
+        effect.collect(onEvent)
+    }
+}
