2df223fabf2fe9155d3271c5bb79cfdb856650f3
Hasan Ali <ihasanalihasan@gmail.com>
2025-09-19 21:02:32 +0300

    FTH-24: add HttpClientProvider with base configuration for Ktor

---
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/remote/client/HttpClientProvider.kt
@@ -0,0 +1,42 @@
+package net.thechance.mena.faith.data.remote.client
+
+
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class HttpClientProvider {
+
+    fun create(): HttpClient {
+        return HttpClient {
+            configureBaseSettings()
+        }
+    }
+
+    private fun HttpClientConfig<*>.configureBaseSettings() {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                isLenient = true
+                explicitNulls = false
+            })
+        }
+
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 20_000
+            connectTimeoutMillis = 20_000
+            socketTimeoutMillis = 20_000
+        }
+
+    }
+
+}
