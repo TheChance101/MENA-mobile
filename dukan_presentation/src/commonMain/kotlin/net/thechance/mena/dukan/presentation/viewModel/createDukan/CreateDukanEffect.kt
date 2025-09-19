af8c1d90f4c09dddb3bbb0405c3130d97447afc9
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-09-19 11:42:47 +0300

    feature: add new effect to navigate the pending screen.

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanEffect.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/createDukan/CreateDukanEffect.kt
@@ -3,4 +3,5 @@ package net.thechance.mena.dukan.presentation.viewModel.createDukan
 sealed interface CreateDukanEffect {
     data object NavigateNext : CreateDukanEffect
     data object NavigateBack : CreateDukanEffect
+    data class NavigateToPending(val name: String): CreateDukanEffect
 }
