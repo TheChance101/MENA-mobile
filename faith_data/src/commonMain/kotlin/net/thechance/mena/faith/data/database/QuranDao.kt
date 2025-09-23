ac266ac699a23ab5c612c934459346123d6cf9e9
Fares <dev.faresmohamed@gmail.com>
2025-09-23 22:13:40 +0300

    FTH-29: add QuranDao and QuranDatabase for Room database integration

---
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/database/QuranDao.kt
@@ -0,0 +1,46 @@
+package net.thechance.mena.faith.data.database
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface QuranDao {
+    @Query(
+        """
+    SELECT *
+    FROM ayat 
+    WHERE sura_no = :surahNumber
+"""
+    )
+    suspend fun getAyatOfSurah(surahNumber: Int): List<AyahDto>
+
+
+    @Query(
+        """SELECT 
+        sura_no,
+        sura_name_en,
+        COUNT(*) as ayahCount
+    FROM ayat 
+    GROUP BY sura_no, sura_name_en
+"""
+    )
+    suspend fun getAllSur(): List<SurahDto>
+
+    @Query(
+        """
+        SELECT aya_text 
+        FROM ayat 
+        WHERE sura_no = :surahId AND aya_no = :ayahNumber
+        LIMIT 1
+    """
+    )
+    suspend fun getAyahContent(ayahNumber: Int, surahId: Int): String
+
+
+    @Query("SELECT * FROM ayat WHERE aya_no = :ayahId AND sura_no = :surahId")
+    suspend fun getAyah(ayahId: Int, surahId: Int): AyahDto
+
+    @Query("SELECT * FROM ayat WHERE sura_no = :surahId")
+    suspend fun getSurah(surahId: Int): SurahDto
+
+}
