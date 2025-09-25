0c5cfea39eef4842b10008ad6c69c6f6a4493435
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-09-25 17:13:52 +0300

    TRN /feat : implement updateReelById

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/repository/ReelsRepositoryImpl.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/repository/ReelsRepositoryImpl.kt
@@ -4,8 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import net.thechance.mena.trends.data.dto.ReelDto
 import net.thechance.mena.trends.data.dto.RemotePaginationResponse
+import net.thechance.mena.trends.data.dto.UpdateReelRequestDTO
 import net.thechance.mena.trends.data.mapper.toEntity
 import net.thechance.mena.trends.data.util.NetworkConstants.PAGE_PARAMETER
 import net.thechance.mena.trends.data.util.NetworkConstants.REELS_ENDPOINT
@@ -17,7 +22,7 @@ import org.koin.core.annotation.Provided
 import org.koin.core.annotation.Single
 
 @Single(binds = [ReelsRepository::class])
-internal class ReelsRepositoryImpl(
+class ReelsRepositoryImpl(
     @Provided private val httpClient: HttpClient
 ) : ReelsRepository {
 
@@ -33,5 +38,19 @@ internal class ReelsRepositoryImpl(
                 parameter(PAGE_PARAMETER, pageNumber)
             }
         }.results?.mapNotNull { it.toEntity() } ?: emptyList()
+    }
+
+    override suspend fun updateReelById(
+        id: String,
+        description: String,
+        categoryIds: List<String>
+    ): Reel {
+        val request = UpdateReelRequestDTO(description, categoryIds)
+        return safeApiCall<ReelDto> {
+            httpClient.put("$TRENDS_PATH/$REELS_ENDPOINT/$id") {
+                contentType(ContentType.Application.Json)//TODO dont know if this part is necessary
+                setBody(request)
+            }
+        }.toEntity() ?: throw Exception("Update failed")//TODO dont know if it okey if i throw exception and what it will replace it
     }
 }
