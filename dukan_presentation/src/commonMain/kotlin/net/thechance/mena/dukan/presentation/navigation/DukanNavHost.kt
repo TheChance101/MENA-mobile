6a97675e44a9d6ddf5715911e08b41e57612d225
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-09-26 19:41:30 +0300

    feat: add new screens to navigation graph

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/navigation/DukanNavHost.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/navigation/DukanNavHost.kt
@@ -6,7 +6,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import net.thechance.mena.dukan.presentation.screen.approvedDukan.ApprovedDukanScreen
 import net.thechance.mena.dukan.presentation.screen.createDukan.CreateDukanScreen
+import net.thechance.mena.dukan.presentation.screen.createShelf.CreateShelfScreen
 import net.thechance.mena.dukan.presentation.screen.main.MainScreen
 import net.thechance.mena.dukan.presentation.screen.pendingDukan.PendingDukanScreen
 
@@ -27,6 +29,15 @@ fun DukanNavHost() {
             composable<DukanRoute.CreateDukanScreenRoute> {
                 CreateDukanScreen()
             }
+            
+            composable<DukanRoute.CreateShelfScreenRoute> {
+                CreateShelfScreen()
+            }
+            
+            composable<DukanRoute.ApprovedDukanScreenRoute> {
+                ApprovedDukanScreen()
+            }
+            
             composable<DukanRoute.MyDukanScreenRoute> {
                 // MyDukanScreen()
             }
