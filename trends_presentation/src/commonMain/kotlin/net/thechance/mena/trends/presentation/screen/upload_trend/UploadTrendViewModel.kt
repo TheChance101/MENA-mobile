d6a2ed8f3710cefad5ead274d192bd029e8d3426
Ahmed Mahgoub <aed7862@gmail.com>
2025-09-27 00:27:02 +0300

    refactor: make `viewModelScope` default in base `tryToCollectFlow` function

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/upload_trend/UploadTrendViewModel.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/upload_trend/UploadTrendViewModel.kt
@@ -69,8 +69,7 @@ internal class UploadTrendViewModel(
             onStart = ::onUploadStarted,
             onEach = ::onCollectEachFlow,
             onError = ::onUploadError,
-            onComplete = ::onUploadCompleted,
-            scope = viewModelScope
+            onComplete = ::onUploadCompleted
         )
     }
 
