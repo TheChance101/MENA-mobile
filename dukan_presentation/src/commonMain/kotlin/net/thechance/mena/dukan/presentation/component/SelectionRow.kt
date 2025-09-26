fbf8dbb76bf35e234bda04269ce0aba013316d2a
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-09-26 23:07:35 +0300

    refactor: simplify painter assignment in SelectionRow

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/SelectionRow.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/SelectionRow.kt
@@ -53,11 +53,9 @@ private fun <T> SelectionChip(
 ) {
     Chip(
         text = getItemName(item),
-        painter = if (getItemImageUrl(item).isNotEmpty()) {
-            rememberAsyncImagePainter(getItemImageUrl(item))
-        } else {
-            null // No image for shelves
-        },
+        painter = if (getItemImageUrl(item).isNotEmpty()) rememberAsyncImagePainter(
+            getItemImageUrl(item)
+        ) else null,
         isSelected = isSelected,
         isEnabled = isEnabled,
         modifier = Modifier,
