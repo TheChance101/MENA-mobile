f8b83bef8b660008aad38cdca83ba81a03b61eaf
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-09-27 13:54:48 +0300

    refactor: remove IconBackground, inline icon with background

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/productCard/EditProductIcon.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/productCard/EditProductIcon.kt
@@ -1,12 +1,17 @@
 package net.thechance.mena.dukan.presentation.component.productCard
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import mena.dukan_presentation.generated.resources.Res
 import mena.dukan_presentation.generated.resources.edit_product_pencil
+import net.thechance.mena.designsystem.presentation.component.icon.Icon
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -15,13 +20,16 @@ fun EditProductIcon(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    IconBackground(
-        drawableResource = Res.drawable.edit_product_pencil,
+    Icon(
+        painter = painterResource(Res.drawable.edit_product_pencil),
         contentDescription = stringResource(Res.string.edit_product_pencil),
-        backgroundColor = Theme.colorScheme.primary.primary,
-        iconModifier = Modifier.padding(Theme.spacing._8),
-        boxModifier = modifier,
-        onClick = onClick,
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .background(
+                color = Theme.colorScheme.primary.primary,
+                shape = RoundedCornerShape(size = Theme.radius.full)
+            )
+            .padding(Theme.spacing._8)
     )
 }
 
