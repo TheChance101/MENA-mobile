58b7cff25e1893ac59a46fc660c1d1c7f499cc5b
Menna Srour <mennasrour441@gmail.com>
2025-09-27 21:19:45 +0300

    refactor: remove prefix transaction from screen state

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryScreen.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryScreen.kt
@@ -39,7 +39,6 @@ import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
-
 @Composable
 fun TransactionHistoryContent(
     state: List<TransactionHistoryScreenState>,
