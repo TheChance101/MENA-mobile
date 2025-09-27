f4ac4df68408ea7f2037c3780797da5405369e68
Ahmed Mahgoub <aed7862@gmail.com>
2025-09-27 17:58:54 +0300

    refactor: create exception for VideoValidation exceptions

--- trends_domain/src/commonMain/kotlin/net/thechance/mena/trends/domain/exception/TrendsException.kt
+++ trends_domain/src/commonMain/kotlin/net/thechance/mena/trends/domain/exception/TrendsException.kt
@@ -4,5 +4,6 @@ sealed class TrendsException(message: String = ""): Exception(message)
 
 class NoInternetException(message: String = ""): TrendsException(message)
 
-class MaxFileSizeExceededException(message: String = ""): TrendsException(message)
-class MaxFileDurationExceededException(message: String = ""): TrendsException(message)
+open class VideoValidationException(message: String = ""): TrendsException(message)
+class MaxFileSizeExceededException(message: String = ""): VideoValidationException(message)
+class MaxFileDurationExceededException(message: String = ""): VideoValidationException(message)
