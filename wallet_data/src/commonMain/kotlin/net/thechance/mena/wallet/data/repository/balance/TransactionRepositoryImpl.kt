6ca9878f43be2b2c1010c250ae9e1686aead48be
Menna Srour <mennasrour441@gmail.com>
2025-09-27 23:51:24 +0300

    feat: add Repo Interface and fake repo Implementation

---
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/TransactionRepositoryImpl.kt
@@ -0,0 +1,72 @@
+package net.thechance.mena.wallet.data.repository.balance
+
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import net.thechance.mena.wallet.domain.entity.Transaction
+import net.thechance.mena.wallet.domain.repository.TransactionRepository
+import kotlin.time.Clock
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+import kotlinx.datetime.*
+import kotlinx.datetime.format.*
+import kotlinx.datetime.format.FormatStringsInDatetimeFormats
+
+@OptIn(FormatStringsInDatetimeFormats::class)
+fun formatTransactionDateTime(dateTime: LocalDateTime): String {
+    val formatter = LocalDateTime.Format {
+        byUnicodePattern("dd MMM yyyy HH:mm") // مثال: 27 Sep 2025 14:45
+    }
+    return formatter.format(dateTime)
+}
+
+class TransactionRepositoryImpl(): TransactionRepository {
+        @OptIn(ExperimentalUuidApi::class)
+        override suspend fun getTransactionHistory(): List<Transaction> {
+
+            return listOf(
+                Transaction(
+                    id = Uuid.random(),
+                    createdAt =LocalDateTime(
+                        date = LocalDate(2025, 8, 20),
+                        time = LocalTime(12, 0)
+                    ),
+                    amount = 120.0,
+                    status = Transaction.Status.SUCCESS,
+                    senderId = Uuid.random(),
+                    senderName = "Alice",
+                    receiverId = Uuid.random(),
+                    receiverName = "Bob",
+                    type = Transaction.Type.SENT
+                ),
+                Transaction(
+                    id = Uuid.random(),
+                    createdAt = LocalDateTime(
+                        date = LocalDate(2025, 8, 20),
+                        time = LocalTime(12, 0)
+                    ),
+                    amount = 75.5,
+                    status = Transaction.Status.FAIL,
+                    senderId = Uuid.random(),
+                    senderName = "Charlie",
+                    receiverId = Uuid.random(),
+                    receiverName = "You",
+                    type = Transaction.Type.RECEIVED
+                ),
+                Transaction(
+                    id = Uuid.random(),
+                    createdAt = LocalDateTime(
+                        date = LocalDate(2025, 8, 20),
+                        time = LocalTime(12, 0)
+                    ),
+                    amount = 200.0,
+                    status = Transaction.Status.SUCCESS,
+                    senderId = Uuid.random(),
+                    senderName = "Online Shop",
+                    receiverId = Uuid.random(),
+                    receiverName = "You",
+                    type = Transaction.Type.ONLINE_PURCHASE
+                )
+            )
+        }
+    }
