abd575e130dffdde68167db6cccc7aa6d42aa7aa
Ahmed Mahgoub <aed7862@gmail.com>
2025-09-27 21:15:57 +0300

    test: add `UploadTrendViewModel` unit tests

---
+++ trends_presentation/src/commonTest/kotlin/net/thechance/mena/trends/presentation/utils/TestExtensions.kt
@@ -0,0 +1,25 @@
+package net.thechance.mena.trends.presentation.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+@OptIn(ExperimentalCoroutinesApi::class)
+open class TestExtensions(
+    val testDispatcher: TestDispatcher = StandardTestDispatcher(),
+) {
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+}
