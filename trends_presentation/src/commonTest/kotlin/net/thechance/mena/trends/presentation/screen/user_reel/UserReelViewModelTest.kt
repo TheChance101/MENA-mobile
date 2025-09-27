d0c180b7c46b8777f611611f832b9b6683a7e586
norhanadel <norhanadel367@gmail.com>
2025-09-27 13:42:21 +0300

    edit unit test for UserReelViewModel

--- trends_presentation/src/commonTest/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelViewModelTest.kt
+++ trends_presentation/src/commonTest/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelViewModelTest.kt
@@ -29,7 +29,9 @@ import kotlin.test.assertTrue
 class UserReelViewModelTest {
 
     private val testDispatcher = StandardTestDispatcher()
-    private lateinit var viewModel: UserReelViewModel
+    private val viewModel by lazy {
+        UserReelViewModel(userReelArgs, mockReelsRepository,testDispatcher)
+    }
     private val mockReelsRepository: ReelsRepository = mock(MockMode.autofill)
     private val userReelArgs: UserReelArgs = mock(MockMode.autofill)
 
@@ -42,16 +44,13 @@ class UserReelViewModelTest {
 
         everySuspend { mockReelsRepository.deleteReelById("1") }
 
-        viewModel = UserReelViewModel(userReelArgs, mockReelsRepository, testDispatcher)
     }
 
     @Test
     fun `should initialize UserReelUiState with default state`() = runTest {
-        // When
         viewModel.state.test {
             val initialState = awaitItem()
 
-            // Then
             assertFalse(initialState.isLoading)
             assertNull(initialState.error)
             assertEquals("Istanbul Squad", initialState.username)
@@ -64,10 +63,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should expand description when onDescriptionClick is called with collapsed state`() = runTest {
-        // When
         viewModel.onDescriptionClick(isCollapsed = false)
 
-        // Then
          viewModel.state.test {
             val state = awaitItem()
             assertTrue(state.isDescriptionExpanded)
@@ -77,11 +74,9 @@ class UserReelViewModelTest {
 
     @Test
     fun `should collapse description when onDescriptionClick is called with expanded state`() = runTest {
-        // When
         viewModel.onDescriptionClick(isCollapsed = true)
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         viewModel.state.test {
             val state = awaitItem()
             assertFalse(state.isDescriptionExpanded)
@@ -91,10 +86,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should send NavigateBack effect when onBackClick is called`() = runTest {
-        // When
         viewModel.onBackClick()
 
-        // Then
         viewModel.effect.test {
             val effect = awaitItem()
             assertTrue(effect is UserReelEffect.NavigateBack)
@@ -104,10 +97,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should show confirmation dialog when onDeleteClick is called`() = runTest {
-        // When
         viewModel.onDeleteClick()
 
-        // Then
         viewModel.state.test {
             val state = awaitItem()
             assertTrue(state.isConfirmationDialogVisible)
@@ -117,10 +108,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should dismiss confirmation dialog when onDismissConfirmationDialog is called`() = runTest {
-        // When
         viewModel.onDeleteClick()
 
-        // Then
         viewModel.onDismissConfirmationDialog()
         viewModel.state.test {
             val state = awaitItem()
@@ -131,10 +120,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should delete reel successfully when onConfirmDeleteClick is called`() = runTest {
-        // When
         viewModel.onConfirmDeleteClick()
 
-        // Then
         viewModel.state.test {
             skipItems(1)
             val state2 = awaitItem()
@@ -148,10 +135,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should dismiss success dialog when onDismissSuccessDialog is called`() = runTest {
-        // When
         viewModel.onDismissSuccessDialog()
 
-        // Then
         viewModel.state.test {
             val state = awaitItem()
             assertNull(state.isReelDeleted)
@@ -162,10 +147,8 @@ class UserReelViewModelTest {
 
     @Test
     fun `should dismiss error dialog when onDismissErrorDialog is called`() = runTest {
-        // When
         viewModel.onDismissErrorDialog()
 
-        // Then
         viewModel.state.test {
             val state = awaitItem()
             assertFalse(state.isConfirmationDialogVisible)
@@ -175,15 +158,12 @@ class UserReelViewModelTest {
 
     @Test
     fun `onConfirmDeleteClick should update error state when repository throws exception`() = runTest {
-        // Given
         val errorMessage = "Delete failed"
         everySuspend { mockReelsRepository.deleteReelById("1") } throws Exception(errorMessage)
 
-        // When
         viewModel.onConfirmDeleteClick()
         testDispatcher.scheduler.advanceUntilIdle()
 
-        // Then
         viewModel.state.test {
             val errorState = awaitItem()
             assertNotNull(errorState.error is ErrorState)
