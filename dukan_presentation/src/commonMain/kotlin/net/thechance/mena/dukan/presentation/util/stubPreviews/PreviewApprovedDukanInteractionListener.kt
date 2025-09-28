9be3313bc83462c1e7e3e89ca08bd5020874e842
nada <nadamansoursamir@gmail.com>
2025-09-28 22:47:26 +0300

    feat: add missing methods to PreviewApprovedDukanInteractionListener

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/stubPreviews/PreviewApprovedDukanInteractionListener.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/stubPreviews/PreviewApprovedDukanInteractionListener.kt
@@ -2,11 +2,21 @@ package net.thechance.mena.dukan.presentation.util.stubPreviews
 
 import net.thechance.mena.dukan.domain.entity.Product
 import net.thechance.mena.dukan.domain.entity.Shelf
+import net.thechance.mena.dukan.presentation.component.SnackBarType
 import net.thechance.mena.dukan.presentation.viewModel.approvedDukan.ApprovedDukanInteractionListener
+import org.jetbrains.compose.resources.StringResource
 
 object PreviewApprovedDukanInteractionListener : ApprovedDukanInteractionListener {
     override fun onBackButtonClicked() {}
     override fun onDismissSnackBar() {}
+    override fun showSnackBar(
+        message: StringResource,
+        type: SnackBarType
+    ) {}
+
+    override fun onDismissDeleteShelfConfirmationDialog(){}
+    override fun onShowDeleteShelfConfirmationDialog() {}
+    override fun deleteShelf(shelfId: String) {}
     override fun onAddProductClicked() {}
     override fun onEditShelfClicked() {}
     override fun onAddShelfClicked() {}
