f37543ac9fb0b39d474f0d17952265bcf2c2bf25
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-09-28 20:33:25 +0300

    Refactor: Format code in TrendsNavGraph

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/navigation/TrendsNavGraph.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/navigation/TrendsNavGraph.kt
@@ -26,52 +26,53 @@ fun TrendsNavHost() {
 
     val navController = rememberNavController()
 
-   CompositionLocalProvider(
-      LocalNavController provides navController
-   ) {
-      NavHost(
-         modifier = Modifier.fillMaxSize(),
-         navController = navController,
-         startDestination = Route.MainContainer,
-      ) {
+    CompositionLocalProvider(
+        LocalNavController provides navController
+    ) {
+        NavHost(
+            modifier = Modifier.fillMaxSize(),
+            navController = navController,
+            startDestination = Route.MainContainer,
+        ) {
 
-         composable<Route.MainContainer> {
-            MainContainerScreen()
-         }
+            composable<Route.MainContainer> {
+                MainContainerScreen()
+            }
 
-         composable<Route.Categories> {
-            CategoryPickScreen()
-         }
+            composable<Route.Categories> {
+                CategoryPickScreen()
+            }
 
-         composable<Route.ManageReels> {
-            ManageTrendsScreen()
-         }
+            composable<Route.ManageReels> {
+                ManageTrendsScreen()
+            }
 
-         composable<Route.ReelDetails> {
-           UserReelScreen()
-         }
+            composable<Route.ReelDetails> {
+                UserReelScreen()
+            }
 
-         composable<Route.Trends> {
-             // TODO: Just a placeholder for navigation until its user story
-             Text(
-                 text = "Trends Screen",
-                 style = Theme.typography.headline.large,
-                 color = Theme.colorScheme.primary.primary,
-                 textAlign = TextAlign.Center,
-                 modifier = Modifier
-                     .fillMaxSize()
-                     .padding(top = 100.dp)
-             )
-         }
-         composable<Route.VideoDescription> {
-           VideoDescriptionScreen()
-         }
+            composable<Route.Trends> {
+                // TODO: Just a placeholder for navigation until its user story
+                Text(
+                    text = "Trends Screen",
+                    style = Theme.typography.headline.large,
+                    color = Theme.colorScheme.primary.primary,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 100.dp)
+                )
+            }
 
-         composable<Route.CategoriesPublish> {
-           CategoryPublishScreen()
-         }
-      }
-   }
+            composable<Route.VideoDescription> {
+                VideoDescriptionScreen()
+            }
+
+            composable<Route.CategoriesPublish> {
+                CategoryPublishScreen()
+            }
+        }
+    }
 }
 
 val LocalNavController = compositionLocalOf<NavController> {
