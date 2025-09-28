31a272cf88eb856f697a681a8d13b64f642fb41a
Ahmed Mahgoub <aed7862@gmail.com>
2025-09-28 20:22:32 +0300

    refactor: add VideoDurationExtractorImpl for android and ios

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/util/video_util/VideoDurationExtractor.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/util/video_util/VideoDurationExtractor.kt
@@ -1,5 +1,7 @@
 package net.thechance.mena.trends.presentation.shared.util.video_util
 
+expect fun getVideoDurationExtractor(): VideoDurationExtractor
+
 interface VideoDurationExtractor {
     suspend fun getDuration(videoBytes: ByteArray): Long?
 }
