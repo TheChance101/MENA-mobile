a9eef83eb145df2317a5eba28e418c117d449659
Menna Srour <mennasrour441@gmail.com>
2025-09-29 14:24:47 +0300

    refactor: move every composable to seperate file

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/component/TransactionhistoryCard.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/component/TransactionhistoryCard.kt
@@ -1,39 +1,22 @@
 package net.thechance.mena.wallet.presentation.screen.transaction_history.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import mena.wallet_presentation.generated.resources.Res
-import mena.wallet_presentation.generated.resources.failed
-import mena.wallet_presentation.generated.resources.ic_failed
 import mena.wallet_presentation.generated.resources.ic_shopping_bag
-import mena.wallet_presentation.generated.resources.img_silver
-import mena.wallet_presentation.generated.resources.money_icon
 import mena.wallet_presentation.generated.resources.transaction_pay
-import mena.wallet_presentation.generated.resources.transaction_type_icon
-import net.thechance.mena.designsystem.presentation.component.icon.Icon
-import net.thechance.mena.designsystem.presentation.component.text.Text
-import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.wallet.domain.entity.Transaction
 import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -57,7 +40,7 @@ fun TransactionHistoryCard(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        TransactionIcon(
+        TransactionStatusIcon(
             transactionTypeIcon = transactionTypeIcon,
             transactionStatus = transactionStatus
         )
@@ -71,146 +54,6 @@ fun TransactionHistoryCard(
             amount = amount,
             transactionStatus = transactionStatus,
             contactName = contactName
-        )
-    }
-}
-
-@Composable
-private fun TransactionIcon(
-    transactionTypeIcon: DrawableResource,
-    transactionStatus: Transaction.Status,
-) {
-    Box(
-        modifier = Modifier.height(64.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .background(Theme.colorScheme.primary.onPrimary, CircleShape)
-                .padding(12.dp)
-                .align(Alignment.Center)
-        ) {
-            Icon(
-                painter = painterResource(transactionTypeIcon),
-                contentDescription = stringResource(Res.string.transaction_type_icon),
-                modifier = Modifier.size(24.dp)
-            )
-        }
-
-        if (transactionStatus == Transaction.Status.FAIL) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_failed),
-                contentDescription = stringResource(Res.string.failed),
-                modifier = Modifier.size(20.dp)
-                    .align(Alignment.BottomCenter)
-            )
-        }
-    }
-
-}
-
-@Composable
-private fun TransactionContent(
-    transactionTitle: String,
-    transactionTimeAndDate: String,
-    amount: String,
-    transactionStatus: Transaction.Status,
-    modifier: Modifier = Modifier,
-    contactName: String? = null
-) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = if (transactionStatus == Transaction.Status.FAIL) Arrangement.spacedBy(4.dp) else Arrangement.spacedBy(8.dp)
-    ) {
-        TransactionTitleAndAmount(
-            transactionTitle = transactionTitle,
-            amount = amount,
-            contactName = contactName
-        )
-        if (transactionStatus == Transaction.Status.FAIL) {
-            Text(
-                modifier = Modifier.align(Alignment.Start),
-                text = stringResource(Res.string.failed),
-                style = Theme.typography.label.small,
-                color = Theme.colorScheme.error
-            )
-        }
-        Text(
-            modifier = Modifier.align(Alignment.Start),
-            text = transactionTimeAndDate,
-            style = Theme.typography.label.small,
-            color = Theme.colorScheme.shadeSecondary
-        )
-    }
-}
-
-@Composable
-private fun TransactionTitleAndAmount(
-    transactionTitle: String,
-    amount: String,
-    modifier: Modifier = Modifier,
-    contactName: String? = null
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Row(
-            modifier = Modifier.weight(1f, fill = false),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = transactionTitle,
-                style = Theme.typography.body.small,
-                color = Theme.colorScheme.shadePrimary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-
-            contactName?.let { name ->
-                Text(
-                    text = name,
-                    style = Theme.typography.body.small,
-                    color = Theme.colorScheme.shadePrimary,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    fontWeight = SemiBold
-                )
-            }
-        }
-
-        TransactionAmountRow(
-            modifier = Modifier
-                .align(Alignment.CenterVertically)
-                .padding(start = 8.dp),
-            amount = amount
-        )
-    }
-}
-
-@Composable
-private fun TransactionAmountRow(
-    amount: String,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.End
-    ) {
-        Text(
-            modifier = Modifier.padding(end = 4.dp),
-            text = amount,
-            style = Theme.typography.label.medium,
-            color = Theme.colorScheme.shadePrimary
-        )
-        Icon(
-            modifier = Modifier.size(16.dp),
-            painter = painterResource(Res.drawable.img_silver),
-            contentDescription = stringResource(Res.string.money_icon)
         )
     }
 }
