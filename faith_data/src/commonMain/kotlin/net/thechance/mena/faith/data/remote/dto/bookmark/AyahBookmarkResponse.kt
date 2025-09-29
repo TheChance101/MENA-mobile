273ba0763e71c2e162acd7fc55966de0f9b93e9f
Yusuf Nasser <yusufnassereng@gmail.com>
2025-09-29 17:58:06 +0300

    FTH-42: Remove unused AyahBookmarkResponse

--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/remote/dto/bookmark/AyahBookmarkResponse.kt
+++
@@ -1,9 +0,0 @@
-package net.thechance.mena.faith.data.remote.dto.bookmark
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AyahBookmarkResponse(
-    @SerialName("content") val ayahBookmarks: List<AyahBookmarkDto>
-)
