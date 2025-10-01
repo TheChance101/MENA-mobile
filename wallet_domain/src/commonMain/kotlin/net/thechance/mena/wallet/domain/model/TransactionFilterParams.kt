a25d01a8871ee74f92472e27c6e76a728342f077
Menna Srour <mennasrour441@gmail.com>
2025-10-01 21:03:02 +0300

    refactor: make value of page and page Size not null

--- wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/model/TransactionFilterParams.kt
+++ wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/model/TransactionFilterParams.kt
@@ -3,8 +3,8 @@ package net.thechance.mena.wallet.domain.model
 import kotlinx.datetime.LocalDate
 
 data class TransactionFilterParams(
-    val page: Int?=null,
-    val pageSize: Int?=null,
+    val page: Int=1,
+    val pageSize: Int=20,
     val types: List<TransactionType>? = null,
     val status: TransactionStatus? = null,
     val startDate: LocalDate? = null,
