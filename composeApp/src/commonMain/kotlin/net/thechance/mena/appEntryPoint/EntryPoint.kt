fe2f40075b113e44190e7704800130568b7666ec
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-10-01 19:39:32 +0300

    Refactor EntryPoint and remove cleartext traffic

--- composeApp/src/commonMain/kotlin/net/thechance/mena/appEntryPoint/EntryPoint.kt
+++ composeApp/src/commonMain/kotlin/net/thechance/mena/appEntryPoint/EntryPoint.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import net.thechance.mena.core_chat.api.CoreChatApi
 import net.thechance.mena.designsystem.presentation.component.bottomNavigation.BottomNavigationBar
@@ -28,7 +29,7 @@ import org.koin.compose.koinInject
 
 
 @Composable
-fun EntryPoint() {
+fun EntryPoint(){
     val identityApi = koinInject<IdentityFeatureApi>()
     val authorizationService = koinInject<AuthorizationService>()
 
@@ -42,7 +43,7 @@ fun EntryPoint() {
 }
 
 @Composable
-private fun LoggedInContainer() {
+private fun LoggedInContainer(){
     var activeFeature: Feature by remember { mutableStateOf(Feature.CHAT) }
     Column(
         Modifier
@@ -61,7 +62,7 @@ private fun LoggedInContainer() {
 }
 
 @Composable
-private fun ColumnScope.FeatureContent(activeFeature: Feature) {
+private fun ColumnScope.FeatureContent(activeFeature: Feature){
     val identityApi = koinInject<IdentityFeatureApi>()
     val dukanApi = koinInject<DukanApi>()
     val trendsApi = koinInject<TrendsApi>()
@@ -70,7 +71,7 @@ private fun ColumnScope.FeatureContent(activeFeature: Feature) {
     val walletApi = koinInject<WalletApi>()
 
     Box(Modifier.weight(1f)) {
-        when (activeFeature) {
+        when(activeFeature){
             Feature.CHAT -> chatApi.TabEntry()
             Feature.DUKAN -> dukanApi.TabEntry()
             Feature.TREND -> trendsApi.TabEntry()
@@ -81,13 +82,13 @@ private fun ColumnScope.FeatureContent(activeFeature: Feature) {
     }
 }
 
-private enum class Feature {
+private enum class Feature{
     CHAT, DUKAN, TREND, FAITH, PROFILE, WALLET
 }
 
 //TODO: refactor this work around with refactoring bottom navbar
-private fun BottomNavigationItem.toFeature(): Feature {
-    return when (this.title) {
+private fun BottomNavigationItem.toFeature(): Feature{
+    return when(this.title) {
         "Home" -> Feature.CHAT
         "Dukan" -> Feature.DUKAN
         "Trends" -> Feature.TREND
