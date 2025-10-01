bb48ffa6e3ea7845841cfad579c2cf0badf0f780
Marwan Qashwa <marawankashwa24@gmail.com>
2025-10-01 22:04:17 +0300

    feat: update Surah.kt hash and clarify line ending sensitivity

--- faith_domain/src/jvmTest/kotlin/net/thechance/mena/faith/domain/entity/SurahTest.kt
+++ faith_domain/src/jvmTest/kotlin/net/thechance/mena/faith/domain/entity/SurahTest.kt
@@ -14,11 +14,15 @@ class SurahTest {
         assertEquals(
             FILE_HASH,
             actualHash,
-            message = "Surah.kt file has been modified! new hash:${actualHash}\n Please check the changes."
+            message = "Surah.kt file has been modified! actual hash:${actualHash}\n Please check the changes."
         )
     }
 
     private companion object {
+        // NOTE: This MD5 hash is calculated from the Surah.kt file with LF line endings.
+        // Do not change unless Surah.kt itself is intentionally modified.
+        // If you run the test locally on Windows and see a different hash, make sure
+        // the file uses LF line endings (see .gitattributes).
         const val FILE_HASH = "3ee45be83f266197549c3a15534bfe18"
         const val SURAH_FILE_PATH =
             "src/commonMain/kotlin/net/thechance/mena/faith/domain/entity/Surah.kt"
