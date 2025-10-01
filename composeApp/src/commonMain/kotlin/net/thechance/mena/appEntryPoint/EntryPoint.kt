6bf5d94919924ec2ed66f42705fc1f6037d337d2
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-10-01 19:02:43 +0300

    Refactor AuthInterceptor logic and fix typos

--- composeApp/src/commonMain/kotlin/net/thechance/mena/appEntryPoint/EntryPoint.kt
+++ composeApp/src/commonMain/kotlin/net/thechance/mena/appEntryPoint/EntryPoint.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import net.thechance.mena.core_chat.api.CoreChatApi
 import net.thechance.mena.designsystem.presentation.component.bottomNavigation.BottomNavigationBar
@@ -29,7 +28,7 @@ import org.koin.compose.koinInject
 
 
 @Composable
-fun EntryPoint(){
+fun EntryPoint() {
     val identityApi = koinInject<IdentityFeatureApi>()
     val authorizationService = koinInject<AuthorizationService>()
 
@@ -43,8 +42,8 @@ fun EntryPoint(){
 }
 
 @Composable
-private fun LoggedInContainer(){
-    var activeFeature: Feature by remember { mutableStateOf(Feature.WALLET) }
+private fun LoggedInContainer() {
+    var activeFeature: Feature by remember { mutableStateOf(Feature.CHAT) }
     Column(
         Modifier
             .fillMaxSize()
@@ -62,7 +61,7 @@ private fun LoggedInContainer(){
 }
 
 @Composable
-private fun ColumnScope.FeatureContent(activeFeature: Feature){
+private fun ColumnScope.FeatureContent(activeFeature: Feature) {
     val identityApi = koinInject<IdentityFeatureApi>()
     val dukanApi = koinInject<DukanApi>()
     val trendsApi = koinInject<TrendsApi>()
@@ -71,7 +70,7 @@ private fun ColumnScope.FeatureContent(activeFeature: Feature){
     val walletApi = koinInject<WalletApi>()
 
     Box(Modifier.weight(1f)) {
-        when(activeFeature){
+        when (activeFeature) {
             Feature.CHAT -> chatApi.TabEntry()
             Feature.DUKAN -> dukanApi.TabEntry()
             Feature.TREND -> trendsApi.TabEntry()
@@ -82,13 +81,13 @@ private fun ColumnScope.FeatureContent(activeFeature: Feature){
     }
 }
 
-private enum class Feature{
+private enum class Feature {
     CHAT, DUKAN, TREND, FAITH, PROFILE, WALLET
 }
 
 //TODO: refactor this work around with refactoring bottom navbar
-private fun BottomNavigationItem.toFeature(): Feature{
-    return when(this.title) {
+private fun BottomNavigationItem.toFeature(): Feature {
+    return when (this.title) {
         "Home" -> Feature.CHAT
         "Dukan" -> Feature.DUKAN
         "Trends" -> Feature.TREND
--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/login/LoginScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/login/LoginScreen.kt
@@ -197,7 +197,7 @@ class LoginScreen(
         when (effect) {
             is LoginScreenUIEffect.NavigateToRegister -> navigator.push(RegisterScreen())
             LoginScreenUIEffect.NavigateToForgotPassword -> navigator.push(ForgetPasswordScreen())
-            LoginScreenUIEffect.NavigateToHome -> {}
+            LoginScreenUIEffect.NavigateToHome -> TODO()
         }
     }
 }
