4ce1b8ee3b94bebd57995ea4b690cf4dbabfc4a7
ahmed samy <ahmbdelkhalek@gmail.com>
2025-10-02 23:56:30 +0300

    feat: Implement delete reel functionality and UI updates

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelScreen.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelScreen.kt
@@ -74,7 +74,6 @@ internal fun UserReelScreen(
     )
 }
 
-// TODO: That Screen Should be improved later when implementing video player
 @Composable
 private fun UserReelScreenContent(
     state: UserReelState,
@@ -105,6 +104,7 @@ private fun UserReelScreenContent(
                     buttonText = "",
                     dismissOnBackPress = true,
                     dismissOnClickOutside = true,
+                    isVisible = state.isReelDeleted == true && state.error == null,
                     onDismiss = {
                         listener.onDismissSuccessDialog()
                         listener.onBackClick()
@@ -126,6 +126,7 @@ private fun UserReelScreenContent(
                     buttonText = "",
                     dismissOnBackPress = true,
                     dismissOnClickOutside = true,
+                    isVisible = state.error != null,
                     onDismiss = { listener.onDismissErrorDialog() },
                     onCancelClick = { listener.onDismissErrorDialog() },
                     dialogCornerShape = RoundedCornerShape(12.dp),
