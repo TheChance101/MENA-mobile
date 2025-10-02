3887c5007c149230fd1f4434ef6f94b56673fae5
Menna Srour <mennasrour441@gmail.com>
2025-10-02 23:33:55 +0300

    remove incorrect test case

--- wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryViewModelTest.kt
+++ wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryViewModelTest.kt
@@ -96,19 +96,6 @@ class TransactionHistoryViewModelTest {
         }
     }
 
-    @Test
-    fun `should send NavigateToFilterBottomSheet effect when onFilterClicked is called`() = runTest(testDispatcher) {
-        everySuspend { transactionRepository.getTransactionHistory(PAGE,PAGE_SIZE,any()) } returns emptyList()
-        val viewModel = TransactionHistoryViewModel(transactionRepository)
-        advanceUntilIdle()
-        viewModel.uiEffect.test {
-            viewModel.onFilterClicked()
-            val effect = awaitItem()
-            assertEquals(TransactionHistoryEffect.NavigateToFilterBottomSheet, effect)
-            cancelAndIgnoreRemainingEvents()
-        }
-    }
-
     companion object {
         val history = listOf(
             Transaction(
