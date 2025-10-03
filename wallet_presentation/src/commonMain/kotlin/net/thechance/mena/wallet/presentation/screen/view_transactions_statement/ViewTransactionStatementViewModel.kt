e03d4859cd4138cafe1f4a1e4a8b766e42031b8b
abdallahmohamed <abdallah.mohamed2274@gmail.com>
2025-10-03 17:23:10 +0300

    refactor: enhance error handling in ViewTransactionsStatement to utilize ErrorView and add retry functionality

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/view_transactions_statement/ViewTransactionStatementViewModel.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/view_transactions_statement/ViewTransactionStatementViewModel.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import net.thechance.mena.wallet.domain.repository.StatementRepository
 import net.thechance.mena.wallet.presentation.base.BaseViewModel
+import net.thechance.mena.wallet.presentation.base.ErrorState
 import net.thechance.mena.wallet.presentation.base.UiState
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.Provided
@@ -13,13 +14,8 @@ import org.koin.core.annotation.Provided
 class ViewTransactionStatementViewModel(
     @Provided private val statementRepository: StatementRepository,
     private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO
-):
-    BaseViewModel<
-            ViewTransactionStatementScreenState,
-            ViewTransactionStatementEffect>
-        (ViewTransactionStatementScreenState()),
-    ViewTransactionStatementInteractionListener
-{
+) : BaseViewModel<ViewTransactionStatementScreenState, ViewTransactionStatementEffect>
+    (ViewTransactionStatementScreenState()), ViewTransactionStatementInteractionListener {
 
     init {
         fetchLastStatement()
@@ -27,20 +23,24 @@ class ViewTransactionStatementViewModel(
 
     private fun fetchLastStatement() {
         tryToExecute(
-            onStart = { updateState { currentState.copy(statement = UiState.Loading) }},
+            onStart = { updateState { currentState.copy(statement = UiState.Loading) } },
             callee = { statementRepository.getStoredTransactionsPdf() },
             onSuccess = ::onSuccessFetchPdf,
-            onError = { e -> updateState { it.copy(statement = UiState.Error(e)) } },
+            onError = ::onErrorFetchPdf,
             dispatcher = dispatcherIO
         )
     }
 
     private fun onSuccessFetchPdf(pdf: ByteArray?) {
         if (pdf == null) {
-            updateState { it.copy(statement = UiState.Error(Exception("no pdf was found"))) }
+            updateState { it.copy(statement = UiState.Error(ErrorState.NoDataFound)) }
         } else {
             updateState { it.copy(statement = UiState.Success(pdf)) }
         }
+    }
+
+    private fun onErrorFetchPdf(error: ErrorState) {
+        updateState { it.copy(statement = UiState.Error(error)) }
     }
 
     override fun onNavigateBackClicked() {
@@ -52,5 +52,9 @@ class ViewTransactionStatementViewModel(
             val statement = (currentState.statement as UiState.Success<ByteArray>).data
             sendEffect(ViewTransactionStatementEffect.ShareStatement(statement))
         }
+    }
+
+    override fun onRetryClicked() {
+        fetchLastStatement()
     }
 }
