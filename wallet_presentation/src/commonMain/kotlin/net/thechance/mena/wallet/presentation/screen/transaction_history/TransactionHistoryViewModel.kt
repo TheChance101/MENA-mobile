b4c793fbaaa46b8253162e617870aaaf298ee53e
Menna Srour <mennasrour441@gmail.com>
2025-10-04 10:39:15 +0300

    refactor: make reset button close filter bottom sheet

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryViewModel.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryViewModel.kt
@@ -58,7 +58,7 @@ class TransactionHistoryViewModel(
     }
 
     override fun onResetFilterClicked() {
-        updateState { it.copy(filterState = TransactionFilterState()) }
+        updateState { it.copy(filterState = TransactionFilterState(), isFilterVisible = false) }
         resetPaginator()
     }
 
