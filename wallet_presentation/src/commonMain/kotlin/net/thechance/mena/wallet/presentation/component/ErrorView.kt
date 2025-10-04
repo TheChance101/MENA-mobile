d21ce47fd0d747e89a46d1d1d2750ef46ec811ae
Menna Srour <mennasrour441@gmail.com>
2025-10-04 10:36:07 +0300

    refactor: add high quality no internet image

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/ErrorView.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/ErrorView.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
 import mena.wallet_presentation.generated.resources.Res
-import mena.wallet_presentation.generated.resources.img_no_internet
+import mena.wallet_presentation.generated.resources.no_internet
 import mena.wallet_presentation.generated.resources.retry
 import mena.wallet_presentation.generated.resources.unknown_error_description
 import mena.wallet_presentation.generated.resources.unknown_error_title
@@ -26,7 +26,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 internal fun ErrorView(
-    image: Painter = painterResource(Res.drawable.img_no_internet),
+    image: Painter = painterResource(Res.drawable.no_internet),
     title: String = stringResource(Res.string.unknown_error_title),
     description: String = stringResource(Res.string.unknown_error_description),
     onRetry: () -> Unit,
