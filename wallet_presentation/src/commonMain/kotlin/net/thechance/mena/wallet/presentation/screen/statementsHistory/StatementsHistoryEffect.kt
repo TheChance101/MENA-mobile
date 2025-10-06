e639ed5ef28133888e013fa81231923c4e16843e
Malak Raef <malak.raef20@gmail.com>
2025-10-06 18:48:48 +0300

    create ui state, interaction listener and effect for statements history screen

---
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/statementsHistory/StatementsHistoryEffect.kt
@@ -0,0 +1,12 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
+package net.thechance.mena.wallet.presentation.screen.statementsHistory
+
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+sealed interface StatementsHistoryEffect {
+    data object NavigateBack : StatementsHistoryEffect
+    data class NavigateToStatementDetails(val id: Uuid) : StatementsHistoryEffect
+
+}
