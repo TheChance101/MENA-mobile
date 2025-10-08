b48dde1bab3e4930393eff864d35e438c0fa3d16
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-10-08 11:45:06 +0300

    feature: created the base structure for dukan details screens

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/content/SmallImageDukanDetails.kt
@@ -0,0 +1,13 @@
+package net.thechance.mena.dukan.presentation.screen.dukanDetails.content
+
+import androidx.compose.runtime.Composable
+import net.thechance.mena.dukan.presentation.viewModel.dukanDetails.DukanDetailsInteractionListener
+import net.thechance.mena.dukan.presentation.viewModel.dukanDetails.DukanDetailsUiState
+
+@Composable
+fun SmallImageDukanDetails(
+    state: DukanDetailsUiState,
+    listener: DukanDetailsInteractionListener
+) {
+
+}
