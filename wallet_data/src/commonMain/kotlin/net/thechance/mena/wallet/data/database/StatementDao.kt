b10dcd565127b5f9dd0109415f9d8efc9b8a9d2d
gehadepada <gehadepada@gmail.com>
2025-10-09 02:37:31 +0300

    refactor:refactor dao amd database

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/database/StatementDao.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/database/StatementDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import net.thechance.mena.wallet.data.database.entity.Statement
+import net.thechance.mena.wallet.data.database.Statement
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
