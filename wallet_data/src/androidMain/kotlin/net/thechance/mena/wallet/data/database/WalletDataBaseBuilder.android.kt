cfce72992ff4f2ae8d6ad51ed8a158954c077db2
gehadepada <gehadepada@gmail.com>
2025-10-09 16:42:29 +0300

    refactor:refactor WalletDataBaseBuilder.android.kt

--- wallet_data/src/androidMain/kotlin/net/thechance/mena/wallet/data/database/WalletDataBaseBuilder.android.kt
+++ wallet_data/src/androidMain/kotlin/net/thechance/mena/wallet/data/database/WalletDataBaseBuilder.android.kt
@@ -2,15 +2,14 @@ package net.thechance.mena.wallet.data.database
 
 import android.content.Context
 import androidx.room.Room
-import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import androidx.room.RoomDatabase
 
-fun getDatabaseBuilder(context: Context): WalletDatabase {
+
+fun getDatabaseBuilder(context: Context): RoomDatabase.Builder<WalletDatabase> {
     val appContext = context.applicationContext
     val dbFile = appContext.getDatabasePath("wallet.db")
     return Room.databaseBuilder<WalletDatabase>(
         context = appContext,
         name = dbFile.absolutePath
     )
-        .setDriver(BundledSQLiteDriver())
-        .build()
 }
