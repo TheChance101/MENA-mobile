259fb7b573f9f71605f5c8e79e1424674b782516
Radwa85 <radwa.mohamed5033@gmail.com>
2025-10-09 20:41:28 +0300

    remove camera launcher

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/ChatScreen.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/ChatScreen.kt
@@ -7,110 +7,96 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.preat.peekaboo.image.picker.SelectionMode
 import com.preat.peekaboo.image.picker.rememberImagePickerLauncher
-import com.preat.peekaboo.ui.camera.PeekabooCamera
-import com.preat.peekaboo.ui.camera.rememberPeekabooCameraState
-import dev.icerock.moko.permissions.compose.BindEffect
-import dev.icerock.moko.permissions.compose.rememberPermissionsControllerFactory
+import net.thechance.mena.core_chat.presentation.navigation.ChatEffect
+import net.thechance.mena.core_chat.presentation.screen.chat.components.AttachmentsBottomSheet
 import net.thechance.mena.core_chat.presentation.screen.chat.components.ChatHeader
 import net.thechance.mena.core_chat.presentation.screen.chat.components.ChatInputBar
 import net.thechance.mena.core_chat.presentation.screen.chat.components.ChatList
 import net.thechance.mena.core_chat.presentation.screen.chat.components.ChatScreenOverlays
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.parameter.parametersOf
 import kotlin.uuid.ExperimentalUuidApi
 
 @Composable
-fun ChatScreen() {
-    val factory = rememberPermissionsControllerFactory()
-    val controller = remember(factory) { factory.createPermissionsController() }
-    val viewModel: ChatViewModel = koinViewModel(parameters = { parametersOf(controller) })
-    BindEffect(controller)
-
+fun ChatScreen(
+    viewModel: ChatViewModel = koinViewModel()
+) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     ChatScreenContent(
         state = state,
         interactions = viewModel
     )
+
 }
-
-
 @Composable
 fun ChatScreenContent(
     state: ChatScreenState,
     interactions: ChatInteractionListener
 ) {
-    val scope = rememberCoroutineScope()
 
-    val imagePickerLauncher = rememberImagePickerLauncher(
-        selectionMode = SelectionMode.Multiple(maxSelection = 10),
-        scope = scope,
-        onResult = { byteArrays ->
-            interactions.onSendImageClicked(byteArrays)
-        }
-    )
-
-    val cameraState = rememberPeekabooCameraState(onCapture = { capturedBytes ->
-        capturedBytes?.let {
-            interactions.onSendImageClicked(listOf(it))
-            interactions.onCameraDismissed()
-        }
-    })
-
-    if (state.isCameraOpen) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            PeekabooCamera(
-                state = cameraState,
-                modifier = Modifier.fillMaxSize()
+    Scaffold(
+        topBar = {
+            ChatHeader(
+                chatName = state.chatName,
+                onMenuClick = {},
+                onBackClick = interactions::onBackClicked,
+                modifier = Modifier.fillMaxWidth()
             )
-        }
-    } else {
-        Scaffold(
-            topBar = {
-                ChatHeader(
-                    chatName = state.chatName,
-                    onMenuClick = {},
-                    onBackClick = interactions::onBackClicked,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            },
-            bottomBar = {
+        },
+        bottomBar = {
                 ChatInputBar(
                     userInput = state.inputMessage,
                     onTextChange = interactions::onInputMessageChanged,
                     onSendButtonClick = interactions::onSendMessageClicked,
-                    onAttachButtonClick = { imagePickerLauncher.launch() },
-                    onCameraButtonClick = interactions::onCameraClicked,
+                    onAttachButtonClick = interactions::onAttachmentClicked,
                     modifier = Modifier
                         .fillMaxWidth()
                         .background(Theme.colorScheme.background.surface)
                 )
-            },
-            overlays = {
-                ChatScreenOverlays(
-                    showResendMessageDialog = state.isResendMessageDialogVisible,
-                    onDismissResendMessageDialog = interactions::onResendMessageDialogDismissed,
-                    onDeleteFailedMessageClick = interactions::onDeleteFailedMessageClicked,
-                    onResendFailedMessageClick = interactions::onResendMessageClicked,
+            if (state.isAttachmentsOverlayVisible) {
+                AttachmentsBottomSheet(
+                    attachmentsInteractionListener = interactions
                 )
             }
-        ) {
+
+        },
+        overlays = {
+            ChatScreenOverlays(
+                showResendMessageDialog = state.isResendMessageDialogVisible,
+                onDismissResendMessageDialog = interactions::onResendMessageDialogDismissed,
+                onDeleteFailedMessageClick = interactions::onDeleteFailedMessageClicked,
+                onResendFailedMessageClick = interactions::onResendMessageClicked,
+            )
+        }
+    ) {
             ChatList(
                 items = state.chatListItems,
                 chatAvatarUrl = state.chatAvatarUrl,
                 onMessageClick = interactions::onMessageClicked,
                 onFailedMessageClick = interactions::onFailedMessageClicked,
             )
+
         }
+    }
+
+@Composable
+@Preview()
+private fun PreviewMessagingScreenDark() {
+
+    MenaTheme {
+        ChatScreen()
     }
 
 }
