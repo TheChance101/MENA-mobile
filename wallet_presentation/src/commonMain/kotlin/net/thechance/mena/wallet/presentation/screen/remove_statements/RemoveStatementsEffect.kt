1a7b7920afa9d6811f4afa85dfdb94b3a3a00328
Farah315 <fara734isgreat@gmail.com>
2025-10-10 14:20:14 +0300

    refactor: remove init files

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/remove_statements/RemoveStatementsEffect.kt
+++
@@ -1,5 +0,0 @@
-package net.thechance.mena.wallet.presentation.screen.remove_statements
-
-sealed interface RemoveStatementsEffect{
-    data object navigateToStatements : RemoveStatementsEffect
-}
--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/remove_statements/RemoveStatementsInteractionListener.kt
+++
@@ -1,6 +0,0 @@
-package net.thechance.mena.wallet.presentation.screen.remove_statements
-
-interface RemoveStatementsInteractionListener {
-    fun onCancelClicked()
-    fun onDeleteClicked()
-}
