607582f69aeaaccf1864c00b2eaf8e4d468cdb2e
gehadepada <gehadepada@gmail.com>
2025-10-10 12:23:25 +0300

    feat:make statement dao return paginated data

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/database/StatementDao.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/database/StatementDao.kt
@@ -4,17 +4,14 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import net.thechance.mena.wallet.data.database.Statement
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 @Dao
 interface StatementDao {
     @Insert
     suspend fun insertStatement(statement: Statement)
 
-    @Query("SELECT * FROM statement ORDER BY endDate DESC, startDate DESC")
-    suspend fun getAllStatement(): List<Statement>
+    @Query("SELECT * FROM statement ORDER BY endDate DESC, startDate DESC  LIMIT :limit OFFSET :offset")
+    suspend fun getAllStatement(limit:Int,offset:Int): List<Statement>
 
     @Delete
     suspend fun deleteStatement(statement: Statement)
