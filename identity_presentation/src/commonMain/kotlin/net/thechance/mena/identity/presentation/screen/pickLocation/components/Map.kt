86ac3f4991e47b589c7c0f9f859054160faad13a
Aboud <gh.abd.com.207@gmail.com>
2025-10-10 12:48:55 -0700

    fix: remove duplication from ic_arrow_left.xml vector drawable

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/pickLocation/components/Map.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/pickLocation/components/Map.kt
@@ -38,7 +38,12 @@ fun Map(
     currentLocation: PickLocationScreenUIState.CoordinatesUiState?,
     onMapClick: (PickLocationScreenUIState.CoordinatesUiState, DpOffset) -> Unit,
     onCameraMoved: (CameraPosition) -> Unit,
-    animateToCurrentLocation: Boolean = false,
+    animateToCurrentLocation: Boolean = false
+
+
+
+
+    ,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
