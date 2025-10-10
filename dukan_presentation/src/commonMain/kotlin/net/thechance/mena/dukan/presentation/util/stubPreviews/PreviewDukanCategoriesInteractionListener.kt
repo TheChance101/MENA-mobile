ee7a9dfbfa68a67c1f745e81423f68a8720ea931
a7med naser <ad0112727@gmail.com>
2025-10-11 01:39:03 +0300

    feat(categories): Add strings of dukan categories and preview listener for categories screen

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/stubPreviews/PreviewDukanCategoriesInteractionListener.kt
@@ -0,0 +1,17 @@
+package net.thechance.mena.dukan.presentation.util.stubPreviews
+
+import net.thechance.mena.dukan.presentation.viewModel.dukanCategories.DukanCategoriesInteractionListener
+
+object PreviewDukanCategoriesInteractionListener: DukanCategoriesInteractionListener {
+    override fun onBackClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCategoryClicked(categoryId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDismissSnackBar() {
+        TODO("Not yet implemented")
+    }
+}
