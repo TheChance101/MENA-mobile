73e941807aedebf35b9e11a6f4a3dee58954989b
gehadepada <gehadepada@gmail.com>
2025-10-11 21:59:10 +0300

    feat:add di for database

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/di/WalletDataModule.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/di/WalletDataModule.kt
@@ -1,8 +1,27 @@
 package net.thechance.mena.wallet.data.di
 
+import net.thechance.mena.wallet.data.database.StatementDao
+import net.thechance.mena.wallet.data.database.WalletDatabase
+import net.thechance.mena.wallet.data.database.WalletDatabaseBuilder
+import net.thechance.mena.wallet.data.database.getWalletDataBase
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("net.thechance.mena.wallet.data")
-class WalletDataModule
+class WalletDataModule{
+    @Single
+    fun provideWalletDatabaseBuilder(): WalletDatabaseBuilder {
+        return WalletDatabaseBuilder()
+    }
+    @Single
+    fun provideWalletDatabase(builder: WalletDatabaseBuilder): WalletDatabase {
+        return getWalletDataBase(builder.getBuilder())
+    }
+
+    @Single
+    fun provideStatementDao(database: WalletDatabase): StatementDao {
+        return database.getStatementDao()
+    }
+}
