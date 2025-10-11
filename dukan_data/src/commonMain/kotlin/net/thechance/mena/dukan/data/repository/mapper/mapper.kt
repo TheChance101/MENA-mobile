d19818db554b7ab50f8f52e95aa0e4a87f210a6f
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-10-12 02:11:03 +0300

    feat: add DukanDto and mapper to domain model

--- dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/mapper/mapper.kt
+++ dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/repository/mapper/mapper.kt
@@ -3,6 +3,7 @@ package net.thechance.mena.dukan.data.repository.mapper
 import net.thechance.mena.dukan.data.repository.dto.CreateDukanRequest
 import net.thechance.mena.dukan.data.repository.dto.DukanCategoryDto
 import net.thechance.mena.dukan.data.repository.dto.DukanColorDto
+import net.thechance.mena.dukan.data.repository.dto.DukanDto
 import net.thechance.mena.dukan.data.repository.dto.MyDukanStatusDto
 import net.thechance.mena.dukan.domain.entity.Category
 import net.thechance.mena.dukan.domain.entity.Color
@@ -46,4 +47,19 @@ fun MyDukanStatusDto.toMyDukanStatus(): MyDukanStatus {
         status = Dukan.Status.valueOf(status),
         dukanName = dukanName
     )
+}
+
+fun DukanDto.toDomain(): Dukan {
+    return Dukan(
+        id = id,
+        name = name,
+        imageUrl = imageUrl,
+        categories = emptySet(),
+        coordinates = Dukan.Coordinates(0.0, 0.0),
+        address = "",
+        status = Dukan.Status.APPROVED,
+        color = Color("", ""),
+        style = Dukan.Style.WIDE_IMAGE
+    )
+
 }
