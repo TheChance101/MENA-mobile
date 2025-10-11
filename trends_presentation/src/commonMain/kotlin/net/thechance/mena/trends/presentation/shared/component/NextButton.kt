915a65c19de42e8901b99645789eb9480f3759e0
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-10-11 10:14:05 +0300

    refactor: use PrimaryButton

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/NextButton.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/shared/component/NextButton.kt
@@ -1,19 +1,14 @@
 package net.thechance.mena.trends.presentation.shared.component
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import mena.trends_presentation.generated.resources.Res
 import mena.trends_presentation.generated.resources.next
-import net.thechance.mena.designsystem.presentation.component.button.Button
-import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.component.button.PrimaryButton
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
-import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -24,32 +19,14 @@ internal fun NextButton(
     modifier: Modifier = Modifier,
     onNextClick: () -> Unit
 ) {
-    Button(
+    PrimaryButton(
+        modifier = modifier.fillMaxWidth(),
+        text = stringResource(resource = Res.string.next),
         onClick = onNextClick,
         isEnabled = isButtonEnabled,
         isLoading = isButtonLoading,
-        modifier = modifier
-            .fillMaxWidth()
-            .background(Theme.colorScheme.background.surface)
-            .padding(bottom = Theme.spacing._24),
-        shape = RoundedCornerShape(Theme.radius.md),
-        containerColor = Theme.colorScheme.primary.primary,
-        disabledContainerColor = Theme.colorScheme.primary.primary.copy(alpha = 0.5f),
-        contentColor = Theme.colorScheme.primary.onPrimary,
-        disabledContentColor = Theme.colorScheme.primary.onPrimary.copy(alpha = 0.5f),
-    ) { contentColor ->
-        Text(
-            text = stringResource(Res.string.next),
-            color = contentColor,
-            style = Theme.typography.label.medium,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(
-                    vertical = 13.dp,
-                    horizontal = Theme.spacing._24
-                )
-        )
-    }
+        contentPadding = PaddingValues(vertical = 13.dp)
+    )
 }
 
 @Preview(showBackground = true)
@@ -57,9 +34,10 @@ internal fun NextButton(
 private fun NextButtonPreview() {
     MenaTheme {
         NextButton(
-            onNextClick = {},
             isButtonEnabled = true,
             isButtonLoading = false
-        )
+        ){
+            //AAction
+        }
     }
 }
