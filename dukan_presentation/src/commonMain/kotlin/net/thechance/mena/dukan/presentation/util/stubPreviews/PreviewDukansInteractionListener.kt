332679deda6f0b37695c34cc17b9ae58d3889db0
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-10-11 20:42:19 +0300

    feat: add fake data for dukans screen previews

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/stubPreviews/PreviewDukansInteractionListener.kt
@@ -0,0 +1,9 @@
+package net.thechance.mena.dukan.presentation.util.stubPreviews
+
+import net.thechance.mena.dukan.presentation.viewModel.dukans.DukanUiState
+import net.thechance.mena.dukan.presentation.viewModel.dukans.DukansInteractionListener
+
+object PreviewDukansInteractionListener : DukansInteractionListener {
+    override fun onBackClick() {}
+    override fun onDukanClick(dukan: DukanUiState) {}
+}
