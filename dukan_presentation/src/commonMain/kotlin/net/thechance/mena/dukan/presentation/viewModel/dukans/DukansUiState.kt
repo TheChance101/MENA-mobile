2e962ac26fd78625637435d13c99f4f58ae49486
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-10-11 21:18:05 +0300

    feat: add `DukansUiState` and `EmptyStateContent` component

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/dukans/DukansUiState.kt
@@ -0,0 +1,20 @@
+package net.thechance.mena.dukan.presentation.viewModel.dukans
+
+import net.thechance.mena.dukan.presentation.util.pagination.PagingData
+
+data class DukansUiState(
+    val dukans: PagingData<DukanUiState> = PagingData(),
+    val dukansState: DukansState = DukansState.LOADING,
+)
+
+enum class DukansState {
+    LOADING,
+    LOADED,
+    EMPTY
+}
+
+data class DukanUiState(
+    val id: String = "",
+    val name: String = "",
+    val imageUrl: String = ""
+)
