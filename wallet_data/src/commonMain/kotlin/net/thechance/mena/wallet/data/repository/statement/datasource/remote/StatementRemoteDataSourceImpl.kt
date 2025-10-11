396932372b426a2ab6bdec56a4a1efbe7ebc095d
gehadepada <gehadepada@gmail.com>
2025-10-11 15:42:35 +0300

    feat: add extractStatementInfoFromHeaders function

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/statement/datasource/remote/StatementRemoteDataSourceImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/statement/datasource/remote/StatementRemoteDataSourceImpl.kt
@@ -2,6 +2,8 @@ package net.thechance.mena.wallet.data.repository.statement.datasource.remote
 
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.readRawBytes
+import net.thechance.mena.wallet.data.database.Statement
+import net.thechance.mena.wallet.data.database.StatementDao
 import net.thechance.mena.wallet.data.exceptions.safeApiCall
 import net.thechance.mena.wallet.data.mapper.toStatementRequest
 import net.thechance.mena.wallet.data.network_client.NetworkClient
@@ -11,14 +13,26 @@ import org.koin.core.annotation.Single
 
 @Single(binds = [StatementRemoteDataSource::class])
 class StatementRemoteDataSourceImpl(
-    private val networkClient: NetworkClient
+    private val networkClient: NetworkClient,
+    private val statementDao : StatementDao
 ): StatementRemoteDataSource {
     override suspend fun getTransactionPdf(filterRequestParams: TransactionFilterParams?): ByteArray {
-        return safeApiCall<HttpResponse> {
+        val response =safeApiCall<HttpResponse> {
             networkClient.get(
                 urlString = STATEMENT_PATH,
                 block = filterRequestParams?.toStatementRequest() ?: {}
             )
-        }.readRawBytes()
+        }
+        return response.readRawBytes()
+
     }
+}
+private fun extractStatementInfoFromHeaders(response : HttpResponse): Statement{
+    return Statement(
+        totalInflows = response.headers["X-Statement-Total-Inflows"]?.toDoubleOrNull() ?: 0.0,
+        totalOutflows = response.headers["X-Statement-Total-Outflows"]?.toDoubleOrNull() ?: 0.0,
+        startDate = response.headers["X-Statement-Start-Date"].orEmpty(),
+        endDate = response.headers["X-Statement-End-Date"].orEmpty()
+    )
+
 }
