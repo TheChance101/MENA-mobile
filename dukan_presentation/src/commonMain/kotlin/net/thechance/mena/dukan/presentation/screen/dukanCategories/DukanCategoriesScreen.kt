d5fe6f7084160f2e16aa8fb1e8b098a5bcf4f90b
a7med naser <ad0112727@gmail.com>
2025-10-12 17:27:10 +0300

    Refactor: Remove unused `LazyGridScope` import

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanCategories/DukanCategoriesScreen.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanCategories/DukanCategoriesScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
