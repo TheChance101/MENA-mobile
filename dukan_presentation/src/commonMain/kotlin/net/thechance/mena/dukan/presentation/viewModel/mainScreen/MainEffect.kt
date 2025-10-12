c65d20d790d355dff22ac9b40d8e558f61c80988
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-10-12 16:45:50 +0300

    refactor: remove NavigateToDukansUnderCategory effect

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/mainScreen/MainEffect.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/mainScreen/MainEffect.kt
@@ -4,8 +4,4 @@ sealed class MainEffect {
     object NavigateToAddDukanScreen : MainEffect()
     object NavigateToPendingDukanScreen : MainEffect()
     object NavigateToManageDukanScreen : MainEffect()
-    data class NavigateToDukansUnderCategory(
-        val categoryId: String,
-        val categoryTitle: String
-    ) : MainEffect()
 }
