065f73be294a85c5788d2341b8436be2b9b2c14b
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-13 18:45:29 +0300

    refactor: resolve conflict

--- wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/confirm_payment/ConfirmPaymentViewModelTest.kt
+++ wallet_presentation/src/commonTest/kotlin/net/thechance/mena/wallet/presentation/screen/confirm_payment/ConfirmPaymentViewModelTest.kt
@@ -88,7 +88,6 @@ class ConfirmPaymentViewModelTest {
             }
         }
 
-
     @Test
     fun `ConfirmPaymentViewModel should update payment ui state when user repository returns value`() =
         runTest {
@@ -178,31 +177,6 @@ class ConfirmPaymentViewModelTest {
     }
 
     @Test
-    fun `onPayButtonClicked should set isPayBtnLoading to true`() = runTest {
-        everySuspend {
-            paymentRepository.getPaymentConfirmation(
-                any(),
-                any()
-            )
-        } returns paymentConfirmation1
-
-        val viewModel = ConfirmPaymentViewModel(
-            args = ConfirmPaymentArgs(receiver1Id.toString(), amount1),
-            paymentRepository = paymentRepository,
-            ioDispatcher = testDispatcher
-        )
-
-        viewModel.state.test {
-            skipItems(3)
-            viewModel.onPayButtonClicked()
-            val loadingState = awaitItem()
-            assertTrue(loadingState.isPayBtnLoading)
-            cancelAndIgnoreRemainingEvents()
-        }
-    }
-
-
-    @Test
     fun `onRefresh should set state with loading when initially called`() = runTest {
         everySuspend { balanceRepository.getBalance() } returns balance1
         everySuspend { userRepository.getReceiverByTransactionId(receiver1Id) } returns receiver1
@@ -250,5 +224,4 @@ class ConfirmPaymentViewModelTest {
             balance = balance1_ui
         )
     }
-
 }
