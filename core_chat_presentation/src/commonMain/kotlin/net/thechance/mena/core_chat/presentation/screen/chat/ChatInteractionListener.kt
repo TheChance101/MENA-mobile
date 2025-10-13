666ff04102fdea2939360edff441c1cb99b1421c
Radwa85 <radwa.mohamed5033@gmail.com>
2025-10-13 22:09:56 +0300

    add camera permission handling to chat screen

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/ChatInteractionListener.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/ChatInteractionListener.kt
@@ -27,6 +27,7 @@ interface AttachmentsInteractionListener {
     fun onSendImageClicked(imageByteArrays: List<ByteArray>)
     fun onGalleryClicked()
     fun onCameraClicked()
+    fun onCameraResultHandled()
     fun onCloseAttachmentClicked()
 }
 
