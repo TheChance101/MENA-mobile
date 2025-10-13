12e3dfbebcc2191cfd25b0df0b3598f3b7bece3c
Al-Qassim <71926296+Al-Qassim@users.noreply.github.com>
2025-10-13 20:13:38 +0300

    remove unnecessary navigation parameter

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/home/HomeViewModel.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/home/HomeViewModel.kt
@@ -94,8 +94,6 @@ class HomeViewModel(
             ChatDetailsRoute(
                 chatId = chat.id.toString(),
                 chatName = chat.name,
-                chatImageUrl = chat.imageUrl.toString(),
-                chatRequesterId = chat.id.toString() // TODO("Replace with the actual requester id")
             )
         )
     }
