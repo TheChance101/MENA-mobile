8b7090d8d3139cc78f316aa71576965dd1583579
nada <nadamansoursamir@gmail.com>
2025-10-14 00:03:09 +0300

    refactor: remove unused import

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/components/CartOrQuantityProductComponent.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/dukanDetails/components/CartOrQuantityProductComponent.kt
@@ -1,14 +1,12 @@
 package net.thechance.mena.dukan.presentation.screen.dukanDetails.components
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
