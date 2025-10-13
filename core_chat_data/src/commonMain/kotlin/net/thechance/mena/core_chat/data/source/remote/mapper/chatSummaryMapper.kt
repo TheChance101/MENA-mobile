f489fa7c7a5e972bca783d08f9c24871e3631d7e
AdelTamer <adeltammer@gmail.com>
2025-10-13 22:46:13 +0300

    refactor chat summary domain and chat summary mapper

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/mapper/chatSummaryMapper.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/mapper/chatSummaryMapper.kt
@@ -2,15 +2,16 @@
 
 package net.thechance.mena.core_chat.data.source.remote.mapper
 
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import net.thechance.mena.core_chat.data.source.remote.dto.ChatSummaryDto
-import net.thechance.mena.core_chat.data.source.remote.dto.ChatSummaryStatusDto
 import net.thechance.mena.core_chat.data.source.remote.dto.PagedDataDto
 import net.thechance.mena.core_chat.data.utils.getUuidOrNull
 import net.thechance.mena.core_chat.domain.entity.ChatSummary
-import net.thechance.mena.core_chat.domain.entity.ChatSummaryStatus
 import net.thechance.mena.core_chat.domain.exception.ContactsFetchFailedException
 import net.thechance.mena.core_chat.domain.model.PagedData
-import kotlin.collections.orEmpty
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 import kotlin.uuid.ExperimentalUuidApi
 
 fun PagedDataDto<ChatSummaryDto>?.toPagedListOfChatSummary(): PagedData<ChatSummary> {
@@ -23,19 +24,22 @@ fun PagedDataDto<ChatSummaryDto>?.toPagedListOfChatSummary(): PagedData<ChatSumm
 }
 
 private fun List<ChatSummaryDto>.toListOfChatSummary(): List<ChatSummary> {
-    return mapNotNull { it.toDomain() }
+    return mapNotNull { it.toDomain() }.sortedByDescending { it.lastMessage.sendAt }
 }
 
+@OptIn(ExperimentalTime::class)
 fun ChatSummaryDto.toDomain(): ChatSummary? {
+    val sendAt = Instant.parse(lastMessage.sentAt).toLocalDateTime(TimeZone.currentSystemDefault())
+
     return ChatSummary(
         id = getUuidOrNull(id) ?: return null,
+        name = name,
         imageUrl = imageUrl.orEmpty(),
-        lastMessage = lastMessage.orEmpty(),
-        lastMessageTime = lastMessageTime.orEmpty(),
-        name = name.orEmpty(),
-        status = chatSummaryStatusDto.toDomain()
+        lastMessage = ChatSummary.Message(
+            content = lastMessage.content,
+            sendAt = sendAt,
+            isMine = lastMessage.isMine == true
+        ),
+        unReadMessagesCount = unReadMessagesCount
     )
 }
-
-fun ChatSummaryStatusDto?.toDomain(): ChatSummaryStatus =
-    ChatSummaryStatus(this?.isMine ?: false, this?.unReadMessagesCount ?: 0)
--- core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/entity/ChatSummary.kt
+++ core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/entity/ChatSummary.kt
@@ -2,14 +2,20 @@
 
 package net.thechance.mena.core_chat.domain.entity
 
+import kotlinx.datetime.LocalDateTime
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 data class ChatSummary(
     val id: Uuid,
-    val imageUrl: String,
-    val lastMessage: String,
-    val lastMessageTime: String,
     val name: String,
-    val status: ChatSummaryStatus
-)
+    val imageUrl: String,
+    val lastMessage: Message,
+    val unReadMessagesCount: Int
+) {
+    data class Message(
+        val content: String,
+        val sendAt: LocalDateTime,
+        val isMine: Boolean
+    )
+}
