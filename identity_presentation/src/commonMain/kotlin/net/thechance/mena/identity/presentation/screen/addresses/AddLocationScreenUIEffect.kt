e23b5c175183f69bccac1eec2d30599c23626e44
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-10-14 23:23:42 +0300

    test: add unit tests for AddEditLocationScreenViewModel

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/addresses/AddLocationScreenUIEffect.kt
+++
@@ -1,8 +0,0 @@
-package net.thechance.mena.identity.presentation.screen.addresses
-
-sealed class AddLocationScreenUIEffect {
-
-    data object NavigateBack  : AddLocationScreenUIEffect()
-    data object NavigateToMap : AddLocationScreenUIEffect()
-
-}
