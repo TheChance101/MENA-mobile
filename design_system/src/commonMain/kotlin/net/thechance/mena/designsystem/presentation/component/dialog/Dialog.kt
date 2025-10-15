697dd44e889f5ff11a3622bee4379f3ddc5dcbff
mohamedshemees <mohamedessampd@gmail.com>
2025-10-16 00:01:55 +0300

    refactor: remove center text alignment from dialog message

--- design_system/src/commonMain/kotlin/net/thechance/mena/designsystem/presentation/component/dialog/Dialog.kt
+++ design_system/src/commonMain/kotlin/net/thechance/mena/designsystem/presentation/component/dialog/Dialog.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import net.thechance.mena.designsystem.presentation.component.appBar.HomeAppBar
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
@@ -147,7 +146,6 @@ fun ScaffoldScope.Dialog(
             )
             Text(
                 text = message,
-                textAlign = TextAlign.Center,
                 style = Theme.typography.body.small,
                 color = Theme.colorScheme.shadeSecondary,
             )
