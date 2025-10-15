a9f00f1deb26123a28b38d9a93d9e975466f9653
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-10-15 14:37:46 +0300

    implement AzimuthProvider for iOS to get heading updates

--- faith_presentation/src/iosMain/kotlin/net/thechance/mena/faith/presentation/util/AzimuthProvider.ios.kt
+++ faith_presentation/src/iosMain/kotlin/net/thechance/mena/faith/presentation/util/AzimuthProvider.ios.kt
@@ -1,13 +1,43 @@
 package net.thechance.mena.faith.presentation.util
 
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.callbackFlow
+import platform.CoreLocation.CLHeading
+import platform.CoreLocation.CLLocationManager
 import platform.CoreLocation.CLLocationManagerDelegateProtocol
+import platform.Foundation.NSError
 import platform.darwin.NSObject
 
 actual class AzimuthProvider {
-    private val locationManager = LocationManager()
-    actual fun startListening(): Flow<Float> = flowOf(0f)
-}
+    private val locationManager = CLLocationManager()
 
-internal class LocationManager : NSObject(), CLLocationManagerDelegateProtocol
+    actual fun startListening(): Flow<Float> = callbackFlow {
+        val delegate = object : NSObject(), CLLocationManagerDelegateProtocol {
+            override fun locationManager(
+                manager: CLLocationManager,
+                didUpdateHeading: CLHeading
+            ) {
+                // Azimuth in degrees (0-360)
+                val azimuth = didUpdateHeading.trueHeading.toFloat()
+                println("Azimuth: $azimuth")
+                trySend(azimuth)
+            }
+
+            override fun locationManager(
+                manager: CLLocationManager,
+                didFailWithError: NSError
+            ) {
+                close(Exception(didFailWithError.localizedDescription))
+            }
+        }
+
+        locationManager.delegate = delegate
+        locationManager.startUpdatingHeading()
+
+        awaitClose {
+            locationManager.stopUpdatingHeading()
+            locationManager.delegate = null
+        }
+    }
+}
