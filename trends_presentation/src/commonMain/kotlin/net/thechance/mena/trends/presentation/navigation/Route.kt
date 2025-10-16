deff0bc9f7b887ee2529ca66ff043d4f0a4b9d9c
Yusuf Nasser <yusufnassereng@gmail.com>
2025-10-16 16:36:14 +0300

    TRN-55: add update interests screen route

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/navigation/Route.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/navigation/Route.kt
@@ -7,6 +7,9 @@ internal sealed interface Route {
     data object Categories : Route
 
     @Serializable
+    data object UpdateInterests : Route
+
+    @Serializable
     data object Trends : Route
 
     @Serializable
