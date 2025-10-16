3a9ae7222bc60479e2c61099175c0ea726ffc9c6
Aboud <gh.abd.com.207@gmail.com>
2025-10-16 00:28:16 -0700

    feat: make location parameters optional in AddEditLocationScreen

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/addresses/AddEditLocationScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/addresses/AddEditLocationScreen.kt
@@ -36,9 +36,9 @@ import org.koin.core.parameter.parametersOf
 import org.maplibre.compose.camera.CameraPosition
 
 class AddEditLocationScreen(
-    private val latitude: Double?,
-    private val longitude: Double?,
-    private val address: String?,
+    private val latitude: Double? = null,
+    private val longitude: Double? = null,
+    private val address: String? = null,
 ) : BaseScreen<
         AddEditLocationScreenViewModel,
         AddLocationScreenUIState,
