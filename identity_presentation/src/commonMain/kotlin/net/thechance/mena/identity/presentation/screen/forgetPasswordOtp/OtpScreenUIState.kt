0fea3b8407eed475d561ff5b7d60dcd439807d3c
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-10-19 16:55:52 +0300

    feat: Use StringResource for OTP error message

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/forgetPasswordOtp/OtpScreenUIState.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/forgetPasswordOtp/OtpScreenUIState.kt
@@ -1,10 +1,12 @@
 package net.thechance.mena.identity.presentation.screen.forgetPasswordOtp
 
+import org.jetbrains.compose.resources.StringResource
+
 data class OtpScreenUIState (
     val otpValue: String = "",
     val isVerifyEnabled: Boolean = false,
     val isLoading: Boolean = false,
     val isResendEnabled: Boolean = true,
     val timer: String = "",
-    val errorMessage: String? = null
+    val errorMessage: StringResource? = null
 )
