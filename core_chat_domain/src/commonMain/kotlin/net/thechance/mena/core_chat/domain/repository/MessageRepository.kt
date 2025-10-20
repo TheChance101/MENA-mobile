05cedbf6a98c740d0e66da46c02b2069fbd0097d
AdelTamer <adeltammer@gmail.com>
2025-10-20 17:27:27 +0300

    move mark message as read event to event package and write comment documentation to be more readable

--- core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/repository/MessageRepository.kt
+++ core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/repository/MessageRepository.kt
@@ -1,7 +1,7 @@
 package net.thechance.mena.core_chat.domain.repository
 
 import kotlinx.coroutines.flow.Flow
-import net.thechance.mena.core_chat.domain.entity.MarkMessageAsReadEvent
+import net.thechance.mena.core_chat.domain.event.MarkMessageAsReadEvent
 import net.thechance.mena.core_chat.domain.entity.Message
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
