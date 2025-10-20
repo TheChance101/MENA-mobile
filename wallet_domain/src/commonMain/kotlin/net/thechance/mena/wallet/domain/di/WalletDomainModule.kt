7543db6554a1225811d1ee7954e7aea7a6ad7d7d
EL-MANCY <abdelrahmanelmancy@gmail.com>
2025-10-20 18:42:32 +0300

    remove WalletDomainModule di

--- wallet_domain/src/commonMain/kotlin/net/thechance/mena/wallet/domain/di/WalletDomainModule.kt
+++
@@ -1,8 +0,0 @@
-package net.thechance.mena.wallet.domain.di
-
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
-
-@Module
-@ComponentScan("net.thechance.mena.wallet.domain")
-class WalletDomainModule
