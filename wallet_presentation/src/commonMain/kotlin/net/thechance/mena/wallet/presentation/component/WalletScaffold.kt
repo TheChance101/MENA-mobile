439205839034c66caf0144fd7ae7ab47e5330bab
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-10-20 21:48:06 +0300

    implement custom toast

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/WalletScaffold.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/component/WalletScaffold.kt
@@ -26,7 +26,6 @@ fun WalletScaffold(
     topBar: (@Composable () -> Unit)? = null,
     snackBar: (@Composable () -> Unit)? = null,
     overlays: (ScaffoldScope.() -> Unit)? = null,
-    toast: (@Composable () -> Unit)? = null,
     bottomContent: (@Composable () -> Unit)? = null,
     backgroundColor: Color = Theme.colorScheme.background.surface,
     errorState: ErrorState? = null,
@@ -65,12 +64,6 @@ fun WalletScaffold(
                     .align(Alignment.TopCenter)
                     .padding(start = 16.dp, end = 16.dp, top = 68.dp)
             ) { snackBarContent() }
-        }
-
-        toast?.let { toast ->
-            Box(
-                modifier = Modifier.align(Alignment.Center)
-            ) { toast() }
         }
 
         bottomContent?.let {
