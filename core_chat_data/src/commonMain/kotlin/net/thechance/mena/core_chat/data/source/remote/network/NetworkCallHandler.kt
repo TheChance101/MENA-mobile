37b2edc95ea56e3aed7acadba9d646c9ea7c6e75
Radwa85 <radwa.mohamed5033@gmail.com>
2025-10-20 21:12:07 +0300

    move network call handler with custom exceptions to network package

---
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/network/NetworkCallHandler.kt
@@ -0,0 +1,46 @@
+package net.thechance.mena.core_chat.data.source.remote.network
+
+import com.bilalazzam.contacts_provider.ContactsPermissionDeniedException
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.isSuccess
+import io.ktor.util.reflect.TypeInfo
+import net.thechance.mena.core_chat.domain.exception.*
+
+suspend fun <T> tryNetworkCall(
+    defaultException: (Throwable) -> ChatException = { e ->
+        UnknownException("Unknown error occurred", e)
+    },
+    bodyType: TypeInfo,
+    call: suspend () -> HttpResponse,
+): T? {
+    return runCatchingWithException(defaultException) {
+        val response = call()
+        response.getSuccessBodyOrThrow(bodyType)
+    }
+}
+
+private suspend fun <T> runCatchingWithException(
+    defaultException: (Throwable) -> ChatException,
+    block: suspend () -> T?
+): T? {
+    return try {
+        block()
+    } catch (e: ContactsPermissionDeniedException) {
+        throw ContactsPermissionDeniedException("Contacts Permission Denied!", e)
+    } catch (e: ChatException) {
+        throw e
+    } catch (e: Throwable) {
+        throw defaultException(e)
+    }
+}
+
+private suspend fun <T> HttpResponse.getSuccessBodyOrThrow(bodyType: TypeInfo): T? {
+    return when {
+        this.status.isSuccess() -> this.body(bodyType)
+        this.status == HttpStatusCode.Unauthorized -> throw UnAuthorizedException()
+        this.status == HttpStatusCode.NotFound -> throw NotFoundException(this.status.description)
+        else -> throw UnknownException(this.status.description)
+    }
+}
