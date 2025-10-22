36fe5daaa2c388e37ace179526b313449453781f
Hend Sayed <hendsayed853@gmail.com>
2025-10-22 21:49:07 +0300

    refactor: Remove unused `setUploadRequestTimeout` extension

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/httpClientExtensions.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/httpClientExtensions.kt
@@ -1,16 +1,7 @@
 package net.thechance.mena.trends.data.util
 
-import io.ktor.client.plugins.HttpTimeoutConfig
 import io.ktor.client.plugins.onUpload
-import io.ktor.client.plugins.timeout
 import io.ktor.client.request.HttpRequestBuilder
-
-fun HttpRequestBuilder.setUploadRequestTimeout() {
-    timeout {
-        requestTimeoutMillis = HttpTimeoutConfig.INFINITE_TIMEOUT_MS
-        socketTimeoutMillis = HttpTimeoutConfig.INFINITE_TIMEOUT_MS
-    }
-}
 
 fun HttpRequestBuilder.observeUploading(
     onProgress: suspend (sent: Long, total: Long) -> Unit
