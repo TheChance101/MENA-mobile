0ffeb8ae6e584bb47fb65017ee0b1176a499a396
norhanadel <norhanadel367@gmail.com>
2025-10-22 16:55:16 +0300

    add background brand when loading

--- trends_presentation/src/iosMain/kotlin/net/thechance/mena/trends/presentation/video_player/VideoPlayer.ios.kt
+++ trends_presentation/src/iosMain/kotlin/net/thechance/mena/trends/presentation/video_player/VideoPlayer.ios.kt
@@ -1,6 +1,7 @@
 package net.thechance.mena.trends.presentation.video_player
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
@@ -164,15 +165,17 @@ actual fun VideoPlayer(
             }
 
             if (isLoading && !isPaused) {
-                DotsProgressIndicator(
-                    modifier = Modifier.align(Alignment.Center),
-                    dotSize = 10.dp,
-                    colors = listOf(
-                        Theme.colorScheme.stroke,
-                        Theme.colorScheme.shadeTertiary,
-                        Theme.colorScheme.shadeTertiary
+                Box(Modifier.fillMaxSize().background(Theme.colorScheme.brand.brand)){
+                    DotsProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center),
+                        dotSize = 10.dp,
+                        colors = listOf(
+                            Theme.colorScheme.stroke,
+                            Theme.colorScheme.shadeTertiary,
+                            Theme.colorScheme.shadeTertiary
+                        )
                     )
-                )
+                }
             }
 
             ProgressBar(
