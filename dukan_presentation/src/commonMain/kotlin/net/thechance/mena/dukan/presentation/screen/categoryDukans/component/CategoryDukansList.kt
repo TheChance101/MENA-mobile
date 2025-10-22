d60391a91e7890f40520e4cc2ffdebb7719a380a
abdelrhman <abdelrhmanshabana999@gmail.com>
2025-10-22 13:47:38 +0300

    Refactor: migrate categoryDukans screen to AndroidX Paging 3 library

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/categoryDukans/component/CategoryDukansList.kt
@@ -0,0 +1,34 @@
+package net.thechance.mena.dukan.presentation.screen.categoryDukans.component
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.paging.compose.LazyPagingItems
+import net.thechance.mena.dukan.presentation.component.shared.DukanCard
+import net.thechance.mena.dukan.presentation.viewModel.categoryDukans.CategoryDukansInteractionListener
+import net.thechance.mena.dukan.presentation.viewModel.categoryDukans.CategoryDukansUiState
+
+@Composable
+fun CategoryDukansList(
+    dukans: LazyPagingItems<CategoryDukansUiState.DukanUiState>,
+    listener: CategoryDukansInteractionListener,
+    isLoading: Boolean = false,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            count = dukans.itemCount,
+        ) { index ->
+            val dukan = dukans[index] ?: return@items
+            DukanCard(
+                dukan = dukan,
+                isFavorite = dukan.isFavorite,
+                onClick = { listener.onDukanClick(dukan) },
+                onFavoriteClick = { listener.onFavoriteClick(dukan) },
+                isLoading = isLoading
+            )
+        }
+    }
+}
--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/categoryDukans/CategoryDukansViewModel.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/categoryDukans/CategoryDukansViewModel.kt
@@ -1,18 +1,14 @@
 package net.thechance.mena.dukan.presentation.viewModel.categoryDukans
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flowOf
 import net.thechance.mena.dukan.domain.repository.DukanDiscoveryRepository
-import net.thechance.mena.dukan.presentation.util.pagination.PagerOld
-import net.thechance.mena.dukan.presentation.util.pagination.PagingDataOld
-import net.thechance.mena.dukan.presentation.util.pagination.base.createPagingSource
 import net.thechance.mena.dukan.presentation.viewModel.base.BaseViewModel
 import net.thechance.mena.dukan.presentation.viewModel.categoryDukans.CategoryDukansUiState.DukanUiState
-import net.thechance.mena.dukan.presentation.viewModel.categoryDukans.CategoryDukansUiState.DukansState
 
 class CategoryDukansViewModel(
     private val dukanDiscoveryRepository: DukanDiscoveryRepository,
@@ -23,10 +19,8 @@ class CategoryDukansViewModel(
     defaultDispatcher = defaultDispatcher
 ), CategoryDukansInteractionListener {
 
-    private var pagerOld: PagerOld<Int, DukanUiState>? = null
-
-    val initializedPagerOld: PagerOld<Int, DukanUiState> by lazy {
-        initializePager()
+    init {
+        loadCategory()
     }
 
     override fun onBackClick() {
@@ -53,13 +47,13 @@ class CategoryDukansViewModel(
     }
 
     private fun updateFavoriteState(dukan: DukanUiState) {
-        updateState {
-            copy(
-                dukans = dukans.copy(
-                    items = dukans.items.toggleFavoriteBest(dukan.id)
-                )
-            )
-        }
+//        updateState {
+//            copy(
+//                dukans = dukans.copy(
+//                    items = dukans.items.toggleFavoriteBest(dukan.id)
+//                )
+//            )
+//        }
     }
 
     private fun List<DukanUiState>.toggleFavoriteBest(dukanId: String): List<DukanUiState> {
@@ -72,67 +66,36 @@ class CategoryDukansViewModel(
         }
     }
 
-    private fun loadDukans(pagerOld: PagerOld<Int, DukanUiState>) {
-        refreshPager(pagerOld)
-        collectDukans(pagerOld)
-        loadNextPage(pagerOld)
-    }
-
-    private fun refreshPager(pagerOld: PagerOld<Int, DukanUiState>) {
-        viewModelScope.launch {
-            pagerOld.refresh()
-        }
-    }
-
-    private fun collectDukans(pagerOld: PagerOld<Int, DukanUiState>) {
+    private fun collectDukans(categoryId: String) {
         tryToCollect(
-            onStart = ::onLoadingStart,
-            block = { pagerOld.flow },
+            block = {
+                createPagingSourceFlow(
+                    mapper = { it.toUiState() }
+                ) { pageNumber, pageSize ->
+                    dukanDiscoveryRepository.getDukansByCategory(
+                        categoryId = categoryId,
+                        page = pageNumber,
+                        size = 20
+                    ).items
+                }
+            },
             onCollect = ::onDukansLoaded
+
         )
     }
 
-    private fun loadNextPage(pagerOld: PagerOld<Int, DukanUiState>) {
-        viewModelScope.launch {
-            pagerOld.load()
-        }
-    }
-
-    private fun onLoadingStart() {
+    private fun onDukansLoaded(dukans: PagingData<DukanUiState>) {
         updateState {
             copy(
-                dukansState = DukansState.LOADING,
-                dukans = PagingDataOld()
+                dukans = flowOf(dukans),
             )
         }
     }
 
-    private fun onDukansLoaded(dukans: PagingDataOld<DukanUiState>) {
-        updateState {
-            copy(
-                dukans = dukans,
-                dukansState = getDukansState(dukans)
-            )
-        }
-    }
-
-    private fun getDukansState(dukans: PagingDataOld<DukanUiState>): DukansState {
-        return when {
-            dukans.isLoading && dukans.items.isEmpty() -> DukansState.LOADING
-            dukans.items.isEmpty() -> DukansState.EMPTY
-            else -> DukansState.LOADED
-        }
-    }
-
-    private fun initializePager(): PagerOld<Int, DukanUiState> {
+    private fun loadCategory() {
         val (categoryId, categoryTitle) = getCategoryArguments()
         updateCategoryState(categoryId, categoryTitle)
-
-        val pager = createDukanPager(categoryId)
-        this.pagerOld = pager
-        loadDukans(pager)
-
-        return pager
+        collectDukans(categoryId)
     }
 
     private fun getCategoryArguments(): Pair<String, String> {
@@ -146,18 +109,6 @@ class CategoryDukansViewModel(
             copy(
                 categoryId = categoryId,
                 categoryTitle = categoryTitle
-            )
-        }
-    }
-
-    private fun createDukanPager(categoryId: String): PagerOld<Int, DukanUiState> {
-        return createPagingSource(
-            mapper = { it.toUiState() }
-        ) { pageNumber ->
-            dukanDiscoveryRepository.getDukansByCategory(
-                categoryId = categoryId,
-                page = pageNumber,
-                size = 20
             )
         }
     }
