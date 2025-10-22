599b50915d76605ead86d857d001d59f41a3069e
bilalazzam <blalazam065@gmail.com>
2025-10-22 08:34:09 +0300

    encode ImageBitmap To ByteArray With Compression incrementally To MaxSize

--- core_chat_presentation/src/androidMain/kotlin/net/thechance/mena/core_chat/presentation/camera/CameraManager.android.kt
+++ core_chat_presentation/src/androidMain/kotlin/net/thechance/mena/core_chat/presentation/camera/CameraManager.android.kt
@@ -4,9 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import io.github.vinceglb.filekit.dialogs.compose.PhotoResultLauncher
 import io.github.vinceglb.filekit.dialogs.compose.rememberCameraPickerLauncher
-import io.github.vinceglb.filekit.dialogs.compose.util.encodeToByteArray
 import io.github.vinceglb.filekit.dialogs.compose.util.toImageBitmap
 import kotlinx.coroutines.launch
+import net.thechance.mena.core_chat.presentation.utils.encodeToByteArrayWithCompressionToMaxSize
 
 @Composable
 actual fun rememberCameraManager(onResult: (ByteArray?) -> Unit): CameraManager {
@@ -14,7 +14,7 @@ actual fun rememberCameraManager(onResult: (ByteArray?) -> Unit): CameraManager 
     val launcher = rememberCameraPickerLauncher { file ->
         file?.let { image ->
             scope.launch {
-                onResult(image.toImageBitmap().encodeToByteArray())
+                onResult(image.toImageBitmap().encodeToByteArrayWithCompressionToMaxSize())
             }
         }
     }
