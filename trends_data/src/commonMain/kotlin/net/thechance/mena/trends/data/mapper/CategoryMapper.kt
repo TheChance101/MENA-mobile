04a2b1fa8fe0bc1081db33d55ff571b140fe5bc8
Hend Sayed <hendsayed853@gmail.com>
2025-10-23 20:26:15 +0300

    Fix: Handle null ID in CategoryDto

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/mapper/CategoryMapper.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/mapper/CategoryMapper.kt
@@ -6,7 +6,7 @@ import net.thechance.mena.trends.domain.entity.Category
 
 internal fun CategoryDto.toEntity(): Category? {
     return Category(
-        id = id,
+        id = id.orEmpty(),
         name = name.orEmpty(),
         emoji = emoji.orEmpty(),
         isSelected = isSelected.orFalse()
