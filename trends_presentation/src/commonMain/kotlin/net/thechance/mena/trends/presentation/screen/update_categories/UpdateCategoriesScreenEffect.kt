f0cf229fbe1c054fde3f8bab1e68be1b1d1bc3cf
Yusuf Nasser <yusufnassereng@gmail.com>
2025-10-25 02:23:45 +0300

    TRN-79: add SaveFailure effect for update categories screen

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/update_categories/UpdateCategoriesScreenEffect.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/update_categories/UpdateCategoriesScreenEffect.kt
@@ -3,4 +3,5 @@ package net.thechance.mena.trends.presentation.screen.update_categories
 internal sealed interface UpdateCategoriesScreenEffect {
     data object NavigateBack : UpdateCategoriesScreenEffect
     data object SaveSuccess : UpdateCategoriesScreenEffect
+    data object SaveFailure : UpdateCategoriesScreenEffect
 }
