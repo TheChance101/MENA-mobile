6d2f976b9bc17c88383c4441cc41f9e8cf8cc0a0
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-10-24 16:36:06 +0300

    feat: Add ImageCacheManager to iOS DI module

--- identity_presentation/src/iosMain/kotlin/net/thechance/mena/identity/presentation/di/platformModule.ios.kt
+++ identity_presentation/src/iosMain/kotlin/net/thechance/mena/identity/presentation/di/platformModule.ios.kt
@@ -3,6 +3,7 @@ package net.thechance.mena.identity.presentation.di
 import net.thechance.mena.identity.presentation.util.LocationForegroundPermission
 import net.thechance.mena.identity.presentation.util.PermissionManager
 import net.thechance.mena.identity.presentation.util.permissionHandler.PermissionController
+import net.thechance.mena.identity.presentation.utils.ImageCacheManager
 import net.thechance.mena.identity.presentation.utils.ImageCacheManagerImpl
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
