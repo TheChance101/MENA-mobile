d803ec85376506082c78b28f4dd44e13af7dbff7
abdelrhman <abdelrhmanshabana999@gmail.com>
2025-10-24 17:01:41 +0300

    fix: add spacing and padding to list

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/categoryDukans/component/CategoryDukansList.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/categoryDukans/component/CategoryDukansList.kt
@@ -1,9 +1,12 @@
 package net.thechance.mena.dukan.presentation.screen.categoryDukans.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import app.cash.paging.compose.LazyPagingItems
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.dukan.presentation.component.shared.DukanCard
 import net.thechance.mena.dukan.presentation.viewModel.categoryDukans.CategoryDukansInteractionListener
 import net.thechance.mena.dukan.presentation.viewModel.categoryDukans.CategoryDukansUiState
@@ -16,7 +19,9 @@ fun CategoryDukansList(
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(
-        modifier = modifier
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(Theme.spacing._8),
+        contentPadding = PaddingValues(horizontal = Theme.spacing._16)
     ) {
         items(
             count = dukans.itemCount,
