cce0ea5a2c4fd4b26cafe5ca18992f9be64fb375
Malak Raef <malak.raef20@gmail.com>
2025-10-26 21:33:33 +0300

    feat: add koin module and environment configuration for admin panel

---
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/di/AppModule.kt
@@ -0,0 +1,8 @@
+package net.thechance.mena.admin_panel.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("net.thechance.mena.admin_panel")
+class AppModule
