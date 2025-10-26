8debe910cc336b423a64c101f462e34bec1754ee
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-10-26 10:48:06 +0300

    refactor: remove unused ImageCacheManager and its implementations

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/utils/ImageCacheManager.kt
+++
@@ -1,9 +0,0 @@
-package net.thechance.mena.identity.presentation.utils
-
-import androidx.compose.ui.graphics.ImageBitmap
-
-interface ImageCacheManager {
-    fun getCachedImage(key: String): ImageBitmap?
-    fun cacheImage(key: String, imageBitmap: ImageBitmap)
-    fun removeCachedImage(key: String)
-}
