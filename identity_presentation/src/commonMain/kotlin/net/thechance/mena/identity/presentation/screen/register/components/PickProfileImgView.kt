65ae79573733e1eaee65f37bca3ee70561b97912
shahad-97 <shos8406@gmail.com>
2025-10-27 01:22:33 +0300

    add PickProfileImgView composable

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/register/components/PickProfileImgView.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/register/components/PickProfileImgView.kt
@@ -1,2 +1,118 @@
 package net.thechance.mena.identity.presentation.screen.register.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.unit.dp
+import mena.identity_presentation.generated.resources.Res
+import mena.identity_presentation.generated.resources.button_skip_for_now
+import mena.identity_presentation.generated.resources.button_upload
+import mena.identity_presentation.generated.resources.desc_upload_profile_image
+import mena.identity_presentation.generated.resources.title_complete_your_profile
+import net.thechance.mena.designsystem.presentation.component.button.PrimaryButton
+import net.thechance.mena.designsystem.presentation.component.button.TextButton
+import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import net.thechance.mena.identity.presentation.components.AuthScreenContainer
+import net.thechance.mena.identity.presentation.components.PageDescription
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PickProfileImgView(
+    imageBitmap: ImageBitmap?,
+    onUploadClick: () -> Unit = {},
+    onSkipClick: () -> Unit = {},
+    onEditClick: () -> Unit = {}
+) {
+    val isImageLoaded = imageBitmap != null
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Theme.colorScheme.background.surface)
+    ) {
+        AuthScreenContainer(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+            PageDescription(
+                title = stringResource(Res.string.title_complete_your_profile),
+                subtitle = stringResource(Res.string.desc_upload_profile_image),
+            )
+
+            UploadImageContainer(
+                onClick = {onEditClick()},
+                image = imageBitmap,
+                modifier = Modifier
+                    .width(328.dp)
+                    .align (Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            PrimaryButton(
+                text = stringResource(Res.string.button_upload),
+                onClick = {onUploadClick()},
+                isEnabled = isImageLoaded,
+                contentPadding = PaddingValues(vertical = 13.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = Theme.spacing._12)
+            )
+
+            TextButton(
+                text = stringResource(Res.string.button_skip_for_now),
+                onClick = {onSkipClick()},
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PickProfileImgViewPreview() {
+    MenaTheme {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Theme.colorScheme.background.surface)) {
+
+            PickProfileImgView(
+                imageBitmap = null,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PickProfileImgViewPreview2() {
+    MenaTheme {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Theme.colorScheme.background.surface)) {
+
+            PickProfileImgView(
+                imageBitmap = ImageBitmap(1, 1),
+            )
+        }
+    }
+}
