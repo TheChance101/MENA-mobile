caf37916f73b9da4e231dc91a0eb5076512c624d
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-10-27 14:10:13 +0100

    feature: add share profile QR code dialog to `ProfileScreen`

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/profile/ProfileScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/profile/ProfileScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
+import coil3.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.delay
 import mena.identity_presentation.generated.resources.Res
 import mena.identity_presentation.generated.resources.dismiss
@@ -44,15 +45,16 @@ import net.thechance.mena.identity.presentation.screen.profile.components.Invite
 import net.thechance.mena.identity.presentation.screen.profile.components.OtherSettingsSection
 import net.thechance.mena.identity.presentation.screen.profile.components.ProfileInfoContainer
 import net.thechance.mena.identity.presentation.screen.profile.components.ShareIcon
+import net.thechance.mena.identity.presentation.screen.profile.components.ShareProfileQrCode
 import net.thechance.mena.identity.presentation.screen.register.RegisterScreen
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 class ProfileScreen : BaseScreen<
-        ProfileScreenViewModel,
-        ProfileScreenUIState,
-        ProfileScreenUIEffect,
-        ProfileScreenInteractionListener>() {
+    ProfileScreenViewModel,
+    ProfileScreenUIState,
+    ProfileScreenUIEffect,
+    ProfileScreenInteractionListener>() {
     @Composable
     override fun Content() {
         InitScreen(getScreenModel())
@@ -63,48 +65,63 @@ class ProfileScreen : BaseScreen<
         state: ProfileScreenUIState,
         listener: ProfileScreenInteractionListener,
     ) {
-        Scaffold(overlays =
-            {
-            bottomSheet(
-                isVisible = state.showShareBottomSheet
-            ) {
-                BottomSheet(
-                    isVisible = it,
-                    onDismissRequest = listener::onDismissBottomSheet,
-                ) {
-                    Column(
-                        Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+        Scaffold(
+            overlays =
+                {
+                    bottomSheet(
+                        isVisible = state.showShareBottomSheet
                     ) {
-                        Text(
-                            text = "Invite friends Not Yet Implemented",
-                            style = Theme.typography.label.small
-                        )
-                        NegativeButton(
-                            text = stringResource(Res.string.dismiss),
-                            onClick = listener::onDismissBottomSheet,
+                        BottomSheet(
+                            isVisible = it,
+                            onDismissRequest = listener::onDismissBottomSheet,
+                        ) {
+                            Column(
+                                Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    text = "Invite friends Not Yet Implemented",
+                                    style = Theme.typography.label.small
+                                )
+                                NegativeButton(
+                                    text = stringResource(Res.string.dismiss),
+                                    onClick = listener::onDismissBottomSheet,
+                                )
+                            }
+                        }
+                    }
+                    dialog(state.showLanguageDialog) {
+                        Dialog(
+                            isVisible = it,
+                            title = "HI",
+                            message = "Not Yet Implemented",
+                            onDismiss = listener::onDismissLanguageDialog,
+                            actionButtons = {}
                         )
                     }
-                }
-            }
-            dialog(state.showLanguageDialog) {
-                Dialog(
-                    isVisible = it,
-                    title = "HI",
-                    message = "Not Yet Implemented",
-                    onDismiss = listener::onDismissLanguageDialog,
-                    actionButtons = {}
-                )
-            }
-            dialog(state.showThemeDialog) {
-                Dialog(
-                    isVisible = it,
-                    title = "HI",
-                    message = "Not Yet Implemented",
-                    onDismiss = listener::onDismissThemeDialog,
-                    actionButtons = {}
-                )
-            }
-        }) {
+                    dialog(state.showThemeDialog) {
+                        Dialog(
+                            isVisible = it,
+                            title = "HI",
+                            message = "Not Yet Implemented",
+                            onDismiss = listener::onDismissThemeDialog,
+                            actionButtons = {}
+                        )
+                    }
+                    dialog(state.showShareProfileDialog) {
+                        ShareProfileQrCode(
+                            showDialog = it,
+                            qrCodePainter = rememberAsyncImagePainter(
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/QR_Code_Example.svg/2048px-QR_Code_Example.svg.png"
+                            ),
+                            onDismiss = listener::onDismissShareProfileDialog,
+                            fullName = "Hassan Nabil",
+                            onShareProfile = {},
+                            onClipboardContent = { },
+                            onDownload = {},
+                        )
+                    }
+                }) {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
@@ -183,7 +200,7 @@ class ProfileScreen : BaseScreen<
                 ) {
                     SnackBar(
                         title = stringResource(Res.string.error),
-                        message =  stringResource(state.errorMessage!!),
+                        message = stringResource(state.errorMessage!!),
                         leadingIcon = painterResource(Res.drawable.ic_close_circle),
                         modifier = Modifier.fillMaxWidth().padding(bottom = Theme.spacing._16)
                             .padding(horizontal = Theme.spacing._16)
