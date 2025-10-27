e74820e3ef97e9ae97bcc0448b93a58b16aebd07
Hend Sayed <hendsayed853@gmail.com>
2025-10-27 16:42:15 +0300

    refactor: Move network keys to a separate file

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/NetworkEndpoint.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/NetworkEndpoint.kt
@@ -15,9 +15,4 @@ internal object NetworkEndpoint {
 
     // Parameters
     const val PAGE_PARAMETER = "page"
-
-    // Keys
-    const val VIDEO = "video"
-    const val THUMBNAIL = "thumbnail"
-    const val THUMBNAIL_MIME_TYPE = "image/jpeg"
 }
