acae1e0eef7ab2b4011ca90989db82a543168a80
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-10-29 17:18:16 +0300

    fix: UserReelScreen

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelScreen.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelScreen.kt
@@ -1,15 +1,19 @@
 package net.thechance.mena.trends.presentation.screen.user_reel
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -40,6 +44,7 @@ import app.cash.paging.compose.collectAsLazyPagingItems
 import coil3.compose.rememberAsyncImagePainter
 import mena.trends_presentation.generated.resources.Res
 import mena.trends_presentation.generated.resources.avatar_image
+import mena.trends_presentation.generated.resources.back_arrow
 import mena.trends_presentation.generated.resources.confirmation_message
 import mena.trends_presentation.generated.resources.delete
 import mena.trends_presentation.generated.resources.delete_reel
@@ -49,19 +54,20 @@ import mena.trends_presentation.generated.resources.ic_arrow_left
 import mena.trends_presentation.generated.resources.ic_delete
 import mena.trends_presentation.generated.resources.ic_eye
 import mena.trends_presentation.generated.resources.ic_like
+import mena.trends_presentation.generated.resources.just_now
 import mena.trends_presentation.generated.resources.react
 import mena.trends_presentation.generated.resources.success_delete_message
 import mena.trends_presentation.generated.resources.success_delete_title
 import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
 import net.thechance.mena.designsystem.presentation.component.dialog.Dialog
 import net.thechance.mena.designsystem.presentation.component.icon.Icon
-import net.thechance.mena.designsystem.presentation.component.image.Image
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.trends.presentation.navigation.LocalNavController
 import net.thechance.mena.trends.presentation.navigation.Route
 import net.thechance.mena.trends.presentation.shared.util.ObserveAsEffect
+import net.thechance.mena.trends.presentation.shared.util.asString
 import net.thechance.mena.trends.presentation.shared.util.gradientShadow
 import net.thechance.mena.trends.presentation.video_player.VideoPlayer
 import org.jetbrains.compose.resources.painterResource
@@ -116,7 +122,7 @@ private fun UserReelScreenContent(
                             modifier = Modifier
                                 .align(Alignment.End)
                                 .padding(
-                                    top = 24.dp,
+                                    top = Theme.spacing._24,
                                     bottom = Theme.spacing._12,
                                     end = Theme.spacing._8
                                 )
@@ -217,7 +223,7 @@ private fun TopAppBar(
         leadingContent = {
             Icon(
                 painter = painterResource(resource = Res.drawable.ic_arrow_left),
-                contentDescription = "Back Icon",
+                contentDescription = stringResource(resource = Res.string.back_arrow),
             )
         },
         onLeadingClick = { onBackClick() }
@@ -241,7 +247,12 @@ private fun ReelContent(
         isReelVisible = shouldRender,
         onVideoPlaying = incrementViewsCount
     ) {
-        Box(Modifier.fillMaxSize()) {
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            val isLandscape = maxWidth > maxHeight
+
             UsersReact(
                 viewCount = reel.viewsCount.toString(),
                 likeCount = reel.likesCount.toString(),
@@ -249,13 +260,17 @@ private fun ReelContent(
                 onDeleteClick = onDeleteClick,
                 onLikeClick = onLikeClick,
                 isLiked = reel.isLiked,
-                modifier = Modifier.align(Alignment.BottomEnd)
-                    .padding(end = Theme.spacing._16, bottom = 140.dp)
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(
+                        end = Theme.spacing._16,
+                        bottom = if (isLandscape) 60.dp else 140.dp
+                    )
             )
 
             PublisherInfo(
                 userName = reel.username,
-                timeOfPublish = reel.createdAt.toString(),
+                timeOfPublish = reel.createdAt?.asString() ?: stringResource(resource = Res.string.just_now),
                 description = reel.description,
                 avatar = reel.profileImageUrl,
                 modifier = Modifier.align(Alignment.BottomCenter),
@@ -347,12 +362,19 @@ private fun UsersReact(
 ) {
     Column(
         modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(Theme.spacing._24)
     ) {
+
+        val likeIconColor by animateColorAsState(
+            targetValue = if (isLiked) Color.White else Theme.colorScheme.shadeTertiary,
+            animationSpec = tween(durationMillis = 500)
+        )
+
         ReActIcon(
             icon = painterResource(resource = Res.drawable.ic_like),
             onClick = onLikeClick,
-            tint = if (isLiked) Color.White else Theme.colorScheme.shadeTertiary,
+            tint = likeIconColor,
             label = likeCount
         )
 
