64a4da1633e18f73e8e194315a96d1d10757a570
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-29 18:50:46 +0300

    feat: remove admin panel scaffold

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/LoginScreen.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/LoginScreen.kt
@@ -1,7 +1,6 @@
 package net.thechance.mena.admin_panel.presentation.screen.login
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,19 +13,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import net.thechance.mena.admin_panel.presentation.component.AdminPanelScaffold
 import net.thechance.mena.admin_panel.presentation.screen.login.component.LoginHeader
 import net.thechance.mena.admin_panel.presentation.screen.login.component.PasswordInputField
 import net.thechance.mena.admin_panel.presentation.screen.login.component.UsernameInputField
 import net.thechance.mena.admin_panel.resources.Res
 import net.thechance.mena.admin_panel.resources.login
 import net.thechance.mena.designsystem.presentation.component.button.PrimaryButton
+import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun LoginScreen() {
-    AdminPanelScaffold {
+    Scaffold {
         var username by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
         var isPasswordVisible by remember { mutableStateOf(false) }
@@ -50,7 +49,7 @@ fun LoginScreen() {
                 PrimaryButton(
                     modifier = Modifier.align(Alignment.End),
                     text = stringResource(Res.string.login),
-                    onClick = {  },
+                    onClick = { },
                     isLoading = false,
                     isEnabled = false,
                     contentPadding = PaddingValues(horizontal = 16.dp, vertical = 13.dp)
