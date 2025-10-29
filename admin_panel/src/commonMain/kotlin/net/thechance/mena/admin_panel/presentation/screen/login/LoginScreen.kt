a75a6f9d6bc90e635f14c2c02ab834560a908421
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-29 17:51:27 +0300

    feat: add login btn

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/LoginScreen.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/LoginScreen.kt
@@ -3,6 +3,7 @@ package net.thechance.mena.admin_panel.presentation.screen.login
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -16,7 +17,11 @@ import androidx.compose.ui.unit.dp
 import net.thechance.mena.admin_panel.presentation.component.AdminPanelScaffold
 import net.thechance.mena.admin_panel.presentation.screen.login.component.PasswordInputField
 import net.thechance.mena.admin_panel.presentation.screen.login.component.UsernameInputField
+import net.thechance.mena.admin_panel.resources.Res
+import net.thechance.mena.admin_panel.resources.login
+import net.thechance.mena.designsystem.presentation.component.button.PrimaryButton
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun LoginScreen() {
@@ -29,18 +34,27 @@ fun LoginScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            UsernameInputField(
-                username = username,
-                onChangeValue = { username = it }
-            )
-            PasswordInputField(
-                modifier = Modifier.padding(top = 24.dp, bottom = 40.dp),
-                password = password,
-                isPasswordVisible = isPasswordVisible,
-                onVisiblePasswordBtnClicked = { isPasswordVisible = !isPasswordVisible },
-                onChangeValue = { password = it }
-            )
-
+            Column {
+                UsernameInputField(
+                    username = username,
+                    onChangeValue = { username = it }
+                )
+                PasswordInputField(
+                    modifier = Modifier.padding(top = 24.dp, bottom = 40.dp),
+                    password = password,
+                    isPasswordVisible = isPasswordVisible,
+                    onVisiblePasswordBtnClicked = { isPasswordVisible = !isPasswordVisible },
+                    onChangeValue = { password = it }
+                )
+                PrimaryButton(
+                    modifier = Modifier.align(Alignment.End),
+                    text = stringResource(Res.string.login),
+                    onClick = {  },
+                    isLoading = false,
+                    isEnabled = false,
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 13.dp)
+                )
+            }
         }
     }
 }
