e0502af01078480ffa87561e6d7d4a69e488256f
yousef_osama11 <oyousef709@gmail.com>
2025-10-29 14:06:14 +0300

    refactor: remove unnecessary composables in chat actions menu overlay

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/components/ChatActionsMenuOverlay.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/components/ChatActionsMenuOverlay.kt
@@ -1,5 +1,6 @@
 package net.thechance.mena.core_chat.presentation.screen.chat.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
@@ -10,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import mena.core_chat_presentation.generated.resources.Res
@@ -38,9 +41,23 @@ fun ScaffoldScope.chatActionsMenuOverlay(
             onDismiss = actionsMenuInteractionListener::onChatActionsMenuDialogDismissed,
             onCancelClick = actionsMenuInteractionListener::onChatActionsMenuDialogDismissed,
             contentPadding = PaddingValues(Theme.spacing._12),
-            actionButtons = { DeleteActionMenuItem(onDeleteChatClicked = actionsMenuInteractionListener::onDeleteChatClicked) }
+            actionButtons = {
+                ActionMenuItem(
+                    icon = painterResource(Res.drawable.ic_delete),
+                    text = stringResource(Res.string.delete_chat),
+                    onClick = actionsMenuInteractionListener::onDeleteChatClicked,
+                    contentColor = Theme.colorScheme.error,
+                    modifier = Modifier.padding(bottom = Theme.spacing._12)
+                )
+            }
         ) {
-            DeleteChatContent()
+            Text(
+                text = stringResource(Res.string.actions),
+                style = Theme.typography.title.small,
+                color = Theme.colorScheme.shadePrimary,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().padding(vertical = Theme.spacing._12)
+            )
         }
     }
     dialog(isVisible = showConfirmDeleteChatDialog) {
@@ -53,28 +70,6 @@ fun ScaffoldScope.chatActionsMenuOverlay(
             ConfirmDeleteChatContent(onConfirmDeleteChatClicked = actionsMenuInteractionListener::onConfirmDeleteChatClicked)
         }
     }
-}
-
-@Composable
-private fun DeleteActionMenuItem(onDeleteChatClicked: () -> Unit) {
-    ActionMenuItem(
-        icon = painterResource(Res.drawable.ic_delete),
-        text = stringResource(Res.string.delete_chat),
-        onClick = onDeleteChatClicked,
-        contentColor = Theme.colorScheme.error,
-        modifier = Modifier.padding(bottom = Theme.spacing._12)
-    )
-}
-
-@Composable
-private fun DeleteChatContent() {
-    Text(
-        text = stringResource(Res.string.actions),
-        style = Theme.typography.title.small,
-        color = Theme.colorScheme.shadePrimary,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.fillMaxWidth().padding(vertical = Theme.spacing._12)
-    )
 }
 
 @Composable
@@ -94,22 +89,20 @@ private fun ConfirmDeleteChatContent(onConfirmDeleteChatClicked: () -> Unit) {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth().padding(bottom = Theme.spacing._12)
         )
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Spacer(Modifier.weight(1f))
-            Text(
-                text = stringResource(Res.string.delete),
-                style = Theme.typography.label.medium,
-                color = Theme.colorScheme.error,
-                modifier = Modifier
-                    .padding(vertical = Theme.spacing._12)
-                    .padding(end = 8.dp)
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                        onClick = onConfirmDeleteChatClicked
-                    )
-            )
-        }
+        Text(
+            text = stringResource(Res.string.delete),
+            style = Theme.typography.label.medium,
+            color = Theme.colorScheme.error,
+            modifier = Modifier
+                .padding(vertical = Theme.spacing._12)
+                .padding(end = 8.dp)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = onConfirmDeleteChatClicked
+                )
+                .align(Alignment.End)
+        )
     }
 }
 
