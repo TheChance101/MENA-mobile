94452aefef815a98846929643374c3a7be51df84
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-29 16:53:50 +0300

    feat: add username input field

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/LoginScreen.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/LoginScreen.kt
@@ -1,14 +1,26 @@
 package net.thechance.mena.admin_panel.presentation.screen.login
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import net.thechance.mena.admin_panel.presentation.component.AdminPanelScaffold
+import net.thechance.mena.admin_panel.presentation.screen.login.component.UsernameInputField
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 
 @Composable
 fun LoginScreen() {
     AdminPanelScaffold {
-
+        var username by remember { mutableStateOf("") }
+        Column {
+            UsernameInputField(
+                username = username,
+                onChangeValue = { username = it }
+            )
+        }
     }
 }
 
---
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/login/component/UsernameInputField.kt
@@ -0,0 +1,42 @@
+package net.thechance.mena.admin_panel.presentation.screen.login.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.thechance.mena.admin_panel.resources.Res
+import net.thechance.mena.admin_panel.resources.ic_user
+import net.thechance.mena.admin_panel.resources.username
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.component.textField.TextField
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun UsernameInputField(
+    username: String,
+    onChangeValue: (String) -> Unit,
+) {
+    Column{
+        Text(
+            modifier = Modifier.padding(bottom = Theme.spacing._4),
+            text = stringResource(Res.string.username),
+            style = Theme.typography.title.small,
+            color = Theme.colorScheme.shadePrimary
+        )
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(0.4f)
+                .height(48.dp),
+            value = username,
+            hint = "",
+            onValueChanged = { onChangeValue(it) },
+            leadingIcon = painterResource(Res.drawable.ic_user),
+            showTrailingDivider = false
+        )
+    }
+}
