02d5897a5c95164835a98bbed4584043ceb350c9
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-10-30 18:39:11 +0300

    refactor: make modifier first

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/payment_result/component/PaymentSuccessContent.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/payment_result/component/PaymentSuccessContent.kt
@@ -48,10 +48,10 @@ internal fun PaymentSuccessContent(
             )
         }
         PaymentActionButtons(
+            modifier = Modifier.align(Alignment.BottomCenter),
             primaryButtonText = stringResource(Res.string.show_transaction_details),
             onPrimaryButtonClick = interactionListener::onShowTransactionDetailsClicked,
             onCancelClicked = interactionListener::onCloseClicked,
-            modifier = Modifier.align(Alignment.BottomCenter),
             isLoading = state.isLoading,
             isCloseEnabled = state.isCloseButtonEnabled
         )
--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/payment_result/component/PaymentUnknownErrorContent.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/payment_result/component/PaymentUnknownErrorContent.kt
@@ -47,10 +47,10 @@ internal fun PaymentUnknownErrorContent(
             )
         }
         PaymentActionButtons(
+            modifier = Modifier.align(Alignment.BottomCenter),
             primaryButtonText = stringResource(Res.string.try_again),
             onPrimaryButtonClick = interactionListener::onTryAgainClicked,
             onCancelClicked = interactionListener::onCloseClicked,
-            modifier = Modifier.align(Alignment.BottomCenter),
             isLoading = state.isLoading,
             isCloseEnabled = state.isCloseButtonEnabled,
             isTryAgainEnabled = state.isTryAgainButtonEnabled
