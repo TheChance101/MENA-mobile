c27ac8b7fec2f67a136fcd5d462899dd71be575b
Shahd-Hatem <Shahdhatim863@gmail.com>
2025-10-30 12:13:50 +0300

    remove UserRepository and UserEntity from trendsDomain

--- trends_domain/src/commonMain/kotlin/net/thechance/mena/trends/domain/entity/User.kt
+++
@@ -1,8 +0,0 @@
-package net.thechance.mena.trends.domain.entity
-
-data class User(
-    val username: String,
-    val firstName: String,
-    val lastName: String,
-    val profileImageUrl: String
-)
