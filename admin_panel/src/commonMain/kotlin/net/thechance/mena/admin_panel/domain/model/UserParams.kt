711beb30da858d209d4d50538f7cd9b3104e55d1
gehadepada <gehadepada@gmail.com>
2025-10-31 18:53:14 +0200

    feat:add user params class and user repo interface

---
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/domain/model/UserParams.kt
@@ -0,0 +1,8 @@
+package net.thechance.mena.admin_panel.domain.model
+
+data class UserParams(
+    val searchInput: String?,
+    val sortType: String?,
+    val sortDirection: String?
+
+)
--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/domain/repository/user/UserRepository.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/domain/repository/user/UserRepository.kt
@@ -1,7 +1,9 @@
 package net.thechance.mena.admin_panel.domain.repository.user
 
+
 import net.thechance.mena.admin_panel.domain.entity.user.User
+import net.thechance.mena.admin_panel.domain.model.UserParams
 
 interface UserRepository {
-    suspend fun getAllUser():List<User>
+    suspend fun getUsers(userParams: UserParams?):List<User>
 }
