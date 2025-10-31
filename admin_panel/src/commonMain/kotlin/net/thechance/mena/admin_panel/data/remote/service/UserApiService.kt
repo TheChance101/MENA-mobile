34092ed8b0988c3fdb2da49c7401ffb126bb8cdb
gehadepada <gehadepada@gmail.com>
2025-11-01 01:34:48 +0200

    refactor:remove extra lines

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/remote/service/UserApiService.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/remote/service/UserApiService.kt
@@ -10,7 +10,6 @@ import net.thechance.mena.admin_panel.data.remote.dto.PagedResponse
 import net.thechance.mena.admin_panel.data.remote.dto.user.UserResponse
 
 interface UserApiService {
-
     @GET(USER_BASE)
     suspend fun getUsers(
         @Query("query") query: String? = null,
@@ -28,7 +27,5 @@ interface UserApiService {
     private companion object {
         const val USER_BASE = "identity/admin/users"
         const val UPDATE_USER_STATUS_ENDPOINT = "${USER_BASE}/{userId}/status"
-
     }
-
 }
