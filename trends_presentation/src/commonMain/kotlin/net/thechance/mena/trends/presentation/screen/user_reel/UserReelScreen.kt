1118efdee4f9219de6923dfa446099763b1740a9
Hend Sayed <hendsayed853@gmail.com>
2025-10-31 12:39:18 +0200

    feat: Use primary color for video player background

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelScreen.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/user_reel/UserReelScreen.kt
@@ -249,7 +249,7 @@ private fun ReelContent(
     onLikeClick: () -> Unit,
 ) {
     VideoPlayer(
-        modifier = Modifier.background(Color.Black),
+        modifier = Modifier.background(Theme.colorScheme.primary.primary),
         url = reel.videoUrl,
         isReelVisible = shouldRender,
         onVideoPlaying = incrementViewsCount
