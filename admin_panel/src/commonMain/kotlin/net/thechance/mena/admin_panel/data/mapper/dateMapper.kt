373b50589c17f726541004d96692e9e6756e5801
gehadepada <gehadepada@gmail.com>
2025-11-01 01:26:34 +0200

    refactor:remove extra lines

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/mapper/dateMapper.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/mapper/dateMapper.kt
@@ -3,8 +3,7 @@ package net.thechance.mena.admin_panel.data.mapper
 import kotlinx.datetime.LocalDate
 
 fun parseLocalDateOrDefault(value: String?) = value?.let {
-    runCatching { LocalDate.parse(it) }
-        .getOrDefault(DEFAULT_DATE)
+    runCatching { LocalDate.parse(it) }.getOrDefault(DEFAULT_DATE)
 } ?: DEFAULT_DATE
 
 private val DEFAULT_DATE = LocalDate(9999, 12, 31)
