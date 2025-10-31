da6c71498376eac0d4e1cc21168a1d35feb8ab09
Radwa85 <radwa.mohamed5033@gmail.com>
2025-10-31 22:13:38 +0200

    add onMessageLongClick to ChatListItem

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/components/ChatListItem.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/screen/chat/components/ChatListItem.kt
@@ -25,6 +25,7 @@ fun ChatListItem(
     onMessageClick: (Uuid) -> Unit,
     onMessageImageClick: (List<MessageUiState>, Int) -> Unit,
     onFailedMessageClick: (MessageUiState) -> Unit,
+    onMessageLongClick: (MessageUiState) -> Unit,
     modifier: Modifier = Modifier
 ) {
     when (item) {
@@ -52,6 +53,7 @@ fun ChatListItem(
                     showMessageInfo = (markedMessage.isVisibleMessageInfo || markedMessage.isLastInSeries || markedMessage.status == MessageStatus.FAILED),
                     isMarkedLastInSeries = markedMessage.isLastInSeries,
                     onMessageClick = { onMessageClick(markedMessage.id) },
+                    onMessageLongClick = { onMessageLongClick(markedMessage) },
                     onFailClick = { onFailedMessageClick(markedMessage) },
                 )
             }
