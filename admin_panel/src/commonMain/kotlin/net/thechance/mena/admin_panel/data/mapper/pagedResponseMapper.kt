10937c38d653872dccb12a1564bc183ded66a797
gehadepada <gehadepada@gmail.com>
2025-11-01 01:32:56 +0200

    Update admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/mapper/pagedResponseMapper.kt
    Co-authored-by: Ahmed Mohamed Hosny <101954425+Ahmed7osny1@users.noreply.github.com>

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/mapper/pagedResponseMapper.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/data/mapper/pagedResponseMapper.kt
@@ -3,6 +3,5 @@ package net.thechance.mena.admin_panel.data.mapper
 import net.thechance.mena.admin_panel.data.remote.dto.PagedResponse
 import kotlin.collections.orEmpty
 
-fun <DTO, ENTITY> PagedResponse<DTO>.toEntityList(mapper: DTO.() -> ENTITY): List<ENTITY> {
-    return this.items?.map { it.mapper() }.orEmpty()
-}
+fun <DTO, ENTITY> PagedResponse<DTO>.toEntityList(mapper: DTO.() -> ENTITY): List<ENTITY> =
+    this.items?.map { it.mapper() }.orEmpty()
