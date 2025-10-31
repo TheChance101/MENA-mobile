3330c679b0ec02c21c1c733e0e2291f51c070dee
Ahmed-AlMancy <ahmedalmancy20@gmail.com>
2025-10-31 18:47:59 +0200

    feat: implement product click listener in shelf details - navigate to product details when product is clicked

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/shelfDetails/ShelfDetailsEffects.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/shelfDetails/ShelfDetailsEffects.kt
@@ -1,5 +1,8 @@
 package net.thechance.mena.dukan.presentation.viewModel.shelfDetails
 
+import net.thechance.mena.dukan.presentation.viewModel.dukanDetails.DukanDetailsEffects
+
 sealed class ShelfDetailsEffects {
     object NavigateBack : ShelfDetailsEffects()
+    data class NavigateToProductDetails(val productId: String) : ShelfDetailsEffects()
 }
--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/shelfDetails/ShelfDetailsInteractionListener.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/shelfDetails/ShelfDetailsInteractionListener.kt
@@ -3,4 +3,5 @@ package net.thechance.mena.dukan.presentation.viewModel.shelfDetails
 interface ShelfDetailsInteractionListener {
     fun onBackClicked()
     fun onAddToCartClick(productId: String)
+    fun onProductClicked(productId: String)
 }
