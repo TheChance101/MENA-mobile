64c0932f24a9a0be1b49a2eb15d5e67dd3f7c35a
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-11-01 08:45:32 +0200

    refactor: Move reset password DTOs to request/response packages

--- identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/dto/resetPassword/request/VerifyOtpRequestDto.kt
+++ identity_data/src/commonMain/kotlin/net/thechance/mena/identity/data/dto/resetPassword/request/VerifyOtpRequestDto.kt
@@ -1,10 +1,12 @@
-package net.thechance.mena.identity.data.dto.resetPassword
+package net.thechance.mena.identity.data.dto.resetPassword.request
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class OtpResponse(
+class VerifyOtpRequestDto (
+    @SerialName("otp")
+    val otp: String,
     @SerialName("sessionId")
     val sessionId: String
 )
