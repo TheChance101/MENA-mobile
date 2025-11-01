127fc332f4bb6fa063e303f5d04cf9a92f0e0916
Farah315 <fara734isgreat@gmail.com>
2025-11-01 13:13:58 +0200

    fix: remove unused files

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/App.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/App.kt
@@ -2,12 +2,13 @@ package net.thechance.mena.admin_panel
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import net.thechance.mena.admin_panel.presentation.screen.users_management.UsersManagementScreen
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 
 @Composable
 @Preview
 fun App(){
     MenaTheme {
-       AdminPanelEntryPoint()
+        UsersManagementScreen()
     }
 }
