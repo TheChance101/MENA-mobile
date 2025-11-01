584b9a512482165e94a5840eaf2f344de5862d77
a7med naser <ad0112727@gmail.com>
2025-11-01 18:58:35 +0200

    feat(animation): Add horizontal and vertical slide transitions

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/animation/FadeAnimations.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/util/animation/FadeAnimations.kt
@@ -7,11 +7,16 @@ import androidx.compose.animation.core.EaseInSine
 import androidx.compose.animation.core.EaseOut
 import androidx.compose.animation.core.EaseOutCubic
 import androidx.compose.animation.core.EaseOutSine
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
+import androidx.compose.ui.unit.IntOffset
 
 fun fadeTransitionSpec(): ContentTransform {
     return fadeIn(
@@ -54,5 +59,42 @@ fun fadeWithSlideTransition(): ContentTransform {
         animationSpec = tween(
             easing = EaseOutSine
         )
+    )
+}
+
+fun fadeWithSlideHorizontalTransition(toRight: Boolean): ContentTransform {
+    val duration = 700
+    val easing = LinearEasing
+    val animationSpec = tween<Float>(durationMillis = duration, easing = easing)
+    val slideAnimationSpec = tween<IntOffset>(durationMillis = duration, easing = easing)
+
+    return slideInHorizontally (
+        animationSpec = slideAnimationSpec,
+        initialOffsetX = { fullWidth -> if (toRight) fullWidth * 2 else -fullWidth * 2 }
+    ) + fadeIn(
+        animationSpec = animationSpec
+    ) togetherWith slideOutHorizontally(
+        animationSpec = slideAnimationSpec,
+        targetOffsetX = { fullWidth -> if (toRight) -fullWidth * 2 else fullWidth * 2 }
+    ) + fadeOut(
+        animationSpec = animationSpec
+    )
+}
+
+
+
+fun fadeWithSlideVerticalTransition(): ContentTransform {
+    val animationSpec = tween<Float>(durationMillis = 500)
+
+    return slideInVertically(
+        animationSpec = tween(durationMillis = 500, easing = EaseOut),
+        initialOffsetY = { fullHeight -> -fullHeight }
+    ) + fadeIn(
+        animationSpec = animationSpec
+    ) togetherWith slideOutVertically(
+        animationSpec = tween(durationMillis = 500, easing = EaseIn),
+        targetOffsetY = { fullHeight -> -fullHeight }
+    ) + fadeOut(
+        animationSpec = animationSpec
     )
 }
