c3389195e2c8240fe43f9d3fb803b170b58b38d0
Asmaa.Khaled <asmaa.kh.gomaa@gmail.com>
2025-11-01 16:31:58 +0200

    feat: Add ProductImageModel interface

---
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/product/productImage/ProductImageModel.kt
@@ -0,0 +1,12 @@
+package net.thechance.mena.dukan.presentation.component.product.productImage
+
+import androidx.compose.ui.graphics.ImageBitmap
+
+interface ProductImageModel {
+    val id: Long
+    val image: ImageBitmap
+    val imageSizeInMegaByte: Double
+    val imageState: ProductImageState
+    val errorMessage: String?
+}
+
