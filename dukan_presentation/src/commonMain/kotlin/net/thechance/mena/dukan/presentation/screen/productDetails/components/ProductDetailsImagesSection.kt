9cfc3df2dce53317bb014c2f4dc074fdf1f6a3d3
Ahmed-AlMancy <ahmedalmancy20@gmail.com>
2025-11-02 18:39:20 +0200

    Feat: Add ProductDetailsViewModel and fix image clipping

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/productDetails/components/ProductDetailsImagesSection.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/productDetails/components/ProductDetailsImagesSection.kt
@@ -111,8 +111,8 @@ fun ProductDetailsSecondaryImages(
     LazyRow(
         modifier = modifier
             .height(64.dp)
-            .background(Theme.colorScheme.background.surfaceLow)
-            .clip(RoundedCornerShape(Theme.radius.md)),
+            .clip(RoundedCornerShape(Theme.radius.md))
+            .background(Theme.colorScheme.background.surfaceLow),
         horizontalArrangement = Arrangement.spacedBy(Theme.spacing._4),
         verticalAlignment = Alignment.CenterVertically,
         contentPadding = PaddingValues(horizontal = Theme.spacing._4)
