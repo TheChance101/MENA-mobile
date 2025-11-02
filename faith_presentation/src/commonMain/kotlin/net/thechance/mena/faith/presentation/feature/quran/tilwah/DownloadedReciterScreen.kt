5ddbdf443784d5be6d7908185b4a648a557c4c80
Basant Mohamed <bassant.22230@gmail.com>
2025-11-02 17:12:17 +0200

    refactor: split components and add their preview

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/quran/tilwah/DownloadedReciterScreen.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/quran/tilwah/DownloadedReciterScreen.kt
@@ -1,43 +1,26 @@
 package net.thechance.mena.faith.presentation.feature.quran.tilwah
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import mena.faith_presentation.generated.resources.Res
 import mena.faith_presentation.generated.resources.arrow_left
 import mena.faith_presentation.generated.resources.ic_arrow_left
-import mena.faith_presentation.generated.resources.ic_search
-import mena.faith_presentation.generated.resources.ic_tick_double_check
-import mena.faith_presentation.generated.resources.search_icon
 import net.thechance.mena.designsystem.presentation.component.appBar.AppBar
-import net.thechance.mena.designsystem.presentation.component.button.radioButton.RadioButton
 import net.thechance.mena.designsystem.presentation.component.icon.Icon
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
-import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.faith.presentation.base.ObserveAsEffect
 import net.thechance.mena.faith.presentation.designSystem.theme.QuranTheme
+import net.thechance.mena.faith.presentation.feature.quran.tilwah.component.ReciterItem
+import net.thechance.mena.faith.presentation.feature.quran.tilwah.component.TilawahTopBar
 import net.thechance.mena.faith.presentation.navigation.LocalNavController
 import net.thechance.mena.faith.presentation.navigation.Route
 import org.jetbrains.compose.resources.painterResource
@@ -105,90 +88,10 @@ fun Content(
     }
 }
 
-@Composable
-private fun TilawahTopBar(onSearchClick: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .size(40.dp)
-            .clip(RoundedCornerShape(Theme.radius.md))
-            .background(Theme.colorScheme.background.surfaceLow)
-            .clickable(onClick = onSearchClick),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            modifier = Modifier.size(20.dp),
-            painter = painterResource(Res.drawable.ic_search),
-            contentDescription = stringResource(Res.string.search_icon)
-        )
-    }
-}
-
-@Composable
-private fun ReciterItem(
-    reciter: String,
-    recitingType: String,
-    isDownloaded: Boolean,
-    isSelected: Boolean,
-    onSelect: () -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(min = 56.dp)
-            .padding(horizontal = Theme.spacing._16)
-            .padding(bottom = Theme.spacing._8)
-            .background(
-                color = Theme.colorScheme.background.surfaceLow,
-                shape = RoundedCornerShape(Theme.radius.md)
-            )
-            .clickable(onClick = onSelect)
-            .padding(Theme.spacing._8),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(Theme.spacing._8)
-    ) {
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text(
-                text = reciter,
-                style = Theme.typography.label.medium,
-                color = Theme.colorScheme.shadePrimary
-            )
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(Theme.spacing._4)
-            ) {
-                Text(
-                    text = recitingType,
-                    style = Theme.typography.label.medium,
-                    color = Theme.colorScheme.shadePrimary
-                )
-                if (isDownloaded) {
-                    Icon(
-                        painterResource(Res.drawable.ic_tick_double_check),
-                        contentDescription = "",
-                        modifier = Modifier.size(Theme.spacing._12)
-                    )
-
-                    Text(
-                        text = "Downloaded",
-                        style = Theme.typography.label.small,
-                        color = Theme.colorScheme.success
-                    )
-                }
-            }
-        }
-
-        RadioButton(
-            isSelected = isSelected,
-            onClick = onSelect
-        )
-    }
-}
 
 @Preview
 @Composable
-fun Preview() {
+private fun Preview() {
     QuranTheme {
         Content(
             uiState = TilawahUiState(
