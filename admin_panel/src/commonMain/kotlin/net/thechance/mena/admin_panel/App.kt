2a1c35f64ba94295e50e5420e376a9138f454183
Malak Raef <malak.raef20@gmail.com>
2025-11-02 20:07:13 +0200

    feat: format dates in users management screen

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/App.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/App.kt
@@ -1,15 +1,15 @@
 package net.thechance.mena.admin_panel
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import net.thechance.mena.admin_panel.presentation.screen.users_management.UsersManagementScreen
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import kotlin.uuid.ExperimentalUuidApi
 
-@OptIn(ExperimentalUuidApi::class)
+@Preview
 @Composable
 fun App(){
     MenaTheme {
         UsersManagementScreen()
-
     }
 }
