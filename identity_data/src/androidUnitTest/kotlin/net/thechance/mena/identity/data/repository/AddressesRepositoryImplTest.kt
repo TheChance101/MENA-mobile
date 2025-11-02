2e885c120849e123c73aaf28532713025ed6551b
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-11-02 08:13:22 +0200

    refactor: Update OtpResponse import path in RegisterRepositoryImplTest

--- identity_data/src/androidUnitTest/kotlin/net/thechance/mena/identity/data/repository/AddressesRepositoryImplTest.kt
+++ identity_data/src/androidUnitTest/kotlin/net/thechance/mena/identity/data/repository/AddressesRepositoryImplTest.kt
@@ -14,17 +14,16 @@ import net.thechance.mena.identity.data.repository.location.AddressesRepositoryI
 import net.thechance.mena.identity.data.repository.location.GeocoderWrapper
 import net.thechance.mena.identity.data.utils.mockHttpClient
 import net.thechance.mena.identity.data.utils.mockHttpClientError
-import net.thechance.mena.identity.domain.entity.Address
 import net.thechance.mena.identity.domain.entity.AddressType
 import net.thechance.mena.identity.domain.exception.AddressNotFoundException
 import net.thechance.mena.identity.domain.exception.UnAuthorizedException
 import net.thechance.mena.identity.domain.model.AddressInput
-import net.thechance.mena.identity.domain.model.Coordinates as DomainCoordinates
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
+import net.thechance.mena.identity.domain.model.Coordinates as DomainCoordinates
 
 @OptIn(ExperimentalUuidApi::class)
 class AddressesRepositoryImplTest {
