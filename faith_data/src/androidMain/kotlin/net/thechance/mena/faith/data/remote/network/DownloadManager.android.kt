1581f573117a5b2dc9aad0a1513cdaa73b40d538
Abdallah Bakhet <abakhet2010@gmail.com>
2025-11-02 14:09:45 +0200

    refactor: Simplify file download logic by removing zip extraction

--- faith_data/src/androidMain/kotlin/net/thechance/mena/faith/data/remote/network/DownloadManager.android.kt
+++ faith_data/src/androidMain/kotlin/net/thechance/mena/faith/data/remote/network/DownloadManager.android.kt
@@ -3,20 +3,19 @@ package net.thechance.mena.faith.data.remote.network
 import android.content.Context
 import org.koin.mp.KoinPlatform.getKoin
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.net.URL
-import java.util.zip.ZipInputStream
 
-actual suspend fun downloadFileIntoPrivateStorage(
+actual suspend fun downloadSurahFileToAppStorage(
     url: String,
     fileName: String,
-    isZipFile: Boolean,
 ): String? {
     val context: Context = getKoin().get()
     return try {
-        val file =
-            File(context.filesDir, fileName) // context.filesDir points to private internal storage
+        val file = File(context.filesDir, fileName)
+
+        file.parentFile?.mkdirs()
+
         val connection = URL(url).openConnection()
         connection.connect()
 
@@ -28,37 +27,9 @@ actual suspend fun downloadFileIntoPrivateStorage(
         inputStream.close()
         outputStream.close()
 
-        // Extract ZIP
-        val extractedDir = File(context.filesDir, "extracted")
-        if (!extractedDir.exists()) extractedDir.mkdirs()
-
-        unzip(file, extractedDir)
-
-        // Return extracted path
-        return extractedDir.absolutePath
+        file.absolutePath
     } catch (e: Exception) {
         e.printStackTrace()
         null
-    }
-}
-
-private fun unzip(
-    zipFile: File,
-    targetDir: File,
-) {
-    ZipInputStream(FileInputStream(zipFile)).use { zipStream ->
-        var entry = zipStream.nextEntry
-        while (entry != null) {
-            val outFile = File(targetDir, entry.name)
-            if (entry.isDirectory) {
-                outFile.mkdirs()
-            } else {
-                outFile.parentFile?.mkdirs()
-                FileOutputStream(outFile).use { out ->
-                    zipStream.copyTo(out)
-                }
-            }
-            entry = zipStream.nextEntry
-        }
     }
 }
