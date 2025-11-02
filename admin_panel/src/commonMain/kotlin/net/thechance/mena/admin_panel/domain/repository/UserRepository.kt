a31fe27bfe36f7376b08331584b7c7fe6784d655
Farah315 <fara734isgreat@gmail.com>
2025-11-02 15:16:47 +0200

    refactor: remove unused User entity and repository

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/domain/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package net.thechance.mena.admin_panel.domain.repository
-
-import kotlinx.coroutines.flow.Flow
-import net.thechance.mena.admin_panel.domain.entity.User
-
-interface UserRepository {
-    fun getAllUsers(): Flow<List<User>>
-}
