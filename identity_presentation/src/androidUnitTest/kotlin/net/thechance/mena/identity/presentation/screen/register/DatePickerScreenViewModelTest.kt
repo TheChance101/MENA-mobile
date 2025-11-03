c918d6f89f51227fb8d74456eba5af892ae4e825
Aboud <gh.abd.com.207@gmail.com>
2025-11-03 10:14:26 -0800

    refactor: add age validation to `DatePickerScreenViewModel` tests

--- identity_presentation/src/androidUnitTest/kotlin/net/thechance/mena/identity/presentation/screen/register/DatePickerScreenViewModelTest.kt
+++ identity_presentation/src/androidUnitTest/kotlin/net/thechance/mena/identity/presentation/screen/register/DatePickerScreenViewModelTest.kt
@@ -1,23 +1,27 @@
 package net.thechance.mena.identity.presentation.screen.register
 
 import app.cash.turbine.test
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.LocalDate
-import kotlinx.io.discardingSink
+import net.thechance.mena.identity.domain.useCase.validation.age.AgeValidator
 import net.thechance.mena.identity.helper.BaseCoroutineTest
 import net.thechance.mena.identity.presentation.screen.register.datePicker.DatePickerScreenUIEffect
 import net.thechance.mena.identity.presentation.screen.register.datePicker.DatePickerScreenViewModel
 import org.junit.Before
 import org.junit.Test
 
-class DatePickerScreenViewModelTest: BaseCoroutineTest() {
+class DatePickerScreenViewModelTest : BaseCoroutineTest() {
     private lateinit var datePickerScreenViewModel: DatePickerScreenViewModel
+    private var ageValidator = mockk<AgeValidator>()
     private val testDispatcher = StandardTestDispatcher()
 
     @Before
     fun setup() {
         datePickerScreenViewModel = DatePickerScreenViewModel(
+            ageValidator = ageValidator,
             dispatcher = testDispatcher
         )
     }
@@ -49,6 +53,7 @@ class DatePickerScreenViewModelTest: BaseCoroutineTest() {
         val month = 1
         val year = 2000
 
+        every { ageValidator.isValid(LocalDate(year, month, day)) } returns true
         datePickerScreenViewModel.onChangeDate(day, month, year)
         testDispatcher.scheduler.advanceUntilIdle()
 
@@ -56,11 +61,16 @@ class DatePickerScreenViewModelTest: BaseCoroutineTest() {
     }
 
     @Test
-    fun `onChangeDate should disable next button if age is invalid`(){
+    fun `onChangeDate should disable next button if age is invalid`() {
         val day = 1
         val month = 1
         val year = 2025
 
+        every { ageValidator.isValid(LocalDate(year, month, day)) } returns false
 
+        datePickerScreenViewModel.onChangeDate(day, month, year)
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        assert(!datePickerScreenViewModel.state.value.isNextEnabled)
     }
 }
