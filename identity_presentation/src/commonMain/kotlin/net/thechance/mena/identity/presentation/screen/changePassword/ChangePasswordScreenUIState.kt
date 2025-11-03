1141106f734887519f45e1ed60f1fc2f375c1e98
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-11-03 12:36:19 +0200

    refactor: Nest UI state classes in ChangePasswordScreen

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/changePassword/ChangePasswordScreenUIState.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/changePassword/ChangePasswordScreenUIState.kt
@@ -1,6 +1,5 @@
 package net.thechance.mena.identity.presentation.screen.changePassword
 
-import androidx.compose.foundation.pager.PagerState
 import org.jetbrains.compose.resources.StringResource
 
 data class ChangePasswordScreenUIState(
@@ -9,21 +8,22 @@ data class ChangePasswordScreenUIState(
     val currentPage: Int = 0 ,
     val errorMessage: StringResource? = null,
     val isLoading: Boolean = false,
-)
+){
+    data class CurrentPasswordContentUIState(
+        val currentPassword: String = "",
+        val isCurrentPasswordVisible: Boolean = false,
+        val isContinueEnabled: Boolean = false,
+        val currentPasswordErrorMessage: StringResource? = null,
+    )
 
-data class CurrentPasswordContentUIState(
-    val currentPassword: String = "",
-    val isCurrentPasswordVisible: Boolean = false,
-    val isContinueEnabled: Boolean = false,
-    val currentPasswordErrorMessage: String? = null,
-)
+    data class NewPasswordContentUIState(
+        val newPassword: String = "",
+        val confirmPassword: String = "",
+        val isNewPasswordVisible: Boolean = false,
+        val isConfirmPasswordVisible: Boolean = false,
+        val isSaveEnabled: Boolean = false,
+        val newPasswordErrorMessage: StringResource? = null,
+        val confirmPasswordErrorMessage: StringResource? = null,
+    )
+}
 
-data class NewPasswordContentUIState(
-    val newPassword: String = "",
-    val confirmPassword: String = "",
-    val isNewPasswordVisible: Boolean = false,
-    val isConfirmPasswordVisible: Boolean = false,
-    val isSaveEnabled: Boolean = false,
-    val newPasswordErrorMessage: String? = null,
-    val confirmPasswordErrorMessage: String? = null,
-)
