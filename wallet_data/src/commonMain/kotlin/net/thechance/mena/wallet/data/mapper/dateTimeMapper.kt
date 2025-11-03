5f627f38a91d13488afead81f4d7f96bfda5f7d2
edrees <muhammed.edrees101@gmail.com>
2025-11-03 15:23:18 +0200

    fix: update date parsing functions to accept default values

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/mapper/dateTimeMapper.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/mapper/dateTimeMapper.kt
@@ -10,26 +10,26 @@ import kotlinx.datetime.toLocalDateTime
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
-fun parseLocalDateOrDefault(value: String?): LocalDate {
+fun parseLocalDateOrDefault(value: String?, defaultDate: LocalDate = DEFAULT_DATE): LocalDate {
     return value?.let {
         runCatching {
             val normalized = if(it.contains("T")) it else it + "T00:00:00Z"
             val instant = Instant.parse(normalized)
             val userZone = TimeZone.currentSystemDefault()
             instant.toLocalDateTime(userZone).date
-        }.getOrDefault(DEFAULT_DATE)
-    } ?: DEFAULT_DATE
+        }.getOrDefault(defaultDate)
+    } ?: defaultDate
 }
 
-fun parseLocalDateTimeOrDefault(value: String?): LocalDateTime {
+fun parseLocalDateTimeOrDefault(value: String?, defaultDateTime: LocalDateTime = DEFAULT_DATE_TIME): LocalDateTime {
     return value?.let {
         runCatching {
             val normalized = if (it.endsWith("Z") || it.contains('+')) it else it + "Z"
             val instant = Instant.parse(normalized)
             val userZone = TimeZone.currentSystemDefault()
             instant.toLocalDateTime(userZone)
-        }.getOrDefault(DEFAULT_DATE_TIME)
-    } ?: DEFAULT_DATE_TIME
+        }.getOrDefault(defaultDateTime)
+    } ?: defaultDateTime
 }
 private val DEFAULT_DATE = LocalDate(9999, 12, 31)
 private val DEFAULT_DATE_TIME = LocalDateTime(DEFAULT_DATE, LocalTime(0, 0))
