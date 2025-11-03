d7c57ca0ed912c62315b27c41a11f30293268bcf
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-11-03 14:19:00 +0200

    refactor: Move SnackBarUiState and add SnackBarType enum

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/profile/ProfileScreenUIState.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/profile/ProfileScreenUIState.kt
@@ -1,19 +1,31 @@
 package net.thechance.mena.identity.presentation.screen.profile
 
-import net.thechance.mena.identity.presentation.screen.addresses.myAddresses.SnackBarUiState
+import mena.identity_presentation.generated.resources.Res
+import mena.identity_presentation.generated.resources.error
 import org.jetbrains.compose.resources.StringResource
 
 data class ProfileScreenUIState(
-    val fullName:String = "",
-    val userName:String = "",
-    val profileImageUrl:String = "",
-    val showShareBottomSheet:Boolean = false,
-    val showLanguageDialog:Boolean = false,
-    val showShareProfileDialog:Boolean = false,
-    val showThemeDialog:Boolean = false,
-    val versionNumber:String? = null,
+    val fullName: String = "",
+    val userName: String = "",
+    val profileImageUrl: String = "",
+    val showShareBottomSheet: Boolean = false,
+    val showLanguageDialog: Boolean = false,
+    val showShareProfileDialog: Boolean = false,
+    val showThemeDialog: Boolean = false,
+    val versionNumber: String? = null,
     val isLoading: Boolean = false,
     val isSuccess: Boolean = false,
     val errorMessage: StringResource? = null,
     val snackBarUiState: SnackBarUiState = SnackBarUiState()
 )
+
+data class SnackBarUiState(
+    val isVisible: Boolean = false,
+    val snackBarType: SnackBarType = SnackBarType.ERROR,
+    val message: StringResource = Res.string.error,
+)
+
+enum class SnackBarType {
+    ERROR,
+    SUCCESS,
+}
