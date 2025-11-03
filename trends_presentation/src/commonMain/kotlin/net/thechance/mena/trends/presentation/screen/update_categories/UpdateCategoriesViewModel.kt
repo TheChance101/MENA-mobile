d5fa5f23c97efea1b668dbde6ae524d509e8fdda
Yusuf Nasser <yusufnassereng@gmail.com>
2025-11-03 22:25:45 +0200

    TRN-94: rename getCategories to initializeCategories

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/update_categories/UpdateCategoriesViewModel.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/update_categories/UpdateCategoriesViewModel.kt
@@ -21,10 +21,10 @@ internal class UpdateCategoriesViewModel(
 ), UpdateCategoriesInteractionListener {
 
     init {
-        getCategories()
+        initializeCategories()
     }
 
-    fun getCategories() {
+    fun initializeCategories() {
         tryToExecute(
             block = { repository.getAllCategories() },
             onSuccess = ::handleLoadCategoriesSuccess,
@@ -76,6 +76,6 @@ internal class UpdateCategoriesViewModel(
 
     override fun onClickRetry() {
         updateState{ copy(errorState = null) }
-        getCategories()
+        initializeCategories()
     }
 }
