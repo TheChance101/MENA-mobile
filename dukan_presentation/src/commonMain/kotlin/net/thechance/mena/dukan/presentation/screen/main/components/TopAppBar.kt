f0ee37d88cb81bded27a90eafba4dba7c104e365
a7med naser <ad0112727@gmail.com>
2025-11-03 20:06:33 +0200

    resolve conflicts : Apply clip modifier to TopAppBar

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/main/components/TopAppBar.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/main/components/TopAppBar.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import mena.dukan_presentation.generated.resources.Res
 import mena.dukan_presentation.generated.resources.add_dukan_icon
