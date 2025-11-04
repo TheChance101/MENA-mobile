fab52cce00cd3186c3d609b70361d8c15e220960
shahad-97 <shos8406@gmail.com>
2025-11-04 18:52:08 +0300

    feat: set LoginScreen as the start destination of LoginFlow

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/api/IdentityFeatureApiImpl.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/api/IdentityFeatureApiImpl.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import net.thechance.mena.identity.api.IdentityFeatureApi
 import net.thechance.mena.identity.presentation.screen.addresses.myAddresses.AddressesScreen
+import net.thechance.mena.identity.presentation.screen.login.LoginScreen
 import net.thechance.mena.identity.presentation.screen.register.accountCreated.AccountCreatedScreen
 import net.thechance.mena.identity.presentation.screen.profile.ProfileScreen
 import net.thechance.mena.identity.presentation.screen.register.enterName.EnterNameScreen
@@ -16,7 +17,7 @@ class IdentityFeatureApiImpl : IdentityFeatureApi {
 
     @Composable
     override fun LoginFlow() {
-        Navigator(EnterNameScreen())
+        Navigator(LoginScreen())
     }
 
     @Composable
