282ad1e8cf040bf402a3ac41c70f071ee485633b
Abdallah Bakhet <abakhet2010@gmail.com>
2025-11-04 15:27:14 +0200

    feat: Implement saving default reciter in TilawahViewModel

--- faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/quran/tilwah/TilawahViewModel.kt
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/quran/tilwah/TilawahViewModel.kt
@@ -1,14 +1,19 @@
 package net.thechance.mena.faith.presentation.feature.quran.tilwah
 
+import net.thechance.mena.faith.domain.repository.QuranRepository
 import net.thechance.mena.faith.presentation.base.BaseViewModel
 
-class TilawahViewModel : BaseViewModel<TilawahUiState, TilawahEffect>(
+class TilawahViewModel(
+    private val quranRepository: QuranRepository,
+) : BaseViewModel<TilawahUiState, TilawahEffect>(
     TilawahUiState()
 ), TilawahInteractionListener {
     override fun onBackClick() = sendEffect(TilawahEffect.NavigateBack)
     override fun onSearchClick() = sendEffect(TilawahEffect.NavigateToSearch)
     override fun onSelectReciterClick(reciterId: Int) {
-        //TODO() not implemented yet
+        tryToExecute(
+            execute = { quranRepository.saveDefaultReciter(reciterId) },
+            onError = { println("Error: $it") }
+        )
     }
-
 }
