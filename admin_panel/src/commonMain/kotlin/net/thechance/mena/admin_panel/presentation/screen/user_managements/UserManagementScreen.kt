6a4b72414d9920353480326f71c66a9c3217c57c
Menna Srour <mennasrour441@gmail.com>
2025-11-05 18:37:36 +0200

    refactor: update tab selection to use SelectedSidebarTab in AdminPanel components

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/user_managements/UserManagementScreen.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/user_managements/UserManagementScreen.kt
@@ -1,13 +1,21 @@
 package net.thechance.mena.admin_panel.presentation.screen.user_managements
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.thechance.mena.admin_panel.resources.Res
+import net.thechance.mena.admin_panel.resources.users_management
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun UserManagementScreen() {
@@ -23,7 +31,7 @@ fun UserManagementScreen() {
                 .padding(16.dp)
         ) {
             Text(
-                text = stringResource(state.currentTab.title),
+                text = stringResource(Res.string.users_management),
                 style = Theme.typography.title.medium,
                 color = Theme.colorScheme.shadePrimary
             )
