0ed53d1ab670d2b98ce437006aa8a91d4c3579ca
abdelrhman <abdelrhmanshabana999@gmail.com>
2025-11-05 08:49:20 +0200

    feat: add unit test update shelf name repository

--- dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/mapper/ShelfNameRequestMapper.kt
+++ dukan_data/src/commonMain/kotlin/net/thechance/mena/dukan/data/mapper/ShelfNameRequestMapper.kt
@@ -4,5 +4,5 @@ import net.thechance.mena.dukan.data.dto.shelf.UpdateShelfNameRequest
 import net.thechance.mena.dukan.domain.model.UpdateShelfName
 
 fun UpdateShelfName.toRequest(): UpdateShelfNameRequest {
-    return UpdateShelfNameRequest(title = this.title)
+    return UpdateShelfNameRequest(title = title)
 }
