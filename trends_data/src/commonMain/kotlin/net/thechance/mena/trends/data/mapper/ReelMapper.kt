c139bd5d1897670f5ae02006005ef833f5a6c5d2
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-11-05 18:38:24 +0200

    feature: add refresh url logic to data and domain

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/mapper/ReelMapper.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/mapper/ReelMapper.kt
@@ -1,11 +1,13 @@
 package net.thechance.mena.trends.data.mapper
 
 import net.thechance.mena.trends.data.dto.ReelDto
+import net.thechance.mena.trends.data.dto.ReelPathUrlsDto
 import net.thechance.mena.trends.data.util.orFalse
 import net.thechance.mena.trends.data.util.orZero
 import net.thechance.mena.trends.data.util.parseDateStringOrNull
 import net.thechance.mena.trends.data.util.toUrl
 import net.thechance.mena.trends.domain.entity.Reel
+import net.thechance.mena.trends.domain.model.ReelUrls
 
 
 internal fun ReelDto.toEntity(): Reel {
@@ -21,5 +23,12 @@ internal fun ReelDto.toEntity(): Reel {
         profileImageUrl = profilePictureUrl.orEmpty(),
         isCurrentUserOwner = isCurrentUserOwner,
         isLiked = isLiked.orFalse()
+    )
+}
+
+internal fun ReelPathUrlsDto.toReelUrls(): ReelUrls{
+    return ReelUrls(
+        videoUrl = videoPath.toUrl(),
+        thumbnailUrl = thumbnailPath.toUrl()
     )
 }
