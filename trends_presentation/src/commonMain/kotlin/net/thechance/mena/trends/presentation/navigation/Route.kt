b34d834c6aa3aea4c4f44edafdc57bf86df7efdb
ahmedsamy <ahmbdelkhalek@gmail.com>
2025-11-06 08:20:40 +0200

    refactor: Replace UserReelSource enum with boolean flags

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/navigation/Route.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/navigation/Route.kt
@@ -1,7 +1,6 @@
 package net.thechance.mena.trends.presentation.navigation
 
 import kotlinx.serialization.Serializable
-import net.thechance.mena.trends.presentation.screen.user_reel.args.UserReelSource
 
 internal sealed interface Route {
     @Serializable
@@ -11,22 +10,26 @@ internal sealed interface Route {
     data object UpdateCategories : Route
 
     @Serializable
-    data class ReelDetails(val reelId: String, val userReelSource: UserReelSource) : Route
+    data class ReelDetails(
+        val reelId: String,
+        val isFromHome: Boolean = false,
+        val isFromManageTrends: Boolean = false
+    ) : Route
 
     @Serializable
     data object ManageReels : Route
 
     @Serializable
-    data object MainContainer: Route
+    data object MainContainer : Route
 
     @Serializable
     data object UploadReel : Route
 
     @Serializable
-    data class VideoDescription(val trendId : String) : Route
+    data class VideoDescription(val trendId: String) : Route
 
     @Serializable
-    data class CategoriesPublish(val trendId : String, val description : String) : Route
+    data class CategoriesPublish(val trendId: String, val description: String) : Route
 
     @Serializable
     data object Home : Route
