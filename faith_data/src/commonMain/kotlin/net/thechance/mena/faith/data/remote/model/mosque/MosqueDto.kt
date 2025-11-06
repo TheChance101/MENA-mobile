39c78e77237db7850e96f615f2a1d07b078dd05b
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-11-06 03:40:36 +0200

    feat: Implement Mosque API service and data model for nearby mosques

---
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/remote/model/mosque/MosqueDto.kt
@@ -0,0 +1,15 @@
+package net.thechance.mena.faith.data.remote.model.mosque
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MosqueDto(
+    val id: String,
+    val name: String,
+    val address: String,
+    val latitude: Double,
+    val longitude: Double,
+    val imageUrl: String,
+    val distanceKm: Double? = null,
+    val createdAt: String
+)
--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/repository/MosqueRepositoryImpl.kt
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/repository/MosqueRepositoryImpl.kt
@@ -1,20 +1,27 @@
 package net.thechance.mena.faith.data.repository
 
+import net.thechance.mena.faith.data.mapper.mosque.toDomain
+import net.thechance.mena.faith.data.remote.model.mosque.MosqueDto
+import net.thechance.mena.faith.data.remote.service.MosqueApiService
+import net.thechance.mena.faith.data.utils.executeApiSafely
 import net.thechance.mena.faith.domain.entity.Mosque
 import net.thechance.mena.faith.domain.repository.MosqueRepository
 
-class MosqueRepositoryImpl() : MosqueRepository {
+class MosqueRepositoryImpl(
+    private val mosqueApiService: MosqueApiService
+) : MosqueRepository {
+
     override suspend fun addMosque(mosque: Mosque) {
-        // TODO("Not yet implemented")
+        TODO("Not yet implemented")
     }
 
     override suspend fun getNearbyMosques(
         latitude: Double,
         longitude: Double,
         radius: Double
-    ): List<Mosque> {
-        return emptyList()
-    }
+    ): List<Mosque> = executeApiSafely<List<MosqueDto>> {
+        mosqueApiService.getNearbyMosques(latitude, longitude, radius)
+    }.toDomain()
 
     override suspend fun getMosquesByName(query: String): List<Mosque> {
         return emptyList()
