cb41784b2507abf9ffe68567ae44fd9500259211
a7med naser <ad0112727@gmail.com>
2025-11-07 01:57:10 +0200

    feat(search): Update search repository return types

--- dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/repository/SearchRepository.kt
+++ dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/repository/SearchRepository.kt
@@ -2,9 +2,11 @@ package net.thechance.mena.dukan.domain.repository
 
 import net.thechance.mena.dukan.domain.entity.Dukan
 import net.thechance.mena.dukan.domain.entity.Product
+import net.thechance.mena.dukan.domain.model.DukanPreview
+import net.thechance.mena.dukan.domain.entity.ProductSearch
 import net.thechance.mena.dukan.domain.util.PagedResult
 
 interface SearchRepository {
-    suspend fun findDukansByQuery(query: String, page: Int, size: Int): PagedResult<Dukan>
-    suspend fun findProductsByQuery(query: String, page: Int, size: Int): PagedResult<Product>
+    suspend fun findDukansByQuery(query: String, page: Int, size: Int): PagedResult<DukanPreview>
+    suspend fun findProductsByQuery(query: String, page: Int, size: Int): PagedResult<ProductSearch>
 }
