b49672f67bef2e70d87a7b8c3cde195b54189eb4
a7med naser <ad0112727@gmail.com>
2025-11-07 01:57:27 +0200

    chore: remove unused imports in SearchRepository

--- dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/repository/SearchRepository.kt
+++ dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/repository/SearchRepository.kt
@@ -1,9 +1,7 @@
 package net.thechance.mena.dukan.domain.repository
 
-import net.thechance.mena.dukan.domain.entity.Dukan
-import net.thechance.mena.dukan.domain.entity.Product
-import net.thechance.mena.dukan.domain.model.DukanPreview
 import net.thechance.mena.dukan.domain.entity.ProductSearch
+import net.thechance.mena.dukan.domain.model.DukanPreview
 import net.thechance.mena.dukan.domain.util.PagedResult
 
 interface SearchRepository {
