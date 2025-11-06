da47c5a1a4bc4ccf7d255f09a1ed615a173c0518
a7med naser <ad0112727@gmail.com>
2025-11-07 01:56:51 +0200

    feat(dukan_domain): add DukanPreview and ProductSearch models

---
+++ dukan_domain/src/commonMain/kotlin/net/thechance/mena/dukan/domain/model/DukanPreview.kt
@@ -0,0 +1,12 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
+package net.thechance.mena.dukan.domain.model
+
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+data class DukanPreview(
+    val id: Uuid,
+    val name: String,
+    val imageUrl: String,
+)
