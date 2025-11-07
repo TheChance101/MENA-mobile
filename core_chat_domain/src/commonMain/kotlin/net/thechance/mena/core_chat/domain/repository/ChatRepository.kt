311dde21f1b7eee38c09df23a2adf7e3bee736e5
omer1998 <omerfaris11@gmail.com>
2025-11-07 06:15:51 +0300

    refactor: improve chat summary synchronization

--- core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/repository/ChatRepository.kt
+++ core_chat_domain/src/commonMain/kotlin/net/thechance/mena/core_chat/domain/repository/ChatRepository.kt
@@ -1,10 +1,13 @@
 package net.thechance.mena.core_chat.domain.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDateTime
 import net.thechance.mena.core_chat.domain.entity.Chat
 import net.thechance.mena.core_chat.domain.entity.ChatSummary
 import net.thechance.mena.core_chat.domain.model.PagedData
 import net.thechance.mena.core_chat.domain.model.SyncState
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -15,8 +18,10 @@ interface ChatRepository {
     suspend fun getChatById(chatId: Uuid): Chat
     suspend fun disconnect()
     suspend fun getChatsSummary(pageNumber: Int, pageSize: Int): PagedData<ChatSummary>
-    fun observeChats(): Flow<List<ChatSummary>>
 
     fun observeSyncState(): Flow<SyncState>
     suspend fun getChatSummaryById(chatId: Uuid): ChatSummary
+
+    @OptIn(ExperimentalTime::class)
+    suspend fun getDeletedChatAfterSpecificTime(time: Instant): List<Uuid>
 }
