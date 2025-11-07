1ed12850487f9e0e58aa902e4febd16eec279d82
Mostafa Mohammed <tofymofy.55@gmail.com>
2025-11-07 16:25:30 +0300

    refactor: Update `CalculateDistanceUseCase` to use `Address` entity

--- faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/usecase/CalculateDistanceUseCase.kt
+++ faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/usecase/CalculateDistanceUseCase.kt
@@ -1,33 +1,42 @@
 package net.thechance.mena.faith.domain.usecase
 
 
-import net.thechance.mena.faith.domain.entity.Location
 import net.thechance.mena.faith.domain.exception.FaithException
-import kotlin.math.*
+import net.thechance.mena.identity.domain.entity.Address
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 
 class CalculateDistanceUseCase {
 
     operator fun invoke(
-        location1: Location,
-        location2: Location,
+        address1: Address,
+        address2: Address,
 
-    ): Double {
+        ): Double {
 
-        if (location1.latitude == location2.latitude && location1.longitude == location2.longitude) return 0.0
+        if (address1.latitude == address2.latitude && address1.longitude == address2.longitude) return 0.0
 
-        if (!isValidCoordinate(location1.latitude, location1.longitude) || !isValidCoordinate(location2.latitude, location2.longitude)) {
+        if (!isValidCoordinate(address1.latitude, address1.longitude) || !isValidCoordinate(
+                address2.latitude,
+                address2.longitude
+            )
+        ) {
             throw FaithException.InvalidCoordinates
         }
 
         val earthRadiusKm = 6371.0
 
-        val dLat = toRadians(degrees = location2.latitude - location1.latitude)
-        val dLon = toRadians(degrees = location2.longitude - location1.longitude)
+        val dLat = toRadians(degrees = address2.latitude - address1.latitude)
+        val dLon = toRadians(degrees = address2.longitude - address1.longitude)
 
         val haversineComponent = sin(dLat / 2).pow(2.0) +
-                cos(x = toRadians(degrees = location1.latitude)) *
-                cos(x = toRadians(degrees = location2.latitude)) *
+                cos(x = toRadians(degrees = address1.latitude)) *
+                cos(x = toRadians(degrees = address2.latitude)) *
                 sin(x = dLon / 2).pow(x = 2.0)
 
         val centralAngle = 2 * atan2(sqrt(x = haversineComponent), x = sqrt(1 - haversineComponent))
