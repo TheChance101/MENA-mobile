fef4c67c6bb8e62178f25366b85e0d9b3ef79874
Mohamed Hassan <127283755+mohamedd-hassan@users.noreply.github.com>
2025-11-07 20:02:09 +0200

    refactor: remove extra dependency

--- trends_presentation/build.gradle.kts
+++ trends_presentation/build.gradle.kts
@@ -33,7 +33,6 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.coil.network.ktor3)
-            implementation(libs.ktor.client.android)
             implementation(libs.androidx.media3.exoplayer)
             implementation(libs.androidx.media3.ui)
             implementation(libs.androidx.media3.exoplayer.dash)
