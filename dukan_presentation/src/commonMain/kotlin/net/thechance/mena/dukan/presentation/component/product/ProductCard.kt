1c9ada42ec771b64eec93835dad74e785eed71a5
nada <nadamansoursamir@gmail.com>
2025-11-08 12:10:10 +0200

    feat: display placeholder on image load error in ProductCard

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/product/ProductCard.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/component/product/ProductCard.kt
@@ -1,5 +1,6 @@
 package net.thechance.mena.dukan.presentation.component.product
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -12,6 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,12 +24,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
 import mena.dukan_presentation.generated.resources.Res
+import mena.dukan_presentation.generated.resources.ic_no_image_loaded
 import mena.dukan_presentation.generated.resources.koin_icon
 import mena.dukan_presentation.generated.resources.product_image
 import mena.dukan_presentation.generated.resources.silver_tc
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -41,6 +49,8 @@ fun ProductCard(
     productAction: @Composable () -> Unit={},
     onProductClick: () -> Unit = {}
 ) {
+    var isError by remember { mutableStateOf(false) }
+
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -62,11 +72,24 @@ fun ProductCard(
             AsyncImage(
                 model = productImageUrl,
                 contentDescription = stringResource(Res.string.product_image),
+                onState = {state->
+                    isError = state is AsyncImagePainter.State.Error
+                },
                 modifier = Modifier
                     .size(96.dp)
                     .clip(RoundedCornerShape(Theme.radius.sm)),
                 contentScale = ContentScale.Crop
             )
+            if (isError) {
+                Image(
+                    painter = painterResource(Res.drawable.ic_no_image_loaded),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp)
+                        .align(Alignment.Center),
+                    contentScale = ContentScale.Fit
+                )
+            }
         }
         Column(
             modifier = Modifier
