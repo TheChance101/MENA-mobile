0c7d57e4d16ac03733a8289b302183a8d1da3fda
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-11-09 23:53:29 +0200

    feat: add observeBalance function to BalanceRepository for real-time balance updates

--- wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/BalanceRepositoryImpl.kt
+++ wallet_data/src/commonMain/kotlin/net/thechance/mena/wallet/data/repository/balance/BalanceRepositoryImpl.kt
@@ -1,8 +1,15 @@
 package net.thechance.mena.wallet.data.repository.balance
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import net.thechance.mena.wallet.data.dto.remote.BalanceDto
-import net.thechance.mena.wallet.data.utils.safeApiCall
 import net.thechance.mena.wallet.data.network_client.NetworkClient
+import net.thechance.mena.wallet.data.utils.safeApiCall
 import net.thechance.mena.wallet.domain.repository.BalanceRepository
 import org.koin.core.annotation.Single
 
@@ -11,9 +18,24 @@ class BalanceRepositoryImpl(
     private val networkClient: NetworkClient
 ) : BalanceRepository {
 
-    override suspend fun getBalance() = safeApiCall<BalanceDto> {
-        networkClient.get(BALANCE_PATH)
-    }.balance ?: 0.0
+    private val balanceFlow = MutableStateFlow(0.0)
+
+    override suspend fun getBalance(): Double {
+        val balance = safeApiCall<BalanceDto> {
+            networkClient.get(BALANCE_PATH)
+        }.balance ?: 0.0
+        balanceFlow.update { balance }
+        return balance
+    }
+
+    override fun observeBalance(): Flow<Double> {
+        if (balanceFlow.value == 0.0) {
+            CoroutineScope(Dispatchers.IO).launch {
+                getBalance()
+            }
+        }
+        return balanceFlow
+    }
 
     private companion object {
         const val BALANCE_PATH = "wallet/balance"
