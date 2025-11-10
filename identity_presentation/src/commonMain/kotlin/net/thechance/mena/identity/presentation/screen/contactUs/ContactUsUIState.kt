50e3af6d980dfdd77574c4a1c90034ef97d0e1dc
eslam-99 <eslam.mohamed.551999@gmail.com>
2025-11-11 00:55:32 +0200

    feature: add contact us screen

---
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/contactUs/ContactUsUIState.kt
@@ -0,0 +1,14 @@
+package net.thechance.mena.identity.presentation.screen.contactUs
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+data class ContactUsUIState(
+    val urlToOpen: MutableSharedFlow<String> = MutableSharedFlow(),
+    val displayedEmail: String = "MENA2025@gmail.com",
+    val displayedPhoneNumber: String = "+964 770 0000 000",
+    val displayedFacebookAccount: String = "MENA-THE-CHANCE",
+    val email: String = "mohsen",
+//    val email: String = "MENA2025@gmail.com",
+    val phoneNumber: String = "",
+    val facebookUrl: String = "https://www.facebook.com",
+)
