74081f1ed87e7e929da60f5af557ae0f8c52edd2
Zyad <zyadhammad531@gmail.com>
2025-11-11 02:37:50 +0200

    fix: revert changes in wallet gradle

--- wallet_data/build.gradle.kts
+++ wallet_data/build.gradle.kts
@@ -68,10 +68,6 @@ ksp {
     arg("KOIN_CONFIG_CHECK", "true")
 }
 
-room {
-    schemaDirectory("$projectDir/schemas")
-}
-
 dependencies {
     add("kspCommonMainMetadata", libs.koin.ksp.compiler)
     add("kspAndroid", libs.androidx.room.compiler)
@@ -100,6 +96,9 @@ android {
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
+}
+room {
+    schemaDirectory("$projectDir/schemas")
 }
 
 kover.reports {
