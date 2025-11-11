4b8cab4e25c7019e8b8ecaaa5c2bda1b5140f020
Al-Qassim <qassimali624@gmail.com>
2025-11-11 12:55:51 +0300

    fix: issue with the voice message not stoping on finish, and crash on direct second play

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/repository/ChatRepositoryImpl.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/repository/ChatRepositoryImpl.kt
@@ -146,6 +146,7 @@ class ChatRepositoryImpl(
         try {
             callee()
         } catch (_: NoInternetException) {
+            println("asdasd 2")
             _syncState.emit(SyncState.Offline)
         } catch (e: Exception) {
             _syncState.emit(SyncState.Error(e))
