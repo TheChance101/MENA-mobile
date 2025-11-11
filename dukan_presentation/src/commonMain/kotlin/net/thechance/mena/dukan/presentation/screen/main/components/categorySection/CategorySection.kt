00d5beddff0de46f1a0aaaa3f553815dc17ce7eb
nada <nadamansoursamir@gmail.com>
2025-11-11 11:34:16 +0200

    remove unused import

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/main/components/categorySection/CategorySection.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/main/components/categorySection/CategorySection.kt
@@ -1,7 +1,6 @@
 package net.thechance.mena.dukan.presentation.screen.main.components.categorySection
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
