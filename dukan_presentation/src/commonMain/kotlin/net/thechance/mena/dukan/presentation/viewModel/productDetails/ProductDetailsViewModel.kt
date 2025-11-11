5f1d34e901109d48045a86032c908d0e2b416a9b
Mohamed Ashraf <ma1261327@gmail.com>
2025-11-11 15:49:45 +0200

    update cart notification logic to use hasProductInCart state in ProductDetailsScreen

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/productDetails/ProductDetailsViewModel.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/productDetails/ProductDetailsViewModel.kt
@@ -14,7 +14,6 @@ import mena.dukan_presentation.generated.resources.something_went_wrong
 import net.thechance.mena.dukan.domain.entity.Cart
 import net.thechance.mena.dukan.domain.entity.Product
 import net.thechance.mena.dukan.domain.exceptions.NoInternetException
-import net.thechance.mena.dukan.domain.exceptions.NoSuchItemException
 import net.thechance.mena.dukan.domain.model.UpdateProductCartQuantityParams
 import net.thechance.mena.dukan.domain.repository.CartRepository
 import net.thechance.mena.dukan.domain.repository.ProductRepository
@@ -49,19 +48,11 @@ class ProductDetailsViewModel(
     }
 
     private fun onCartInfoError(throwable: Throwable) {
-        when (throwable) {
-            is NoSuchItemException -> updateState { copy(totalPrice = 0.0) }
-            is NoInternetException -> updateState { copy(totalPrice = 0.0) }
-            else -> updateState { copy(totalPrice = 0.0) }
-        }
+        updateState { copy(hasProductInCart = false) }
     }
 
     private fun onLoadCartSuccess(cart: Cart) {
-        updateState {
-            copy(
-                totalPrice = cart.totalPrice,
-            )
-        }
+        updateState { copy(hasProductInCart = cart.totalPrice > 0.0) }
     }
 
     private fun loadProductDetails() {
@@ -153,7 +144,12 @@ class ProductDetailsViewModel(
     }
 
     private fun addProductToCartSuccessfully(success: Unit) {
-        updateState { copy(isAddToCartLoading = false) }
+        updateState {
+            copy(
+                isAddToCartLoading = false,
+                hasProductInCart = product.inCartQuantity > 0
+            )
+        }
         val messageRes = Res.string.add_product_success
         showSnackBar(message = messageRes, type = SnackBarType.SUCCESS)
     }
--- dukan_presentation/src/commonTest/kotlin/net/thechance/mena/dukan/presentation/viewModel/productDetails/ProductDetailsViewModelTest.kt
+++ dukan_presentation/src/commonTest/kotlin/net/thechance/mena/dukan/presentation/viewModel/productDetails/ProductDetailsViewModelTest.kt
@@ -17,10 +17,7 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import mena.dukan_presentation.generated.resources.Res
-import mena.dukan_presentation.generated.resources.added_to_favorites
-import mena.dukan_presentation.generated.resources.error_updating_favorites
 import mena.dukan_presentation.generated.resources.no_internet_connection
-import mena.dukan_presentation.generated.resources.removed_from_favorites
 import net.thechance.mena.dukan.domain.entity.Cart
 import net.thechance.mena.dukan.domain.entity.Product
 import net.thechance.mena.dukan.domain.exceptions.NoInternetException
@@ -79,10 +76,11 @@ class ProductDetailsViewModelTest {
 
         viewModel.state.test {
             val state = awaitItem()
-            assertEquals(500.0, state.totalPrice)
+            assertEquals(true, state.hasProductInCart)
             cancelAndIgnoreRemainingEvents()
         }
     }
+
     @Test
     fun `init SHOULD set isLoading to false after successful load`() = runTest {
         advanceUntilIdle()
@@ -351,7 +349,6 @@ class ProductDetailsViewModelTest {
             // Then
             verifySuspend { productRepository.toggleProductToFavorites(testProductId) }
         }
-
 
 
     @OptIn(ExperimentalUuidApi::class)
