53bac79f156a4a0d2f601e874af61b605c6394b5
nada <nadamansoursamir@gmail.com>
2025-11-12 14:54:39 +0200

    feat: add dukan color to product details screen

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/productDetails/ProductDetailsMapper.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/viewModel/productDetails/ProductDetailsMapper.kt
@@ -23,3 +23,5 @@ fun ProductDetailsUiState.ProductInfo.toDomainParams(dukanId: String): UpdatePro
         dukanId = dukanId
     )
 }
+
+fun String.toColor(): Long = (removePrefix("#").toLong(16) or 0xFF000000)
