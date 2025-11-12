65e11f9d892ae4d4eade80dbdff6ea2173589246
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-11-12 16:43:24 +0200

    fix: remove unused imports in TableHeaderRow and UsersListContent

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/component/TableHeaderRow.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/component/TableHeaderRow.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import net.thechance.mena.admin_panel.presentation.screen.users_management.UsersManagementScreenState
 import net.thechance.mena.admin_panel.resources.Res
