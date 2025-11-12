2a11ff7dd16ebcf89b8e9a3f65f86a2beac951c8
raouf-kamel <raoufkamel2013@gmail.com>
2025-11-12 19:35:04 +0300

    refactor: Update MessageDto to use sealed class for content

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/dto/MessageDto.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/source/remote/dto/MessageDto.kt
@@ -1,17 +1,18 @@
 package net.thechance.mena.core_chat.data.source.remote.dto
 
 import kotlinx.serialization.Serializable
+import net.thechance.mena.core_chat.data.source.remote.dto.message.MessageContentDto
+import net.thechance.mena.core_chat.data.source.remote.dto.message.MessageContentDtoSerializer
 
 @Serializable
 data class MessageDto(
     val id: String,
     val senderId: String,
     val chatId: String,
-    val text: String? = null,
-    val imageUrl: String? = null,
-    val audioUrl: String? = null,
+    val type: String,
+    @Serializable(with = MessageContentDtoSerializer::class)
+    val content: MessageContentDto,
     val reactions: List<MessageReactionDto> = emptyList(),
-    val audioDurationMs: Long? = null,
     val sendAt: String,
     val isRead: Boolean,
     val isMine: Boolean
