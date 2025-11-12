f52b4fb707802b2994c478ae4d44d59d5859d7ec
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-11-12 16:31:34 +0200

    feat: add dukan details component

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/dukan_details/DukanDetailsScreen.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/dukan_details/DukanDetailsScreen.kt
@@ -1,7 +1,14 @@
 package net.thechance.mena.admin_panel.presentation.screen.dukan_details
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import net.thechance.mena.admin_panel.presentation.component.PanelScaffold
+import net.thechance.mena.admin_panel.presentation.screen.dukan_details.component.DukanDetails
 import net.thechance.mena.admin_panel.presentation.screen.dukan_details.component.DukanDetailsAppBar
 
 @Composable
@@ -15,6 +22,19 @@ internal fun DukanDetailsScreen() {
             )
         }
     ) {
-
+        Row (modifier = Modifier.fillMaxSize().padding(16.dp)) {
+            DukanDetails(
+                dukanName = "MINISO - Baghdad mall branch",
+                dukanCategories = listOf("Accessories", "shoes", "perfume"),
+                dukanLocation = "Karada, Baghdad",
+                modifier = Modifier.padding(end = 8.dp).weight(1f).fillMaxHeight()
+            )
+            DukanDetails(
+                dukanName = "MINISO - Baghdad mall branch",
+                dukanCategories = listOf("Accessories", "shoes", "perfume"),
+                dukanLocation = "Karada, Baghdad",
+                modifier = Modifier.weight(1f).fillMaxHeight()
+            )
+        }
     }
 }
---
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/dukan_details/component/DukanDetails.kt
@@ -0,0 +1,127 @@
+package net.thechance.mena.admin_panel.presentation.screen.dukan_details.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import net.thechance.mena.admin_panel.resources.Res
+import net.thechance.mena.admin_panel.resources.ic_store_location
+import net.thechance.mena.admin_panel.resources.img_no_internet
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+internal fun DukanDetails(
+    dukanName: String,
+    dukanCategories: List<String>,
+    dukanLocation: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .background(
+                color = Theme.colorScheme.background.surfaceLow,
+                shape = RoundedCornerShape(Theme.radius.xl)
+            )
+            .padding(vertical = 16.dp, horizontal = 16.dp)
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(2f)
+                .clip(RoundedCornerShape(Theme.radius.md)),
+            painter = painterResource(Res.drawable.img_no_internet),
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = dukanName,
+            style = Theme.typography.title.large,
+            color = Theme.colorScheme.shadePrimary
+        )
+        DukanCategories(modifier = Modifier.padding(top = 2.dp), categories = dukanCategories)
+        Box(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .aspectRatio(2f)
+                .background(
+                    color = Theme.colorScheme.stroke,
+                    shape = RoundedCornerShape(Theme.radius.md)
+                )
+                .clip(RoundedCornerShape(Theme.radius.md)),
+        )
+        DukanLocation(modifier = Modifier.padding(top = 8.dp), location = dukanLocation)
+    }
+}
+
+@Composable
+private fun DukanCategories(
+    categories: List<String>,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        categories.forEachIndexed { index, category ->
+            Text(
+                text = category,
+                style = Theme.typography.label.small,
+                color = Theme.colorScheme.shadeSecondary
+            )
+            if (index < categories.size - 1) {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .size(3.dp)
+                        .background(color = Color(0xFFD9D9D9), shape = CircleShape)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DukanLocation(
+    location: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Icon(
+            modifier = Modifier.size(16.dp).padding(end = 4.dp),
+            painter = painterResource(Res.drawable.ic_store_location),
+            contentDescription = null,
+            tint = Theme.colorScheme.shadeSecondary
+        )
+        Text(
+            text = location,
+            style = Theme.typography.label.small,
+            color = Theme.colorScheme.shadeSecondary
+        )
+
+    }
+
+}
