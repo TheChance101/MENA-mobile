c223fe4931ea740c3f0f607feb6cb05c909cf896
Menna Srour <mennasrour441@gmail.com>
2025-11-12 19:49:20 +0200

    feat: add DukanManagementScreen Header

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/dukan_managements/component/DukanManagementHeader.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/dukan_managements/component/DukanManagementHeader.kt
@@ -1,24 +1,31 @@
 package net.thechance.mena.admin_panel.presentation.screen.dukan_managements.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import net.thechance.mena.admin_panel.presentation.component.DukansCounter
 import net.thechance.mena.admin_panel.presentation.component.SearchBar
 import net.thechance.mena.admin_panel.resources.Res
 import net.thechance.mena.admin_panel.resources.dukans
 import net.thechance.mena.admin_panel.resources.search_hint
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun DukanManagementHeader(
-    dukansNumbers: Int,
+    dukansNumbers: String,
     onQueryChange: (String) -> Unit,
     onClearQueryClicked: () -> Unit,
     query: String,
@@ -26,13 +33,12 @@ fun DukanManagementHeader(
 ) {
     Box(
         modifier = modifier
-            .padding(start = 16.dp)
+            .padding(start = 16.dp, bottom = 12.dp)
             .fillMaxWidth()
             .wrapContentHeight(),
     ) {
-        DukansCounter(
-            count = dukansNumbers,
-            title = stringResource(Res.string.dukans),
+        DukansCount(
+            dukansNumbers = dukansNumbers,
             modifier = Modifier.align(Alignment.CenterStart)
         )
         SearchBar(
@@ -45,5 +51,36 @@ fun DukanManagementHeader(
                 .align(Alignment.CenterEnd)
                 .padding(16.dp)
         )
+    }
+}
+
+@Composable
+private fun DukansCount(
+    dukansNumbers: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.wrapContentSize(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(Res.string.dukans),
+            color = Theme.colorScheme.shadePrimary,
+            style = Theme.typography.title.small,
+        )
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .clip(CircleShape)
+                .background(color = Theme.colorScheme.background.surfaceLow)
+                .padding(vertical = 5.dp, horizontal = 8.dp)
+        ) {
+            Text(
+                text = dukansNumbers,
+                color = Theme.colorScheme.shadePrimary,
+                style = Theme.typography.label.medium,
+            )
+        }
     }
 }
