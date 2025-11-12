9baca1205093d0f2aaa2695cfe222164f83506b1
mohamedshemees <mohamedessampd@gmail.com>
2025-11-13 00:06:38 +0200

    refactor:ThemeDialog Adjust spacing and remove fixed height from save button

--- design_system/src/commonMain/kotlin/net/thechance/mena/designsystem/presentation/component/dialog/BasicDialog.kt
+++ design_system/src/commonMain/kotlin/net/thechance/mena/designsystem/presentation/component/dialog/BasicDialog.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter.Companion.tint
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import mena.design_system.generated.resources.Res
