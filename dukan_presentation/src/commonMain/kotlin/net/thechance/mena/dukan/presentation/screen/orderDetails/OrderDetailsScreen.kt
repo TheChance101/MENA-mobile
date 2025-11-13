ed4f60675d866e34dec92855f9d4d7f1cd282b5b
a7med naser <ad0112727@gmail.com>
2025-11-13 17:49:53 +0200

    feat(order): implement order details screen UI

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/orderDetails/OrderDetailsScreen.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/orderDetails/OrderDetailsScreen.kt
@@ -1,9 +1,9 @@
 package net.thechance.mena.dukan.presentation.screen.orderDetails
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import mena.dukan_presentation.generated.resources.Res
@@ -14,6 +14,8 @@ import net.thechance.mena.designsystem.presentation.component.icon.Icon
 import net.thechance.mena.designsystem.presentation.component.scaffold.Scaffold
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import net.thechance.mena.dukan.presentation.screen.orderDetails.component.CustomerInformationSection
+import net.thechance.mena.dukan.presentation.screen.orderDetails.component.DeliveryAddressSection
 import net.thechance.mena.dukan.presentation.screen.orderDetails.component.OrderDetailsList
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -25,11 +27,11 @@ fun OrderDetailsScreen() {
 }
 
 @Composable
-fun OrderDetailsContent() {
+private fun OrderDetailsContent() {
     Scaffold(
         topBar = {
             AppBar(
-                title = "Order #", // Todo add order number
+                title = "Order #", // Todo add order number Id
                 onLeadingClick = {}, // Todo add back action
                 contentPadding = PaddingValues(
                     horizontal = Theme.spacing._16,
@@ -45,12 +47,32 @@ fun OrderDetailsContent() {
             )
         }
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = Theme.spacing._12),
-            ) {
-            OrderDetailsList()
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(
+                horizontal = Theme.spacing._16,
+                vertical = Theme.spacing._12
+            )
+        ) {
+            item {
+                OrderDetailsList()
+            }
+            item {
+                DeliveryAddressSection(
+                    address = "123 Main St, City, Country", // Todo add real address
+                    onClick = {}, // Todo add change address action
+                    modifier = Modifier
+                        .padding(top = Theme.spacing._24)
+                )
+            }
+            item {
+                CustomerInformationSection(
+                    userName = "John Doe", // Todo add real user name
+                    userPhoneNumber = "+1234567890", // Todo add real phone number
+                    modifier = Modifier
+                        .padding(top = Theme.spacing._12)
+                )
+            }
         }
     }
 }
