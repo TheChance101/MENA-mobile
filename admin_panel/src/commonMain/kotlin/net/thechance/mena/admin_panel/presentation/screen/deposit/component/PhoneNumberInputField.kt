1145ada93e9eea8c683558c5742a8f9a68b425c7
gehadepada <gehadepada@gmail.com>
2025-11-14 01:48:51 +0200

    feat:add PhoneNumberInputField.kt

---
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/deposit/component/PhoneNumberInputField.kt
@@ -0,0 +1,86 @@
+package net.thechance.mena.admin_panel.presentation.screen.deposit.component
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import net.thechance.mena.admin_panel.resources.Res
+import net.thechance.mena.designsystem.presentation.component.text.Text
+import net.thechance.mena.designsystem.presentation.component.textField.MobileNumberLeadingContent
+import net.thechance.mena.designsystem.presentation.component.textField.MobileNumberTextField
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+internal fun PhoneNumberInputField(
+    phoneNumber: String,
+    onPhoneChange: (String) -> Unit,
+    countryCode: String,
+    countryFlag: Painter,
+    onClickCountry: () -> Unit,
+    modifier: Modifier = Modifier,
+    label: String = stringResource(Res.string.phone_number)
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = label,
+            style = Theme.typography.title.small,
+            color = Theme.colorScheme.shadePrimary,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = Theme.spacing._4)
+        )
+        PhoneNumberInput(
+            countryCode = countryCode,
+            countryFlag = countryFlag,
+            onCountryClick = onClickCountry,
+            phoneNumber = phoneNumber,
+            onPhoneChange = onPhoneChange
+        )
+    }
+}
+
+@Composable
+private fun PhoneNumberInput(
+    countryCode: String,
+    countryFlag: Painter,
+    onCountryClick: () -> Unit,
+    phoneNumber: String,
+    onPhoneChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    phoneNumberFilter: (String) -> String = { it.filter { char -> char.isDigit() } }
+) {
+    Row(
+        modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        MobileNumberLeadingContent(
+            countryCode = countryCode,
+            countryPainter = countryFlag,
+            onClick = onCountryClick
+        )
+
+        MobileNumberTextField(
+            value = phoneNumber,
+            leadingIcon = painterResource(Res.drawable.ic_phone),
+            onValueChanged = { newValue ->
+                val filtered = phoneNumberFilter(newValue)
+                onPhoneChange(filtered)
+            },
+            hint = "",
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
