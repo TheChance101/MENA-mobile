0634d6d6b010ef0844ae10b2f854ef5ba3bea160
nada <nadamansoursamir@gmail.com>
2025-11-13 23:41:35 +0200

    add animated color state for Dukan style options

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/createDukan/component/dukanstyle/DukanStyleOptions.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/createDukan/component/dukanstyle/DukanStyleOptions.kt
@@ -1,9 +1,11 @@
 package net.thechance.mena.dukan.presentation.screen.createDukan.component.dukanstyle
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
