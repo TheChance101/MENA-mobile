5a7f4c939e83a55afe8d12460b9713e42f4bfeb3
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-11-14 12:48:56 +0200

    refactor: make splash screen like design

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/navigation/AdminPanelNavHost.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/navigation/AdminPanelNavHost.kt
@@ -11,7 +11,7 @@ import net.thechance.mena.admin_panel.presentation.screen.deposit.DepositScreen
 import net.thechance.mena.admin_panel.presentation.screen.dukan_managements.DukanManagementsScreen
 import net.thechance.mena.admin_panel.presentation.screen.dukan_requests.DukanRequestsScreen
 import net.thechance.mena.admin_panel.presentation.screen.login.LoginScreen
-import net.thechance.mena.admin_panel.presentation.screen.SplashScreen.SplashScreen
+import net.thechance.mena.admin_panel.presentation.screen.splash_screen.SplashScreen
 import net.thechance.mena.admin_panel.presentation.screen.dukan_details.DukanDetailsScreen
 import net.thechance.mena.admin_panel.presentation.screen.users_management.UsersManagementScreen
 
