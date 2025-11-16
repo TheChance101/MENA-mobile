4108c330184c229163ae89ef8f5131207b635294
Menna Srour <mennasrour441@gmail.com>
2025-11-16 13:07:12 +0200

    fix: update import path for SplashScreen in AdminPanelNavHost

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/navigation/AdminPanelNavHost.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/navigation/AdminPanelNavHost.kt
@@ -11,8 +11,8 @@ import net.thechance.mena.admin_panel.presentation.screen.deposit.DepositScreen
 import net.thechance.mena.admin_panel.presentation.screen.dukan_managements.DukanManagementsScreen
 import net.thechance.mena.admin_panel.presentation.screen.dukan_requests.DukanRequestsScreen
 import net.thechance.mena.admin_panel.presentation.screen.login.LoginScreen
-import net.thechance.mena.admin_panel.presentation.screen.SplashScreen.SplashScreen
 import net.thechance.mena.admin_panel.presentation.screen.dukan_details.DukanDetailsScreen
+import net.thechance.mena.admin_panel.presentation.screen.splash_screen.SplashScreen
 import net.thechance.mena.admin_panel.presentation.screen.users_management.UsersManagementScreen
 
 val LocalNavController = staticCompositionLocalOf<NavHostController> {
