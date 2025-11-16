2688e9d66cfc9c2360d681c84c16d2b73081de03
AdelTamer <adeltammer@gmail.com>
2025-11-16 19:20:55 +0200

    add order message type to data and domain

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/messagesender/MessageSenderFactory.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/messagesender/MessageSenderFactory.kt
@@ -11,5 +11,6 @@ class MessageSenderFactory(
         is MessageContent.Text -> textMessageSender
         is MessageContent.Image -> imageMessageSender
         is MessageContent.Audio -> audioMessageSender
+        is MessageContent.Order -> TODO("Not yet implemented")
     }
 }
