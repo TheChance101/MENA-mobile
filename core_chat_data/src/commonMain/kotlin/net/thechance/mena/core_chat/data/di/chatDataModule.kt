a258508ef2fc9bbd25e23b5a39e3cdfe21dc60b3
Radwa85 <radwa.mohamed5033@gmail.com>
2025-11-18 18:02:23 +0200

    add faithDomainModule

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/chatDataModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/chatDataModule.kt
@@ -1,7 +1,8 @@
 package net.thechance.mena.core_chat.data.di
 
+import net.thechance.mena.faith.domain.di.faithDomainModule
 import org.koin.dsl.module
 
 val chatDataModule = module {
-    includes(networkModule, repositoryModule, localDataModule, fileManagerModule)
+    includes(networkModule, repositoryModule, localDataModule, fileManagerModule, faithDomainModule)
 }
