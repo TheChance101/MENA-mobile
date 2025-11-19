ec2ea6a0180bb35798d2de5a415f437ff3f059b5
Farah315 <fara734isgreat@gmail.com>
2025-11-19 14:45:57 +0200

    fix: fix date map and make it like design

--- wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryMapper.kt
+++ wallet_presentation/src/commonMain/kotlin/net/thechance/mena/wallet/presentation/screen/transaction_history/TransactionHistoryMapper.kt
@@ -14,7 +14,7 @@ import kotlin.uuid.ExperimentalUuidApi
 fun Transaction.toUi(): TransactionHistoryScreenState.TransactionHistoryUiState =
     TransactionHistoryScreenState.TransactionHistoryUiState(
         id = id,
-        timeAndDate = formatLocalDateTime(date = createdAt, outputFormat = "dd MMM yyyy, h:mm a"),
+        timeAndDate = formatLocalDateTime(date = createdAt, outputFormat = "dd MMM, h:mm a"),
         amount = formatAmount(amount),
         type = getTransactionType(),
         status = getTransactionStatus(),
