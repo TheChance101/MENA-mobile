cf2f3e0007868d401bc2a1396de815015036a2f3
Radwa85 <radwa.mohamed5033@gmail.com>
2025-11-19 22:02:34 +0200

    Remove unused faithDomainModule

--- core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/chatDataModule.kt
+++ core_chat_data/src/commonMain/kotlin/net/thechance/mena/core_chat/data/di/chatDataModule.kt
@@ -1,8 +1,7 @@
 package net.thechance.mena.core_chat.data.di
 
-import net.thechance.mena.faith.domain.di.faithDomainModule
 import org.koin.dsl.module
 
 val chatDataModule = module {
-    includes(networkModule, repositoryModule, localDataModule, fileManagerModule, faithDomainModule)
+    includes(networkModule, repositoryModule, localDataModule, fileManagerModule)
 }
