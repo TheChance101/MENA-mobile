b36cd0839e908779b80d4b526131a87d4858c209
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-11-20 14:53:54 +0100

    feature: implement custom SnackBarController for identity module

---
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/components/snackBar/IdentitySnackBarController.kt
@@ -0,0 +1,62 @@
+package net.thechance.mena.identity.presentation.components.snackBar
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import mena.identity_presentation.generated.resources.Res
+import mena.identity_presentation.generated.resources.ic_close_circle
+import mena.identity_presentation.generated.resources.ic_success
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
+
+@Immutable
+data class SnackBarData(
+    val message: StringResource,
+    val title: StringResource,
+    val duration: Long = 3_000L,
+    val type: SnackBarType = SnackBarType.ERROR
+) {
+
+    enum class SnackBarType(val icon: DrawableResource) {
+        ERROR(Res.drawable.ic_success),
+        SUCCESS(Res.drawable.ic_close_circle),
+    }
+}
+
+val LocalSnackBarController = staticCompositionLocalOf<IdentitySnackBarController> {
+    error("No SnackBarController provided")
+}
+
+class IdentitySnackBarController() {
+
+    private val _effect = Channel<SnackBarData>(capacity = Channel.UNLIMITED)
+    val effect = _effect.receiveAsFlow()
+
+    var currentSnackBarData by mutableStateOf<SnackBarData?>(null)
+        private set
+
+
+    fun showSnackBar(
+        message: StringResource,
+        title: StringResource,
+        type: SnackBarData.SnackBarType = SnackBarData.SnackBarType.ERROR,
+        duration: Long = 3_000L
+    ) {
+        val snackBarData = SnackBarData(
+            message = message,
+            title = title,
+            type = type,
+            duration = duration
+        )
+        currentSnackBarData = snackBarData
+        _effect.trySend(snackBarData)
+    }
+
+    fun dismissSnackBar() {
+        currentSnackBarData = null
+    }
+}
