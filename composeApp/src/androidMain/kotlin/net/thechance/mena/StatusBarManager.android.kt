6d3c0b4bc41dd2092c900e9ca9683bf2d577a3fb
mohamedshemees <mohamedessampd@gmail.com>
2025-11-20 23:21:28 +0200

    feat: Implement navigation bar appearance management

--- composeApp/src/androidMain/kotlin/net/thechance/mena/StatusBarManager.android.kt
+++ composeApp/src/androidMain/kotlin/net/thechance/mena/StatusBarManager.android.kt
@@ -3,13 +3,15 @@ package net.thechance.mena
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.identity.domain.util.AppTheme
 
 
 @Composable
-actual fun SetStatusBarIconsColor(appTheme: AppTheme) {
+actual fun SetStatusBarAppearance(appTheme: AppTheme) {
     val context = LocalContext.current
     SideEffect {
         (context as? ComponentActivity)?.window?.let { window ->
@@ -17,5 +19,22 @@ actual fun SetStatusBarIconsColor(appTheme: AppTheme) {
                 isAppearanceLightStatusBars = appTheme == AppTheme.LIGHT
             }
         }
+    }
+}
+
+@Composable
+actual fun SetNavigationBarAppearance(appTheme: AppTheme) {
+    val context = LocalContext.current
+    val color = Theme.colorScheme.background.surfaceLow
+    SideEffect {
+        val activity = context as? ComponentActivity ?: return@SideEffect
+        val window = activity.window
+
+        val controller = WindowCompat.getInsetsController(window, window.decorView)
+
+        val isLight = appTheme == AppTheme.LIGHT
+        window.navigationBarColor = color.toArgb()
+
+        controller.isAppearanceLightNavigationBars = isLight
     }
 }
--- composeApp/src/iosMain/kotlin/net/thechance/mena/StatusBarManager.ios.kt
+++ composeApp/src/iosMain/kotlin/net/thechance/mena/StatusBarManager.ios.kt
@@ -2,22 +2,55 @@ package net.thechance.mena
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.identity.domain.util.AppTheme
+import platform.Foundation.NSStringCompareOptions
 import platform.UIKit.UIApplication
+import platform.UIKit.UIColor
+import platform.UIKit.UIDevice
 import platform.UIKit.UIStatusBarStyle
 import platform.UIKit.UIStatusBarStyleDarkContent
 import platform.UIKit.UIStatusBarStyleLightContent
+import platform.UIKit.UITabBar
+import platform.UIKit.UITabBarAppearance
 import platform.UIKit.setStatusBarStyle
 import platform.darwin.dispatch_async
 import platform.darwin.dispatch_get_main_queue
 
 @Composable
-actual fun SetStatusBarIconsColor(appTheme: AppTheme) {
+actual fun SetStatusBarAppearance(appTheme: AppTheme) {
     SideEffect {
         dispatch_async(dispatch_get_main_queue()) {
             val style: UIStatusBarStyle =
-                 if (appTheme == AppTheme.LIGHT) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent
+                if (appTheme == AppTheme.LIGHT) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent
             UIApplication.sharedApplication.setStatusBarStyle(style, animated = true)
         }
     }
+}
+@Composable
+actual fun SetNavigationBarAppearance(appTheme: AppTheme) {
+    SideEffect {
+        dispatch_async(dispatch_get_main_queue()) {
+            val appearance = UITabBarAppearance().apply {
+                configureWithOpaqueBackground()
+                backgroundColor = Color.Transparent.toUIColor()
+            }
+            UITabBar.appearance().standardAppearance = appearance
+
+            val systemVersion = UIDevice.currentDevice.systemVersion.split(".").firstOrNull()?.toIntOrNull() ?: 0
+            if (systemVersion >= 15) {
+                UITabBar.appearance().scrollEdgeAppearance = appearance
+            }
+        }
+    }
+}
+
+private fun Color.toUIColor(): UIColor {
+    return UIColor.colorWithRed(
+        red = this.red.toDouble(),
+        green = this.green.toDouble(),
+        blue = this.blue.toDouble(),
+        alpha = this.alpha.toDouble()
+    )
 }
