ab049bb8dd96b1e2cfa3a3d5ccf1e8fcc89a19f9
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-11-20 19:08:31 +0100

    refactor: Adapt image croper screen snackbar error handling

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/imageCropper/ImageCropperScreenState.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/imageCropper/ImageCropperScreenState.kt
@@ -1,8 +1,5 @@
 package net.thechance.mena.identity.presentation.screen.imageCropper
 
-import org.jetbrains.compose.resources.StringResource
-
 data class ImageCropperScreenState(
     val imageByteArray: ByteArray? = null,
-    val errorMessage: StringResource? = null
 )
