d910c7c7be0cb20fa21098b0e46e6de91284b2d0
Ahmed Mahgoub <aed7862@gmail.com>
2025-11-20 18:21:13 +0200

    refactor: delete the additional day that was for testing

--- trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/getLastMidnightTime.kt
+++ trends_data/src/commonMain/kotlin/net/thechance/mena/trends/data/util/getLastMidnightTime.kt
@@ -1,10 +1,8 @@
 package net.thechance.mena.trends.data.util
 
-import kotlinx.datetime.DateTimePeriod
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
@@ -12,7 +10,6 @@ import kotlin.time.ExperimentalTime
 @OptIn(ExperimentalTime::class)
 fun getLastMidnightTime(): LocalDateTime {
     val todayData = Clock.System.now()
-        .plus(DateTimePeriod(days = 1), TimeZone.currentSystemDefault())
         .toLocalDateTime(TimeZone.currentSystemDefault()).date
     return LocalDateTime(todayData, LocalTime(0, 0))
 }
