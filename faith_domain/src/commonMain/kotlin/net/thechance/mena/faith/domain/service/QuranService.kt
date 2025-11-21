e78d4d51b992873847a62544fbf34c17a664583c
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-11-21 07:57:59 +0200

    make QuranService open to make it mockable

--- faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/service/QuranService.kt
+++ faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/service/QuranService.kt
@@ -3,6 +3,6 @@ package net.thechance.mena.faith.domain.service
 import net.thechance.mena.faith.domain.entity.Surah
 import net.thechance.mena.faith.domain.repository.QuranRepository
 
-class QuranService(private val repository: QuranRepository) {
-    suspend fun getSurahDetails(surahId: Int): Surah = repository.getSurahById(surahId)
+open class QuranService(private val repository: QuranRepository) {
+    open suspend fun getSurahDetails(surahId: Int): Surah = repository.getSurahById(surahId)
 }
