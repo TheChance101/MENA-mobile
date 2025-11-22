e192ea9465572cbe6e676fc0cb6b6439599865de
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-11-22 16:28:55 +0200

    refactor: get total users

--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/users_management/UsersManagementScreenState.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/users_management/UsersManagementScreenState.kt
@@ -18,9 +18,10 @@ data class UsersManagementScreenState(
     val errorState: ErrorState? = null,
     val snackBar: SnackBarState = SnackBarState(),
     val isBlockDialogShown: Boolean = false,
-    val selectedUserId: Uuid? = null
+    val selectedUserId: Uuid? = null,
+    val totalUsers : Int = 0,
 ) {
-    val isSortingDisabled = users.size < 2
+    val isSortingDisabled = totalUsers < 2
 
     data class UserPageInfo(
         val page: Int = 0,
--- admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/users_management/UsersManagementViewModel.kt
+++ admin_panel/src/commonMain/kotlin/net/thechance/mena/admin_panel/presentation/screen/users_management/UsersManagementViewModel.kt
@@ -167,7 +167,8 @@ class UsersManagementViewModel(
                     page = result.currentPage,
                     totalPages = result.totalPages
                 ),
-                errorState = null
+                errorState = null,
+                totalUsers = result.totalElements
             )
         }
     }
