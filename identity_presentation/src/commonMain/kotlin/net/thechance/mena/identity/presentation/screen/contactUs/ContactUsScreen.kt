bac068fbd4db011c651eb2c2cf100f0171fccd5d
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2025-11-24 07:21:40 +0200

    fix: Add LRM mark to phone number in ContactUsScreen

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/contactUs/ContactUsScreen.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/contactUs/ContactUsScreen.kt
@@ -124,7 +124,7 @@ class ContactUsScreen : BaseScreen<
                     ContactCard(
                         icon = Res.drawable.ic_telephone,
                         title = Res.string.contact_phone_number,
-                        info = state.phoneNumber,
+                        info = "\u200E${state.phoneNumber}",
                         onClick = listener::onClickPhoneNumber
                     )
 
