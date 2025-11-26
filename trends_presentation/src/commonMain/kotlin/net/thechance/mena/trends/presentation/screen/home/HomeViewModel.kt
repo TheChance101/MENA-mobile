d4d6ba38a2c936fd713e32eea2780d082d067548
Ahmed Mahgoub <aed7862@gmail.com>
2025-11-26 19:56:54 +0200

    refactor: replace update paging data with paging events and listen to updates from repository

--- trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/home/HomeViewModel.kt
+++ trends_presentation/src/commonMain/kotlin/net/thechance/mena/trends/presentation/screen/home/HomeViewModel.kt
@@ -7,10 +7,15 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import net.thechance.mena.trends.domain.entity.Trend
+import net.thechance.mena.trends.domain.model.TrendUpdates
 import net.thechance.mena.trends.domain.repository.TrendsRepository
+import net.thechance.mena.trends.presentation.screen.user_trend.UserTrendUiState
 import net.thechance.mena.trends.presentation.shared.base.BaseViewModel
+import net.thechance.mena.trends.presentation.shared.base.PagingEvents
+import net.thechance.mena.trends.presentation.shared.base.applyEvent
 import net.thechance.mena.trends.presentation.shared.base.createPager
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.Provided
@@ -24,67 +29,64 @@ internal class HomeViewModel(
 
     init {
         getFeedTrends()
+        observeTrendsUpdates()
     }
 
-    fun addTrendLike(trendId: String) {
+    private fun observeTrendsUpdates() {
+        tryToCollectFlow(
+            block = repository::observeTrendUpdates,
+            onNewValue = ::onTrendUpdates,
+            onError = {},
+            dispatcher = defaultDispatcher
+        )
+    }
+
+    private fun onTrendUpdates(trendUpdates: TrendUpdates) {
+        if(trendUpdates.isDeleted) {
+            sendPagingEvent(PagingEvents.Remove(trendUpdates.trendId))
+            return
+        }
+        sendPagingEvent(
+            PagingEvents.Update(
+                itemId = trendUpdates.trendId,
+                transform = { trend ->
+                    trend.copy(
+                        isLiked = trendUpdates.isLiked,
+                        likesCount = trendUpdates.likesCount,
+                        viewsCount = trendUpdates.viewsCount
+                    )
+                }
+            )
+        )
+    }
+
+    private fun addTrendLike(trendId: String) {
         tryToExecute(
-            onStart = { updateLikesOnUi(trendId) },
             block = { repository.addTrendLike(trendId) },
             onError = { error ->
-                updateLikesOnUi(trendId)
+                toggleTrendLike(trendId)
                 updateState { copy(error = error) }
             },
             dispatcher = defaultDispatcher,
-            onSuccess = { updatedTrend ->
-                onAddLikeSuccess(trendId = trendId, updatedTrend = updatedTrend)
-            }
         )
     }
 
-    private fun onAddLikeSuccess(trendId: String, updatedTrend: Trend) {
-        updateTrendInPagingData(trendId) {
-            updatedTrend.toUiState().copy(isDescriptionExpanded = it.isDescriptionExpanded)
-        }
-    }
-
-    private fun updateLikesOnUi(trendId: String) {
-        updateTrendInPagingData(trendId) { trend ->
-            trend.copy(
-                isLiked = !trend.isLiked,
-                likesCount = if (trend.isLiked) trend.likesCount - 1 else trend.likesCount + 1
-            )
-        }
-    }
-
-    fun removeTrendLike(trendId: String) {
+    private fun removeTrendLike(trendId: String) {
         tryToExecute(
-            onStart = { updateLikesOnUi(trendId) },
             block = { repository.removeTrendLike(trendId) },
             onError = { error ->
-                updateLikesOnUi(trendId)
+                toggleTrendLike(trendId)
                 updateState { copy(error = error) }
             },
             dispatcher = defaultDispatcher,
         )
     }
 
-    private fun updateTrendInPagingData(trendId: String, transform: (TrendUiState) -> TrendUiState) {
-        val currentData = state.value.trendsStateFlow.value
-        val updatedData = currentData.map { trend ->
-            if (trend.id == trendId) transform(trend) else trend
-        }
-        state.value.trendsStateFlow.value = updatedData
-        updateState { copy(trends = state.value.trendsStateFlow) }
-    }
-
-    fun getFeedTrends() {
-        tryToCollectFlow(
+    private fun getFeedTrends() {
+        tryToExecute(
             block = ::createPager,
             onStart = { updateState { copy(isLoading = true) } },
-            onNewValue = { uiPagingData ->
-                state.value.trendsStateFlow.value = uiPagingData
-                updateState { copy(trends = trendsStateFlow, isLoading = false) }
-            },
+            onSuccess = { trends -> updateState { copy(trends = trends) } },
             onError = { error -> updateState { copy(error = error, isLoading = false) } },
             onEnd = { updateState { copy(isLoading = false) } },
             dispatcher = defaultDispatcher
@@ -96,6 +98,14 @@ internal class HomeViewModel(
             scope = viewModelScope,
             loadPage = { page -> repository.getFeedTrends(page) }
         ).map { pagingData -> pagingData.map { it.toUiState() } }
+            .combine(state.value.pagingEvents) { pagingDate, pagingEvents ->
+                pagingEvents.fold(pagingDate) { data, event ->
+                    data.applyEvent(
+                        event = event,
+                        getItemId = { it.id }
+                    )
+                }
+            }
     }
 
     override fun onClickAddTrend() {
@@ -115,11 +125,26 @@ internal class HomeViewModel(
     }
 
     override fun onClickLike(trendId: String, isLiked: Boolean) {
+        toggleTrendLike(trendId)
         if (isLiked) {
             removeTrendLike(trendId)
         } else {
             addTrendLike(trendId)
         }
+    }
+
+    private fun toggleTrendLike(trendId: String) {
+        sendPagingEvent(
+            event = PagingEvents.Update(
+                itemId = trendId,
+                transform = { trend ->
+                    trend.copy(
+                        isLiked = !trend.isLiked,
+                        likesCount = if(trend.isLiked) trend.likesCount-1 else trend.likesCount+1
+                    )
+                }
+            )
+        )
     }
 
     override fun onClickRetry() {
@@ -128,11 +153,14 @@ internal class HomeViewModel(
     }
 
     override fun onClickExpandDescription(trendId: String) {
-        state.value.trendsStateFlow.value =
-            state.value.trendsStateFlow.value.map { trend ->
-                trend.takeIf { it.id != trendId }
-                    ?: trend.copy(isDescriptionExpanded = !trend.isDescriptionExpanded)
-            }
+        sendPagingEvent(
+            PagingEvents.Update(
+                itemId = trendId,
+                transform = { trend ->
+                    trend.copy(isDescriptionExpanded = !trend.isDescriptionExpanded)
+                }
+            )
+        )
     }
 
     override fun onGetRefreshedThumbnail(trendId: String) {
@@ -145,10 +173,15 @@ internal class HomeViewModel(
     }
 
     private fun onGetRefreshedThumbnailSuccess(refreshedUrl: String, trendId: String) {
-        state.value.trendsStateFlow.value =
-            state.value.trendsStateFlow.value.map { trend ->
-                trend.takeIf { it.id != trendId }
-                    ?: trend.copy(thumbnailUrl = refreshedUrl)
-            }
+        sendPagingEvent(
+            PagingEvents.Update(
+                itemId = trendId,
+                transform = { trend -> trend.copy(thumbnailUrl = refreshedUrl) }
+            )
+        )
+    }
+
+    private fun sendPagingEvent(event: PagingEvents<TrendUiState>) {
+        state.value.pagingEvents.value = state.value.pagingEvents.value + event
     }
 }
