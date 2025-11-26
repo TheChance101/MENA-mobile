ef4287ff9e4de537c7066a809d9d17a54e12bbc3
AdelTamer <adeltammer@gmail.com>
2025-11-26 17:51:21 +0200

    refactor `getChatByUserId` in `ChatEntryViewModel`

--- core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/api/ChatEntryViewModel.kt
+++ core_chat_presentation/src/commonMain/kotlin/net/thechance/mena/core_chat/presentation/api/ChatEntryViewModel.kt
@@ -1,5 +1,6 @@
 package net.thechance.mena.core_chat.presentation.api
 
+import net.thechance.mena.core_chat.domain.entity.Chat
 import net.thechance.mena.core_chat.domain.repository.ChatRepository
 import net.thechance.mena.core_chat.presentation.shared.BaseViewModel
 import kotlin.uuid.ExperimentalUuidApi
@@ -10,21 +11,25 @@ class ChatEntryViewModel(private val chatRepository: ChatRepository) :
     BaseViewModel<ChatEntryState, Unit>(ChatEntryState()) {
     fun getChatByUserId(userId: String) {
         tryToExecute(
-            execute = {
-                updateState { it.copy(isContentVisible = false, error = false) }
-                chatRepository.getChatByOtherUserId(Uuid.parse(userId))
-            },
-            onSuccess = { chat ->
-                updateState {
-                    it.copy(
-                        chatId = chat.id,
-                        chatName = chat.name,
-                        isContentVisible = true,
-                        error = false
-                    )
-                }
-            },
-            onError = { chat -> updateState { it.copy(isContentVisible = false, error = true) } }
+            onStart = { updateState { it.copy(isContentVisible = false, error = false) } },
+            execute = { chatRepository.getChatByOtherUserId(Uuid.parse(userId)) },
+            onSuccess = ::onGetChatByOtherUserIdSuccess,
+            onError = { onGetChatByOtherUserIdError() }
         )
+    }
+
+    private fun onGetChatByOtherUserIdError() {
+        updateState { it.copy(isContentVisible = false, error = true) }
+    }
+
+    private fun onGetChatByOtherUserIdSuccess(chat: Chat) {
+        updateState {
+            it.copy(
+                chatId = chat.id,
+                chatName = chat.name,
+                isContentVisible = true,
+                error = false
+            )
+        }
     }
 }
