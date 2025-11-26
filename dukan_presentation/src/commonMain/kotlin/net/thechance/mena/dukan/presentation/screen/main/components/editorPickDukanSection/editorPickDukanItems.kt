f3f81dadbce1e1947a3d93cb30c10840d1e1c0e1
a7med naser <ad0112727@gmail.com>
2025-11-26 23:31:51 +0200

    refactor(main): Remove unused LazyColumn imports

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/main/components/editorPickDukanSection/editorPickDukanItems.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/main/components/editorPickDukanSection/editorPickDukanItems.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
