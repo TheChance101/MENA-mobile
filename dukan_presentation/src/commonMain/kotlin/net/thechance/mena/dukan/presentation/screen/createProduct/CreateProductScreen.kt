433ffddac7b6c89aa2f95b3b2b855cb10ccbd322
a7med naser <ad0112727@gmail.com>
2025-11-28 23:40:00 +0200

    Refactor(create-product): Remove unused imports

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/createProduct/CreateProductScreen.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/createProduct/CreateProductScreen.kt
@@ -3,14 +3,12 @@ package net.thechance.mena.dukan.presentation.screen.createProduct
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.navOptions
 import mena.dukan_presentation.generated.resources.Res
