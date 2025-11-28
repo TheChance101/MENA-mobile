2bcc7dc67527584e1a7829f6579ec41ca4bc50a1
a7med naser <ad0112727@gmail.com>
2025-11-28 23:39:36 +0200

    feat(create-product): Use `wrapContentHeight` for the bottom bar

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/createProduct/CreateProductScreen.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/createProduct/CreateProductScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -110,7 +111,7 @@ private fun CreateProductContent(
                     .background(color = Theme.colorScheme.background.surface)
                     .padding(bottom = Theme.spacing._16)
                     .fillMaxWidth()
-                    .height(48.dp)
+                    .wrapContentHeight()
                     .padding(horizontal = Theme.spacing._16)
             )
         },
