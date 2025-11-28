f2b2fd3f22663a05b24ba878caecc0cfc7648fe2
nada <nadamansoursamir@gmail.com>
2025-11-28 23:02:42 +0200

    feat: Implement dark mode support for Dukan screen images

--- dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/manageDukan/component/DeactivatedDukanScreen.kt
+++ dukan_presentation/src/commonMain/kotlin/net/thechance/mena/dukan/presentation/screen/manageDukan/component/DeactivatedDukanScreen.kt
@@ -11,17 +11,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import mena.dukan_presentation.generated.resources.Res
 import mena.dukan_presentation.generated.resources.deactivated_dukan
+import mena.dukan_presentation.generated.resources.deactivated_dukan_dark
 import mena.dukan_presentation.generated.resources.wrong_information_provided
 import mena.dukan_presentation.generated.resources.your_dukan_has_been_deactivated
 import net.thechance.mena.designsystem.presentation.component.text.Text
 import net.thechance.mena.designsystem.presentation.theme.theme.MenaTheme
 import net.thechance.mena.designsystem.presentation.theme.theme.Theme
 import net.thechance.mena.dukan.presentation.component.state.ImageWithTextContainer
+import net.thechance.mena.dukan.presentation.navigation.LocalDarkTheme
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun DeactivatedDukanScreen() {
+
+    val isDark = LocalDarkTheme.current
+    val icon = if(isDark) Res.drawable.deactivated_dukan_dark else Res.drawable.deactivated_dukan
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -30,7 +36,7 @@ fun DeactivatedDukanScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         ImageWithTextContainer(
-            foregroundImageRes = Res.drawable.deactivated_dukan,
+            foregroundImageRes = icon,
             haveBlurBackground = false,
             header = {
                     Text(
