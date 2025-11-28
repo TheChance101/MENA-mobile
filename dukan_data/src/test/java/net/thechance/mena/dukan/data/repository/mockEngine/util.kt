d77c89df7101bc62fc0a2ed22f364a731005a207
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-11-28 15:59:37 +0200

    fixed the test cases in mock engine

--- dukan_data/src/test/java/net/thechance/mena/dukan/data/repository/mockEngine/util.kt
+++ dukan_data/src/test/java/net/thechance/mena/dukan/data/repository/mockEngine/util.kt
@@ -1,10 +1,17 @@
 package net.thechance.mena.dukan.data.repository.mockEngine
 
+import io.ktor.client.HttpClient
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.headersOf
 import kotlinx.serialization.json.Json
+import net.thechance.mena.dukan.data.util.network.DukanApi
 
 
 val jsonSerialization = Json { ignoreUnknownKeys = true }
 val jsonHeaders = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+
+class MockDukanApiClient(private val httpClient: HttpClient) : DukanApi {
+    override fun getClient(): HttpClient = httpClient
+    override fun reset() {}
+}
