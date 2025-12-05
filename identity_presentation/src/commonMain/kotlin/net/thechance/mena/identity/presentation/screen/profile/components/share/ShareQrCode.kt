89250bb06f32903d570fcbe905de7ea93fd8cdff
Aboud <gh.abd.com.207@gmail.com>
2025-12-05 06:15:32 -0800

    fix: Pass initial state to ShareDialogViewModel

--- identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/profile/components/share/ShareQrCode.kt
+++ identity_presentation/src/commonMain/kotlin/net/thechance/mena/identity/presentation/screen/profile/components/share/ShareQrCode.kt
@@ -54,18 +54,18 @@ import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.parameter.parametersOf
 import sv.lib.squircleshape.SquircleShape
 
 @Composable
 fun ScaffoldScope.ShareQrCode(
-    viewModel: ShareDialogViewModel = koinViewModel(),
     isVisible: Boolean,
     fullName: String,
     onClickShare: () -> Unit,
     onDismissShareDialog: () -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: ShareDialogViewModel = koinViewModel{ parametersOf(ShareQrCodeUIState()) },
 ) {
-
     val shareState by viewModel.state.collectAsStateWithLifecycle()
     val snackBarController = LocalSnackBarController.current
 
