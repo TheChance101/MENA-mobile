5052f13fdc6e33e74fcfe4ce03d4c3f15b185a78
Aboud <gh.abd.com.207@gmail.com>
2025-12-18 12:47:44 -0800

    feat: Add PickLocationArgs for pick location screen

---
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/mosque/pickLocationMap/args/PickLocationArgs.kt
@@ -0,0 +1,7 @@
+package net.thechance.mena.faith.presentation.feature.mosque.pickLocationMap.args
+
+interface PickLocationArgs {
+    val latitude: Double?
+    val longitude: Double?
+}
+
---
+++ faith_presentation/src/commonMain/kotlin/net/thechance/mena/faith/presentation/feature/mosque/pickLocationMap/args/PickLocationArgsImpl.kt
@@ -0,0 +1,10 @@
+package net.thechance.mena.faith.presentation.feature.mosque.pickLocationMap.args
+
+import androidx.lifecycle.SavedStateHandle
+
+class PickLocationArgsImpl(
+    private val savedStateHandle: SavedStateHandle,
+) : PickLocationArgs {
+    override val latitude: Double? = savedStateHandle["latitude"]
+    override val longitude: Double? = savedStateHandle["longitude"]
+}
