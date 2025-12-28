a678f9ecab53510f41ee3251966a968a9bd8aed8
Aboud <gh.abd.com.207@gmail.com>
2025-12-28 05:10:25 -0800

    refactor: Remove `getCurrentLocation` function

--- faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/repository/location/LocationRepositoryImpl.kt
+++ faith_data/src/commonMain/kotlin/net/thechance/mena/faith/data/repository/location/LocationRepositoryImpl.kt
@@ -1,9 +1,5 @@
 package net.thechance.mena.faith.data.repository.location
 
-import dev.jordond.compass.Priority
-import dev.jordond.compass.geolocation.Geolocator
-import dev.jordond.compass.geolocation.GeolocatorResult
-import dev.jordond.compass.geolocation.MobileGeolocator
 import net.thechance.mena.faith.domain.entity.Mosque
 import net.thechance.mena.faith.domain.exception.FaithException
 import net.thechance.mena.faith.domain.repository.LocationRepository
@@ -11,22 +7,6 @@ import net.thechance.mena.faith.domain.repository.LocationRepository
 class LocationRepositoryImpl(
     private val geocoder: GeocoderWrapper,
 ) : LocationRepository  {
-    override suspend fun getCurrentLocation(): Mosque.Coordinates? {
-        val geolocator: Geolocator = MobileGeolocator()
-        return when (val result = geolocator.current(Priority.HighAccuracy)) {
-            is GeolocatorResult.Error -> {
-                throw FaithException.UnableToFindLocationException
-            }
-
-            is GeolocatorResult.Success -> {
-                val location = result.data
-                Mosque.Coordinates(
-                    latitude = location.coordinates.latitude,
-                    longitude = location.coordinates.longitude
-                )
-            }
-        }
-    }
 
     override suspend fun getLocationName(
         coordinates: Mosque.Coordinates,
--- faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/repository/LocationRepository.kt
+++ faith_domain/src/commonMain/kotlin/net/thechance/mena/faith/domain/repository/LocationRepository.kt
@@ -3,6 +3,5 @@ package net.thechance.mena.faith.domain.repository
 import net.thechance.mena.faith.domain.entity.Mosque
 
 interface LocationRepository {
-    suspend fun getCurrentLocation(): Mosque.Coordinates?
     suspend fun getLocationName(coordinates: Mosque.Coordinates): String
 }
